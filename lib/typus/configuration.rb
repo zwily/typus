79fdfb6b057bbc8d5a0a45d5b8c90fbbff6b04fb
Francesc Esplugas <fesplugas@intraducibles.net>
2008-07-04 15:35:03 +0200

    Load typus.yml on plugins ...

--- lib/typus/configuration.rb
+++ lib/typus/configuration.rb
@@ -10,20 +10,19 @@ module Typus
     #   Typus::Configuration.options[:app_name] = "Your App Name"
     #   Typus::Configuration.options[:per_page] = 15
     #
-    @@options = {
-        :app_logo => '',
-        :app_logo_height => '',
-        :app_logo_width => '',
-        :app_name => 'Typus Admin',
-        :app_description => '',
-        :per_page => 15,
-        :prefix => 'admin',
-        :color => '#000',
-        :version => '',
-        :signature => '',
-        :form_rows => '10',
-        :form_columns => '10' 
-        }
+    @@options = { :app_logo => '',
+                  :app_logo_height => '',
+                  :app_logo_width => '',
+                  :app_name => 'Typus Admin',
+                  :app_description => '',
+                  :per_page => 15,
+                  :prefix => 'admin',
+                  :color => '#000',
+                  :version => '',
+                  :signature => '',
+                  :form_rows => '10',
+                  :form_columns => '10' }
+
     mattr_reader :options
 
     ##
@@ -31,11 +30,14 @@ module Typus
     case ENV['RAILS_ENV']
     when 'test'
       config_file = "#{File.dirname(__FILE__)}/../../test/typus.yml"
+      @@config = YAML.load_file(config_file)
     else
       config_file = "#{RAILS_ROOT}/config/typus.yml"
+      @@config = YAML.load_file(config_file)
+      Dir['vendor/plugins/*/config/typus.yml'].each do |plugin|
+        @@config = @@config.merge(YAML.load_file("#{RAILS_ROOT}/#{plugin}"))
+      end
     end
-
-    @@config = YAML.load_file(config_file)
 
     mattr_reader :config
 
