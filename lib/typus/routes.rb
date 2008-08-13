c9081f44334c74de158417949e7059a8907d05f6
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-13 19:16:59 +0200

    Initial routes fixing

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -4,6 +4,21 @@ end
 
 ActionController::Routing::Routes.draw do |map|
 
+  map.with_options :controller => 'typus' do |i|
+    i.typus_dashboard "#{prefix}", :action => 'dashboard'
+    i.typus_login "#{prefix}/login", :action => 'login'
+    i.typus_logout "#{prefix}/logout", :action => 'logout'
+    i.typus_email_password "#{prefix}/email_password", :action => 'email_password'
+    i.typus_index "#{prefix}/:model", :action => 'index'
+    i.connect "#{prefix}/:model/:action", :requirements => { :action => /index|new|create/ }
+    i.connect "#{prefix}/:model/:id/:action", :requirements => { :action => /edit|update|destroy|position|toggle|relate|unrelate/, :id => /\d+/ }
+  end
+
+  map.connect "#{prefix}/:model/:action", :controller => "typus/#{:model}"
+  map.connect "#{prefix}/:model/:id/:action", :controller => "typus/#{:model}"
+
+=begin
+
   map.namespace :admin do |admin|
 
     Typus.models.each do |m|
@@ -15,25 +30,6 @@ ActionController::Routing::Routes.draw do |map|
 
   end
 
-end
-
-=begin
-
-    draw_without_admin do |map|
-
-      map.with_options :controller => 'typus' do |i|
-        i.typus_dashboard "#{prefix}", :action => 'dashboard'
-        i.typus_login "#{prefix}/login", :action => 'login'
-        i.typus_logout "#{prefix}/logout", :action => 'logout'
-        i.typus_email_password "#{prefix}/email_password", :action => 'email_password'
-        i.typus_index "#{prefix}/:model", :action => 'index'
-        i.connect "#{prefix}/:model/:action", :requirements => { :action => /index|new|create/ }
-        i.connect "#{prefix}/:model/:id/:action", :requirements => { :action => /edit|update|destroy|position|toggle|relate|unrelate/, :id => /\d+/ }
-      end
-
-      map.connect "#{prefix}/:model/:action", :controller => "typus/#{:model}"
-      map.connect "#{prefix}/:model/:id/:action", :controller => "typus/#{:model}"
-
-      yield map
-
 =end
+
+end
