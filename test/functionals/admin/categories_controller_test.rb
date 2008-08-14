63568624728d924e87e9edb3f2eb5c7a0db8bd48
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-14 16:26:25 +0200

    Refactored

--- test/functionals/admin/categories_controller_test.rb
+++ test/functionals/admin/categories_controller_test.rb
@@ -2,12 +2,13 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::CategoriesControllerTest < ActionController::TestCase
 
-  def test_should_position_item_one_step_down
-
+  def setup
     user = typus_users(:user)
     @request.session[:typus] = user.id
     @request.env["HTTP_REFERER"] = "/admin/categories"
+  end
 
+  def test_should_position_item_one_step_down
     first_category = categories(:first)
     assert_equal first_category.position, 1
     second_category = categories(:second)
@@ -18,15 +19,9 @@ class Admin::CategoriesControllerTest < ActionController::TestCase
     assert_equal first_category.position, 2
     second_category = Category.find(2)
     assert_equal second_category.position, 1
-
   end
 
   def test_should_position_item_one_step_up
-
-    user = typus_users(:user)
-    @request.session[:typus] = user.id
-    @request.env["HTTP_REFERER"] = "/admin/categories"
-
     first_category = categories(:first)
     assert_equal first_category.position, 1
     second_category = categories(:second)
@@ -37,41 +32,24 @@ class Admin::CategoriesControllerTest < ActionController::TestCase
     assert_equal first_category.position, 2
     second_category = Category.find(2)
     assert_equal second_category.position, 1
-
   end
 
   def test_should_position_top_item_to_bottom
-
-    user = typus_users(:user)
-    @request.session[:typus] = user.id
-    @request.env["HTTP_REFERER"] = "/admin/categories"
-
     first_category = categories(:first)
     assert_equal first_category.position, 1
-
     get :position, { :id => first_category.id, :go => 'move_to_bottom' }
     assert flash[:success]
-
     first_category = Category.find(1)
     assert_equal first_category.position, 3
-
   end
 
   def test_should_position_bottom_item_to_top
-
-    user = typus_users(:user)
-    @request.session[:typus] = user.id
-    @request.env["HTTP_REFERER"] = "/admin/categories"
-
     third_category = categories(:third)
     assert_equal third_category.position, 3
-
     get :position, { :id => third_category.id, :go => 'move_to_top' }
     assert flash[:success]
-
     third_category = Category.find(3)
     assert_equal third_category.position, 1
-
   end
 
 end
