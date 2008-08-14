5bbc77a50d8dcdbfcc77df140278dc25173c39e3
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-14 10:14:21 +0200

    Prefix is no longer used

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -4,22 +4,20 @@ class ActionController::Routing::RouteSet
 
   def draw_with_admin
 
-    prefix = Typus::Configuration.options[:prefix]
-
     draw_without_admin do |map|
 
       map.with_options :controller => 'typus' do |i|
-        i.typus_dashboard "#{prefix}", :action => 'dashboard'
-        i.typus_login "#{prefix}/login", :action => 'login'
-        i.typus_logout "#{prefix}/logout", :action => 'logout'
-        i.typus_email_password "#{prefix}/email_password", :action => 'email_password'
-        i.typus_index "#{prefix}/:model", :action => 'index'
-        i.connect "#{prefix}/:model/:action", :requirements => { :action => /index|new|create/ }
-        i.connect "#{prefix}/:model/:id/:action", :requirements => { :action => /edit|update|destroy|position|toggle|relate|unrelate/, :id => /\d+/ }
+        i.typus_dashboard "admin", :action => 'dashboard'
+        i.typus_login "admin/login", :action => 'login'
+        i.typus_logout "admin/logout", :action => 'logout'
+        i.typus_email_password "admin/email_password", :action => 'email_password'
+        i.typus_index "admin/:model", :action => 'index'
+        i.connect "admin/:model/:action", :requirements => { :action => /index|new|create/ }
+        i.connect "admin/:model/:id/:action", :requirements => { :action => /edit|update|destroy|position|toggle|relate|unrelate/, :id => /\d+/ }
       end
 
-      map.connect "#{prefix}/:model/:action", :controller => "typus/#{:model}"
-      map.connect "#{prefix}/:model/:id/:action", :controller => "typus/#{:model}"
+      map.connect "admin/:model/:action", :controller => "typus/#{:model}"
+      map.connect "admin/:model/:id/:action", :controller => "typus/#{:model}"
 
       yield map
 
