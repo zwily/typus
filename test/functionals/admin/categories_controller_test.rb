8e8945b2098a6faef119e20d9a0d47346f04b6d8
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-14 15:58:46 +0200

    Splitted test functionality ...

---
+++ test/functionals/admin/categories_controller_test.rb
@@ -0,0 +1,77 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Admin::CategoriesControllerTest < ActionController::TestCase
+
+  def test_should_position_item_one_step_down
+
+    user = typus_users(:user)
+    @request.session[:typus] = user.id
+    @request.env["HTTP_REFERER"] = "/admin/categories"
+
+    first_category = categories(:first)
+    assert_equal first_category.position, 1
+    second_category = categories(:second)
+    assert_equal second_category.position, 2
+    get :position, { :id => first_category.id, :go => 'move_lower' }
+    assert flash[:success]
+    first_category = Category.find(1)
+    assert_equal first_category.position, 2
+    second_category = Category.find(2)
+    assert_equal second_category.position, 1
+
+  end
+
+  def test_should_position_item_one_step_up
+
+    user = typus_users(:user)
+    @request.session[:typus] = user.id
+    @request.env["HTTP_REFERER"] = "/admin/categories"
+
+    first_category = categories(:first)
+    assert_equal first_category.position, 1
+    second_category = categories(:second)
+    assert_equal second_category.position, 2
+    get :position, { :id => second_category.id, :go => 'move_higher' }
+    assert flash[:success]
+    first_category = Category.find(1)
+    assert_equal first_category.position, 2
+    second_category = Category.find(2)
+    assert_equal second_category.position, 1
+
+  end
+
+  def test_should_position_top_item_to_bottom
+
+    user = typus_users(:user)
+    @request.session[:typus] = user.id
+    @request.env["HTTP_REFERER"] = "/admin/categories"
+
+    first_category = categories(:first)
+    assert_equal first_category.position, 1
+
+    get :position, { :id => first_category.id, :go => 'move_to_bottom' }
+    assert flash[:success]
+
+    first_category = Category.find(1)
+    assert_equal first_category.position, 3
+
+  end
+
+  def test_should_position_bottom_item_to_top
+
+    user = typus_users(:user)
+    @request.session[:typus] = user.id
+    @request.env["HTTP_REFERER"] = "/admin/categories"
+
+    third_category = categories(:third)
+    assert_equal third_category.position, 3
+
+    get :position, { :id => third_category.id, :go => 'move_to_top' }
+    assert flash[:success]
+
+    third_category = Category.find(3)
+    assert_equal third_category.position, 1
+
+  end
+
+end
---
+++ test/functionals/admin/posts_controller_test.rb
@@ -0,0 +1,56 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Admin::PostsControllerTest < ActionController::TestCase
+
+  def test_should_redirect_to_login
+    get :index
+    assert_response :redirect
+    assert_redirected_to typus_login_url
+  end
+
+  def test_should_render_new
+    @request.session[:typus] = 1
+    get :new
+    assert_response :success
+    assert_template 'new'
+  end
+
+  def test_should_create_item
+    @request.session[:typus] = 1
+
+    items = Post.count
+
+    post :create, { :item => { :title => "This is another title", 
+                               :body => "This is the body.", 
+                               :category => 1 } }
+#    assert_response :redirect
+#    assert_redirected_to :action => 'edit'
+#    assert_equal items + 1, Post.count
+  end
+
+  def test_should_render_edit
+    @request.session[:typus] = 1
+    get :edit, { :id => 1 }
+    assert_response :success
+    assert_template 'edit'
+  end
+
+  def test_should_update_item
+    @request.session[:typus] = 1
+    post :update, { :id => 1, :title => "Updated" }
+    assert_response :success
+  end
+
+  def test_should_allow_admin_to_toggle_item
+    @request.env["HTTP_REFERER"] = "/admin/posts"
+    admin = typus_users(:admin)
+    post = posts(:unpublished)
+    @request.session[:typus] = admin.id
+    get :toggle, { :id => post.id, :field => 'status' }
+    assert_response :redirect
+    assert_redirected_to :action => 'index'
+    assert flash[:success]
+    assert Post.find(post.id).status
+  end
+
+end
