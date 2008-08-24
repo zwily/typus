48cf4bf5aa289d964579a0f6773f7fe90a16b323
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-24 19:52:10 +0200

    Dont check if typus.yml exists as we can load settings directly from the models ...

--- init.rb
+++ init.rb
@@ -1,34 +1,26 @@
 begin
 
-  if File.exists?("#{RAILS_ROOT}/config/typus.yml")
+  ##
+  # Add Typus views ...
+  #
+  ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'app', 'views'))
 
-    ##
-    # Add Typus views ...
-    #
-    ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'app', 'views'))
-
-    ##
-    # Add Typus controllers, models and helpers ...
-    #
-    %w( controllers models helpers ).each do |m|
-      Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', m)
-    end
-
-    ##
-    # Required gems and files.
-    #
-    %w( sha1 paginator typus ).each { |lib| require lib }
-
-    ##
-    # And finally we enable Typus
-    #
-    Typus.enable
-
-  else
-
-    puts "=> Typus is not enabled because 'config/typus.yml' does not exist."
-
+  ##
+  # Add Typus controllers, models and helpers ...
+  #
+  %w( controllers models helpers ).each do |m|
+    Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', m)
   end
+
+  ##
+  # Required gems and files.
+  #
+  %w( sha1 paginator typus ).each { |lib| require lib }
+
+  ##
+  # And finally we enable Typus
+  #
+  Typus.enable
 
 rescue LoadError
   puts "=> [TYPUS] Install required plugins and gems with `rake typus:dependencies`"
