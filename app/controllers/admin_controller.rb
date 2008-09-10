b303c40f0e934777c57bab120d804503cf413252
Francesc Esplugas <fesplugas@intraducibles.net>
2008-09-10 14:47:04 +0200

    Method to generate CSV

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -269,7 +269,19 @@ private
   end
 
   def generate_csv
-    render :text => "generate_csv"
+    fields = @model.typus_fields_for(:csv).collect { |i| i.first }
+    csv_string = FasterCSV.generate do |csv|
+      csv << fields
+      @items.items.each do |item|
+        tmp = []
+        fields.each { |f| tmp << item.send(f) }
+        csv << tmp
+      end
+    end
+    filename = "#{Time.now.strftime("%Y%m%d")}_#{@model.to_s.tableize}.csv"
+    send_data(csv_string,
+             :type => 'text/csv; charset=utf-8; header=present',
+             :filename => filename)
   end
 
 end
