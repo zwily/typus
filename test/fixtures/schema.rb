af7fecba3280e859532296088de0294457707311
Francesc Esplugas <fesplugas@intraducibles.net>
2008-09-18 14:57:53 +0200

    Silent stream ...

--- test/fixtures/schema.rb
+++ test/fixtures/schema.rb
@@ -1,64 +1,68 @@
-ActiveRecord::Schema.define do
+silence_stream(STDOUT) do
 
-  create_table :typus_users, :force => true do |t|
-    t.string :email, :first_name, :last_name
-    t.string :salt, :crypted_password
-    t.boolean :status, :default => false
-    t.string :roles
-    t.timestamps
-  end
+  ActiveRecord::Schema.define do
 
-  create_table :tags, :force => true do |t|
-    t.string :name
-  end
+    create_table :typus_users, :force => true do |t|
+      t.string :email, :first_name, :last_name
+      t.string :salt, :crypted_password
+      t.boolean :status, :default => false
+      t.string :roles
+      t.timestamps
+    end
 
-  create_table :users, :force => true do |t|
-    t.string :first_name, :last_name, :email
-  end
+    create_table :tags, :force => true do |t|
+      t.string :name
+    end
 
-  create_table :posts, :force => true do |t|
-    t.string :title
-    t.text :body
-    t.boolean :status
-    t.timestamps
-    t.integer :user_id
-  end
+    create_table :users, :force => true do |t|
+      t.string :first_name, :last_name, :email
+    end
 
-  add_index :posts, :user_id
+    create_table :posts, :force => true do |t|
+      t.string :title
+      t.text :body
+      t.boolean :status
+      t.timestamps
+      t.integer :user_id
+    end
 
-  create_table :categories, :force => true do |t|
-    t.string :name
-    t.string :permalink
-    t.text :description
-    t.integer :position
-  end
+    add_index :posts, :user_id
 
-  create_table :categories_posts, :force => true, :id => false do |t|
-    t.column :category_id, :integer
-    t.column :post_id, :integer
-  end
+    create_table :categories, :force => true do |t|
+      t.string :name
+      t.string :permalink
+      t.text :description
+      t.integer :position
+    end
 
-  add_index :categories_posts, :category_id
-  add_index :categories_posts, :post_id
+    create_table :categories_posts, :force => true, :id => false do |t|
+      t.column :category_id, :integer
+      t.column :post_id, :integer
+    end
 
-  create_table :posts_tags, :force => true, :id => false do |t|
-    t.column :post_id, :integer
-    t.column :tag_id, :integer
-  end
+    add_index :categories_posts, :category_id
+    add_index :categories_posts, :post_id
 
-  add_index :posts_tags, :post_id
-  add_index :posts_tags, :tag_id
+    create_table :posts_tags, :force => true, :id => false do |t|
+      t.column :post_id, :integer
+      t.column :tag_id, :integer
+    end
 
-  create_table :comments, :force => true do |t|
-    t.string :email, :name
-    t.text :body
-    t.integer :post_id
-  end
+    add_index :posts_tags, :post_id
+    add_index :posts_tags, :tag_id
 
-  add_index :comments, :post_id
+    create_table :comments, :force => true do |t|
+      t.string :email, :name
+      t.text :body
+      t.integer :post_id
+    end
 
-  create_table :schema_info, :force => true do |t|
-    t.string :version
+    add_index :comments, :post_id
+
+    create_table :schema_info, :force => true do |t|
+      t.string :version
+    end
+
   end
 
 end
