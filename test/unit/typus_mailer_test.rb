7aad899ee4c7d1c87825ed1823c31d7a4e759ebd
Francesc Esplugas <fesplugas@intraducibles.net>
2008-09-19 23:38:36 +0200

    Moved old code to setup

--- test/unit/typus_mailer_test.rb
+++ test/unit/typus_mailer_test.rb
@@ -2,29 +2,30 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TypusMailerTest < ActiveSupport::TestCase
 
+  def setup
+    @user = typus_users(:admin)
+    @response = TypusMailer.deliver_password(@user, "12345678", "http://0.0.0.0:3000")
+  end
+
   def test_should_check_email_contains_application_name_on_subject_and_body
-    user = typus_users(:admin)
-    response = TypusMailer.deliver_password(user, "12345678", "http://0.0.0.0:3000")
-    assert_match /#{Typus::Configuration.options[:app_name]}/, response.subject
-    assert_match /#{Typus::Configuration.options[:app_name]}/, response.body
+    assert_match /#{Typus::Configuration.options[:app_name]}/, @response.subject
+    assert_match /#{Typus::Configuration.options[:app_name]}/, @response.body
   end
 
   def test_should_check_contents_of_email_password
-    user = typus_users(:admin)
-    response = TypusMailer.deliver_password(user, "12345678", "http://0.0.0.0:3000")
-    assert_match /You can login at/, response.body
+    assert_match /You can login at/, @response.body
   end
 
   def test_should_check_email_contains_user_full_name
-    user = typus_users(:admin)
-    response = TypusMailer.deliver_password(user, "12345678", "http://0.0.0.0:3000")
-    assert_match /#{user.full_name}/, response.body
+    assert_match /#{@user.full_name}/, @response.body
   end
 
   def test_should_check_email_contains_password
-    user = typus_users(:admin)
-    response = TypusMailer.deliver_password(user, "12345678", "http://0.0.0.0:3000")
-    assert_match /12345678/, response.body
+    assert_match /12345678/, @response.body
+  end
+
+  def test_should_check_email_contains_signature
+    assert_match /--\n#{Typus::Configuration.options[:app_name]}/, @response.body
   end
 
 end
