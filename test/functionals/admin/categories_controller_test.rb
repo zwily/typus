3325610604e10fdc40f972b6821349d6708ae36d
Francesc Esplugas <fesplugas@intraducibles.net>
2008-09-19 23:31:29 +0200

    Swaped params

--- test/functionals/admin/categories_controller_test.rb
+++ test/functionals/admin/categories_controller_test.rb
@@ -13,50 +13,50 @@ class Admin::CategoriesControllerTest < ActionController::TestCase
 
   def test_should_position_item_one_step_down
     first_category = categories(:first)
-    assert_equal first_category.position, 1
+    assert_equal 1, first_category.position
     second_category = categories(:second)
-    assert_equal second_category.position, 2
+    assert_equal 2, second_category.position
     get :position, { :id => first_category.id, :go => 'move_lower' }
     assert flash[:success]
     assert_match /Record moved lower./, flash[:success]
     first_category = Category.find(1)
-    assert_equal first_category.position, 2
+    assert_equal 2, first_category.position
     second_category = Category.find(2)
-    assert_equal second_category.position, 1
+    assert_equal 1, second_category.position
   end
 
   def test_should_position_item_one_step_up
     first_category = categories(:first)
-    assert_equal first_category.position, 1
+    assert_equal 1, first_category.position
     second_category = categories(:second)
-    assert_equal second_category.position, 2
+    assert_equal 2, second_category.position
     get :position, { :id => second_category.id, :go => 'move_higher' }
     assert flash[:success]
     assert_match /Record moved higher./, flash[:success]
     first_category = Category.find(1)
-    assert_equal first_category.position, 2
+    assert_equal 2, first_category.position
     second_category = Category.find(2)
-    assert_equal second_category.position, 1
+    assert_equal 1, second_category.position
   end
 
   def test_should_position_top_item_to_bottom
     first_category = categories(:first)
-    assert_equal first_category.position, 1
+    assert_equal 1, first_category.position
     get :position, { :id => first_category.id, :go => 'move_to_bottom' }
     assert flash[:success]
     assert_match /Record moved to bottom./, flash[:success]
     first_category = Category.find(1)
-    assert_equal first_category.position, 3
+    assert_equal 3, first_category.position
   end
 
   def test_should_position_bottom_item_to_top
     third_category = categories(:third)
-    assert_equal third_category.position, 3
+    assert_equal 3, third_category.position
     get :position, { :id => third_category.id, :go => 'move_to_top' }
     assert flash[:success]
     assert_match /Record moved to top./, flash[:success]
     third_category = Category.find(3)
-    assert_equal third_category.position, 1
+    assert_equal 1, third_category.position
   end
 
   def test_should_allow_admin_to_add_a_category
