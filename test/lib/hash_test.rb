536462c9ed5b86c7e03173b92770f574f5ac60f6
Francesc Esplugas <fesplugas@intraducibles.net>
2008-10-21 18:28:56 +0200

    Fixed order of arguments

--- test/lib/hash_test.rb
+++ test/lib/hash_test.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class HashTest < Test::Unit::TestCase
 
   def test_compact
-    hsh = {'a' => '', 'b'=> nil, 'c' => 'hello'}
-    assert_equal hsh.compact, { 'c' => 'hello' }
+    hsh = { 'a' => '', 'b'=> nil, 'c' => 'hello' }
+    assert_equal ({ 'c' => 'hello' }), hsh.compact
   end
 
 end
