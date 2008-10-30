2542eff2b2647583cc891ffbdf432925cef07301
Francesc Esplugas <fesplugas@intraducibles.net>
2008-10-30 09:29:56 +0100

    Load plugin own controllers, helpers, views and models to isolate it from application when testing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,11 +11,20 @@ ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memo
 ActiveSupport::Dependencies.load_paths.delete(File.join(RAILS_ROOT, 'app/models'))
 
 ##
+# We want to have our own controllers and models to be able to 
+# test the plugin without touching the application.
+#
+%w( models controllers helpers ).each do |folder|
+  ActiveSupport::Dependencies.load_paths << File.join(RAILS_ROOT, 'vendor/plugins/typus/test/fixtures', folder)
+end
+
+##
 # Load only the plugin view_paths to be able to test extensions.
 #
 ActionController::Base.view_paths = []
-ActionController::Base.append_view_path(File.join(RAILS_ROOT, 'vendor/plugins/typus/app/views'))
-ActionController::Base.append_view_path(File.join(RAILS_ROOT, 'vendor/plugins/typus/test/fixtures/views'))
+%w( app/views test/fixtures/views ).each do |folder|
+  ActionController::Base.append_view_path(File.join(RAILS_ROOT, 'vendor/plugins/typus', folder))
+end
 
 require 'test/unit'
 require 'action_controller/test_process'
