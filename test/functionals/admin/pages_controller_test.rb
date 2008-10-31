e8a2fb8467b9cd981a814e7ef8aa70ea99ccd429
Francesc Esplugas <fesplugas@intraducibles.net>
2008-10-31 09:35:11 +0100

    Pages testing.

---
+++ test/functionals/admin/pages_controller_test.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+##
+# Here we test habtm, hm, polimorphism, relate and unrelate.
+#
+class Admin::CategoriesControllerTest < ActionController::TestCase
+
+  def setup
+
+  end
+
+  def test_should_verify_me
+    assert true
+  end
+
+end
