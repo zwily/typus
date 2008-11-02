aa69db8ecc34d08c48f1f67255744608d7f2b294
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-02 21:07:19 +0100

    Routes test file

---
+++ test/lib/routes_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class RoutesTest < Test::Unit::TestCase
+
+  def test_should_verify_typus_routes
+    assert true
+  end
+
+  def test_should_verify_admin_routes_for_a_model
+    assert true
+  end
+
+end
