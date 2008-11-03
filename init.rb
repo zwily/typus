17f8b1516937c05b86cd4ccde71048dd0c451de4
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-03 03:58:28 +0100

    Require sha1

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'app',
   ActiveSupport::Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', m)
 end
 
+require 'sha1'
 require 'typus'
 
 begin
