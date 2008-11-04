d61153a328f105d254783bef42520c8dd36f38cb
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-04 22:42:21 +0100

    Removed space ...

--- lib/typus/authentication.rb
+++ lib/typus/authentication.rb
@@ -1,54 +1,54 @@
 module Authentication
 
-  protected
+protected
 
-    ##
-    # Require require login checks if the user is logged on Typus, 
-    # otherwhise is sent to the login page with a :back_to param to 
-    # return where she tried to go.
-    #
-    def require_login
-      unless session[:typus]
-        back_to = (request.env['REQUEST_URI'] == '/admin') ? nil : request.env['REQUEST_URI']
-        redirect_to typus_login_url(:back_to => back_to)
-      end
+  ##
+  # Require require login checks if the user is logged on Typus, 
+  # otherwhise is sent to the login page with a :back_to param to 
+  # return where she tried to go.
+  #
+  def require_login
+    unless session[:typus]
+      back_to = (request.env['REQUEST_URI'] == '/admin') ? nil : request.env['REQUEST_URI']
+      redirect_to typus_login_url(:back_to => back_to)
     end
+  end
 
-    ##
-    # Check if the user is logged on the system.
-    #
-    def logged_in?
-      return false unless session[:typus]
-      begin
-        @current_user ||= TypusUser.find(session[:typus])
-      rescue ActiveRecord::RecordNotFound
-        session[:typus] = nil
-      end
+  ##
+  # Check if the user is logged on the system.
+  #
+  def logged_in?
+    return false unless session[:typus]
+    begin
+      @current_user ||= TypusUser.find(session[:typus])
+    rescue ActiveRecord::RecordNotFound
+      session[:typus] = nil
     end
+  end
 
-    ##
-    # Return the current user
-    #
-    def current_user
-      @current_user if logged_in?
-      unless Typus::Configuration.roles.keys.include? @current_user.roles
-        session[:typus] = nil
-        flash[:error] = "#{@current_user.roles.capitalize} role doesn't exist on the system."
-        redirect_to typus_login_url
-      end
+  ##
+  # Return the current user
+  #
+  def current_user
+    @current_user if logged_in?
+    unless Typus::Configuration.roles.keys.include? @current_user.roles
+      session[:typus] = nil
+      flash[:error] = "#{@current_user.roles.capitalize} role doesn't exist on the system."
+      redirect_to typus_login_url
     end
+  end
 
-    ##
-    # Password generation using numbers and letters.
-    #
-    def generate_password(length = 8)
-      chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
-      if Typus::Configuration.options[:special_characters_on_password]
-        chars += %w( @ # $ % & / ? ! * . , ; : _ - )
-      end
-      password = ""
-      1.upto(length) { |i| password << chars[rand(chars.size - 1)] }
-      return password
+  ##
+  # Password generation using numbers and letters.
+  #
+  def generate_password(length = 8)
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    if Typus::Configuration.options[:special_characters_on_password]
+      chars += %w( @ # $ % & / ? ! * . , ; : _ - )
     end
+    password = ""
+    1.upto(length) { |i| password << chars[rand(chars.size - 1)] }
+    return password
+  end
 
 end
