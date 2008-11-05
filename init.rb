9fcfad8750a037e477a0d6ff10a8681b91455d4f
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-05 12:25:26 +0100

    Updated how typus says FasterCSV is not installed.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 
 ActionController::Base.append_view_path(File.join(File.dirname(__FILE__), 'app', 'views'))
 
-%w( models controllers helpers ).each do |m|
-  ActiveSupport::Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', m)
+%w( models controllers helpers ).each do |folder|
+  ActiveSupport::Dependencies.load_paths << File.join(File.dirname(__FILE__), 'app', folder)
 end
 
 require 'sha1'
@@ -11,7 +11,7 @@ require 'typus'
 begin
   require 'fastercsv'
 rescue LoadError
-  puts "[typus] FasterCSV gem not installed. CSV export from Typus won't work."
+  puts "=> [typus] FasterCSV gem not installed. CSV export from Typus won't work."
 end
 
 ##
