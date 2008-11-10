40deaec3db9d0943ac9fc2116299f3a89e9c471a
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-10 09:48:14 +0100

    Updated typus_plugin_generator

--- generators/typus_plugin/typus_plugin_generator.rb
+++ generators/typus_plugin/typus_plugin_generator.rb
@@ -2,21 +2,17 @@ class TypusPluginGenerator < Rails::Generator::NamedBase
 
   def manifest
     record do |m|
-
       # Lib
       m.directory "vendor/plugins/#{file_name}/lib/#{file_name}"
       m.file "lib/routes.rb", "vendor/plugins/#{file_name}/lib/#{file_name}/routes.rb"
-
       # Files
       m.directory "vendor/plugins/#{file_name}/config"
-      m.file "config/typus.yml.tpl", "vendor/plugins/#{file_name}/config/typus.yml.template"
-      m.file "config/typus_roles.yml.template", "vendor/plugins/#{file_name}/config/typus_roles.yml.template"
-
+      m.file "config/typus.yml.template", "vendor/plugins/#{file_name}/config/typus.yml"
+      m.file "config/typus_roles.yml.template", "vendor/plugins/#{file_name}/config/typus_roles.yml"
     end
-
   end
 
-protected
+  protected
 
   def banner
     "Usage: #{$0} #{spec.name} PluginName"
