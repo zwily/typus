c43949724d69fadcf87c712330ce76aad5873dce
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-11 14:30:15 +0100

    Moved code to helpers ...

---
+++ app/helpers/typus_form_helper.rb
@@ -0,0 +1,138 @@
+module TypusFormHelper
+
+  def build_form(fields = @item_fields)
+    returning(String.new) do |html|
+      html << "#{error_messages_for :item, :header_tag => "h3"}"
+      html << "<ul>"
+      fields.each do |field|
+        case field.last
+        when "string":          html << typus_string_field(field.first, field.last)
+        when "boolean":         html << typus_boolean_field(field.first, field.last)
+        when "datetime":        html << typus_datetime_field(field.first, field.last)
+        when "text":            html << typus_text_field(field.first, field.last)
+        when "file":            html << typus_file_field(field.first, field.last)
+        when "password":        html << typus_password_field(field.first, field.last)
+        when "selector":        html << typus_selector_field(field.first, field.last)
+        when "collection":      html << typus_collection_field(field.first, field.last)
+        when "tree":            html << typus_tree_field(field.first, field.last)
+        else
+          html << typus_string_field(field.first, field.last)
+        end
+      end
+      html << "</ul>"
+    end
+  end
+
+  def typus_tree_field(attribute, value)
+    returning(String.new) do |html|
+      html << <<-HTML
+<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}</label>
+    <select id="item_#{attribute}" name="item[#{attribute}]">
+      <option value=""></option>
+      #{expand_tree_into_select_field(@item.class.top)}
+    </select></li>
+      HTML
+    end
+  end
+
+  def typus_datetime_field(attribute, value)
+    returning(String.new) do |html|
+      html << "<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}</label>"
+      html << "#{datetime_select :item, attribute, { :minute_step => Typus::Configuration.options[:minute_step] }}</li>"
+    end
+  end
+
+  def typus_text_field(attribute, value)
+    returning(String.new) do |html|
+      html << "<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}</label>"
+      html << "#{text_area :item, attribute, :class => 'text', :rows => Typus::Configuration.options[:form_rows]}</li>"
+    end
+  end
+
+  def typus_selector_field(attribute, value)
+    returning(String.new) do |html|
+      options = ""
+      @item.class.send(attribute).each do |option|
+        options << "<option #{'selected' if @item.send(attribute).to_s == option.to_s} value=\"#{option}\">#{option}</option>"
+      end
+      html << <<-HTML
+<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}</label>
+<select id="item_#{attribute}" name="item[#{attribute}]">
+  <option value="">Select a #{attribute}</option>
+  #{options}
+</select></li>
+      HTML
+    end
+  end
+
+  def typus_collection_field(attribute, value)
+    returning(String.new) do |html|
+      related = attribute.split("_id").first.capitalize.camelize.constantize
+      html << <<-HTML
+<li><label for="item_#{attribute}">#{attribute.titleize.capitalize} <small>#{link_to "Add new", "/admin/#{attribute.titleize.tableize}/new?back_to=#{request.env['REQUEST_URI']}" }</small></label>
+#{select :item, attribute, related.find(:all).collect { |p| [p.typus_name, p.id] }.sort_by { |e| e.first }, :prompt => "Select a #{related.name.downcase}"}</li>
+      HTML
+    end
+  end
+
+  def typus_string_field(attribute, value)
+
+    # Read only fields.
+    if @model.typus_field_options_for(:read_only).include?(attribute)
+      value = 'read_only' if %w( edit ).include?(params[:action])
+    end
+
+    # Auto generated fields.
+    if @model.typus_field_options_for(:auto_generated).include?(attribute)
+      value = 'auto_generated' if %w( new edit ).include?(params[:action])
+    end
+
+    comment = %w( read_only auto_generated ).include?(value) ? (value + " field").titleize : ""
+
+    returning(String.new) do |html|
+      html << <<-HTML
+<li><label for="item_#{attribute}">#{attribute.titleize.capitalize} <small>#{comment}</small></label>
+#{text_field :item, attribute, :class => 'text'}</li>
+      HTML
+    end
+
+  end
+
+  def typus_password_field(attribute, value)
+    returning(String.new) do |html|
+      html << "<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}</label>"
+      html << "#{password_field :item, attribute, :class => 'text'}"
+    end
+  end
+
+  def typus_boolean_field(attribute, value)
+    question = true if @model.typus_field_options_for(:questions).include?(attribute)
+    returning(String.new) do |html|
+      html << "<li><label for=\"item_#{attribute}\">#{attribute.titleize.capitalize}#{"?" if question}</label>"
+      html << "#{check_box :item, attribute} Checked if active</li>"
+    end
+  end
+
+  def typus_file_field(attribute, value)
+
+    attribute_display = attribute.split("_file_name").first
+    content_type = @item.send("#{attribute_display}_content_type")
+
+    returning(String.new) do |html|
+
+      html << "<li><label for=\"item_#{attribute}\">#{attribute_display.titleize.capitalize}</label>"
+
+      case content_type
+      when /image/
+        html << "#{link_to image_tag(@item.send(attribute_display).url(:thumb)), @item.send(attribute_display).url, :style => "border: 1px solid #D3D3D3;"}<br /><br />"
+      when /pdf|flv|quicktime/
+        html << "<p>No preview available. (#{content_type.split('/').last})</p>"
+      end
+
+      html << "#{file_field :item, attribute.split("_file_name").first}</li>"
+
+    end
+
+  end
+
+end
---
+++ app/helpers/typus_table_helper.rb
@@ -0,0 +1,96 @@
+module TypusTableHelper
+
+  def typus_table(model = @model, fields = 'list', items = @items)
+
+    html = "<table>"
+
+    ##
+    # Header of the table
+    #
+    html << "<tr>"
+    model.typus_fields_for(fields).each do |column|
+      order_by = column[0]
+      sort_order = (params[:sort_order] == "asc") ? "desc" : "asc"
+      html << "<th>#{link_to "<div class=\"#{sort_order}\">#{column[0].titleize.capitalize}</div>", { :params => params.merge( :order_by => order_by, :sort_order => sort_order) }}</th>"
+    end
+    html << "<th>&nbsp;</th>\n</tr>"
+
+    ##
+    # Body of the table
+    #
+    items.each do |item|
+
+      html << "<tr class=\"#{cycle('even', 'odd')}\" id=\"item_#{item.id}\">"
+
+      model.typus_fields_for(fields).each do |column|
+        case column[1]
+        when "boolean"
+          image = "#{image_tag(status = item.send(column[0])? "admin/status_true.gif" : "admin/status_false.gif")}"
+          if Typus::Configuration.options[:toggle]
+            html << "<td width=\"20px\" align=\"center\">#{link_to image, { :params => params.merge(:controller => "admin/#{model.name.tableize}", :action => 'toggle', :field => column[0], :id => item.id) } , :confirm => "Change #{column[0]}?"}</td>"
+          else
+            html << "<td width=\"20px\" align=\"center\">#{image}</td>"
+          end
+        when "datetime"
+          html << "<td>#{item.send(column[0]).to_s(:db)}</td>"
+        when "collection"
+          begin
+            html << "<td>#{link_to item.send(column[0].split("_id").first).typus_name, :controller => "admin/#{column[0].split("_id").first.pluralize}", :action => "edit", :id => item.send(column[0])}</td>"
+          rescue
+            html << "<td></td>"
+          end
+        when "tree"
+          html << "<td>#{item.parent.typus_name if item.parent}</td>"
+        when "position"
+          html_position = []
+          [["Up", "move_higher"], ["Down", "move_lower"]].each do |position|
+            html_position << "#{link_to position.first, :params => params.merge(:controller => "admin/#{model.name.tableize}", :action => 'position', :id => item.id, :go => position.last)}"
+          end
+          html << "<td>#{html_position.join("/")}</td>"
+        else # 'string', 'integer', 'selector'
+          if model.typus_fields_for(fields).first == column
+            html << "<td>#{link_to item.send(column[0]) || "", :params => params.merge(:controller => "admin/#{model.name.tableize}", :action => 'edit', :id => item.id)}"
+            html << "<br /><small>#{"Custom actions go here, but only if exist." if Typus::Configuration.options[:actions_on_table]}</small></td>"
+          else
+            html << "<td>#{item.send(column[0])}</td>"
+          end
+        end
+      end
+
+      ##
+      # This controls the action to perform. If we are on a model list we 
+      # will remove the entry, but if we inside a model we will remove the 
+      # relationship between the models.
+      #
+      # Only shown is the user can destroy items.
+      #
+
+      if @current_user.can_perform?(model, 'delete')
+
+        case params[:action]
+        when 'index'
+          @perform = link_to image_tag("admin/trash.gif"), { :controller => "admin/#{model.name.tableize}", 
+                                                             :action => 'destroy', 
+                                                             :id => item.id }, 
+                                                             :confirm => "Remove entry?", 
+                                                             :method => :delete
+        else
+          @perform = link_to image_tag("admin/trash.gif"), { :controller => "admin/#{model.name.tableize}", 
+                                                             :action => "unrelate", 
+                                                             :id => item.id, 
+                                                             :model => @model, 
+                                                             :model_id => params[:id] }, 
+                                                             :confirm => "Remove #{model.humanize.singularize.downcase} \"#{item.typus_name}\" from #{@model.name}?"
+        end
+
+      end
+
+      html << "<td width=\"10px\">#{@perform}</td>\n</tr>"
+
+    end
+
+    html << "</table>"
+
+  end
+
+end
