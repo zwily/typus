a58c84f67fb79fbe37b043ef2495fae63fad4fc5
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-17 01:59:53 +0100

    Added should so they appear on the specs

--- test/lib/hash_test.rb
+++ test/lib/hash_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class HashTest < Test::Unit::TestCase
 
-  def test_compact
+  def test_should_verify_compact
     hsh = { 'a' => '', 'b'=> nil, 'c' => 'hello' }
     assert_equal ({ 'c' => 'hello' }), hsh.compact
   end
--- test/lib/object_test.rb
+++ test/lib/object_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ObjectTest < Test::Unit::TestCase
 
-  def test_try_class_method
+  def test_should_try_class_method
     assert_equal 'plugin', Post.try('typus')
   end
 
