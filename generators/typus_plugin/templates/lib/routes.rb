ef68242e7848f61635af30f4dfeac7118bd093b5
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-18 14:27:30 +0100

    Removed typus_plugin generator

--- generators/typus_plugin/templates/lib/routes.rb
+++
@@ -1,9 +0,0 @@
-class << ActionController::Routing::Routes;self;end.class_eval do
-  define_method :clear!, lambda {}
-end
-
-ActionController::Routing::Routes.draw do |map|
-
-  # Resources here
-
-end
