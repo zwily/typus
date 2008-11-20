2a35216c512d0d9b5656257f5890a381186b97b3
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-20 09:08:10 +0100

    Hidden field

--- app/helpers/admin_table_helper.rb
+++ app/helpers/admin_table_helper.rb
@@ -18,7 +18,12 @@ module AdminTableHelper
       if model.model_fields.map(&:first).include?(field)
         html << "<th>#{link_to "<div class=\"#{sort_order}\">#{field.titleize.capitalize}</div>", { :params => params.merge( :order_by => order_by, :sort_order => sort_order) }}</th>"
       else
-        html << "<th>#{field.titleize.capitalize}</th>"
+        case field
+        when "hidden"
+          html << "<th></th>"
+        else
+          html << "<th>#{field.titleize.capitalize}</th>"
+        end
       end
     end
     html << "<th>&nbsp;</th>\n</tr>"
