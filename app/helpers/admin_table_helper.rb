607c93b3472cc2470e440588c363d997eb481b91
Francesc Esplugas <fesplugas@intraducibles.net>
2008-11-22 12:09:44 +0100

    Refactored

--- app/helpers/admin_table_helper.rb
+++ app/helpers/admin_table_helper.rb
@@ -3,34 +3,6 @@ module AdminTableHelper
   ##
   # All helpers related to table.
   #
-
-=begin
-
-  def build_form(fields = @item_fields)
-    returning(String.new) do |html|
-      html << "#{error_messages_for :item, :header_tag => "h3"}"
-      html << "<ul>"
-      fields.each do |field|
-        case field.last
-        when "boolean":         html << typus_boolean_field(field.first, field.last)
-        when "datetime":        html << typus_datetime_field(field.first, field.last)
-        when "date":            html << typus_date_field(field.first, field.last)
-        when "text":            html << typus_text_field(field.first, field.last)
-        when "file":            html << typus_file_field(field.first, field.last)
-        when "password":        html << typus_password_field(field.first, field.last)
-        when "selector":        html << typus_selector_field(field.first, field.last)
-        when "collection":      html << typus_collection_field(field.first, field.last)
-        when "tree":            html << typus_tree_field(field.first, field.last)
-        else
-          html << typus_string_field(field.first, field.last)
-        end
-      end
-      html << "</ul>"
-    end
-  end
-
-=end
-
   def build_table(model = @model, fields = 'list', items = @items)
 
     returning(String.new) do |html|
@@ -59,29 +31,12 @@ module AdminTableHelper
         model.typus_fields_for(fields).each do |column|
           case column[1]
           when "boolean":           html << typus_table_boolean_field(item, column)
-          when "datetime", "date"
-            html << "<td>#{item.send(column[0]).to_s(:db)}</td>"
-          when "collection"
-            begin
-              html << "<td>#{link_to item.send(column[0].split("_id").first).typus_name, :controller => "admin/#{column[0].split("_id").first.pluralize}", :action => "edit", :id => item.send(column[0])}</td>"
-            rescue
-              html << "<td></td>"
-            end
-          when "tree"
-            html << "<td>#{item.parent.typus_name if item.parent}</td>"
-          when "position"
-            html_position = []
-            [["Up", "move_higher"], ["Down", "move_lower"]].each do |position|
-              html_position << "#{link_to position.first, :params => params.merge(:controller => "admin/#{model.name.tableize}", :action => 'position', :id => item.id, :go => position.last)}"
-            end
-            html << "<td>#{html_position.join("/")}</td>"
-          else # 'string', 'integer', 'selector'
-            if model.typus_fields_for(fields).first == column
-              html << "<td>#{link_to item.send(column[0]) || "", :controller => "admin/#{model.name.tableize}", :action => 'edit', :id => item.id}"
-              html << "<br /><small>#{"Custom actions go here, but only if exist." if Typus::Configuration.options[:actions_on_table]}</small></td>"
-            else
-              html << "<td>#{item.send(column[0])}</td>"
-            end
+          when "datetime", "date":  html << typus_table_datetime_field(item, column)
+          when "collection":        html << typus_table_collection_field(item, column)
+          when "tree":              html << typus_table_tree_field(item, column)
+          when "position":          html << typus_table_position_field(item, column)
+          else
+            html << typus_table_string_field(item, column, fields)
           end
         end
 
@@ -123,19 +78,59 @@ module AdminTableHelper
 
   end
 
+  def typus_table_collection_field(item, column)
+    return "<td>#{link_to item.send(column[0].split("_id").first).typus_name, :controller => "admin/#{column[0].split("_id").first.pluralize}", :action => "edit", :id => item.send(column[0])}</td>"
+  rescue
+    return "<td></td>"
+  end
+
+  def typus_table_string_field(item, column, fields)
+    returning(String.new) do |html|
+      if item.class.typus_fields_for(fields).first == column
+        html << "<td>#{link_to item.send(column[0]) || "", :controller => "admin/#{item.class.name.tableize}", :action => 'edit', :id => item.id}"
+        html << "<br /><small>#{"Custom actions go here, but only if exist." if Typus::Configuration.options[:actions_on_table]}</small></td>"
+      else
+        html << "<td>#{item.send(column[0])}</td>"
+      end
+    end
+  end
+
+  def typus_table_tree_field(item, column)
+    returning(String.new) do |html|
+      html << "<td>#{item.parent.typus_name if item.parent}</td>"
+    end
+  end
+
+  def typus_table_position_field(item, column)
+    returning(String.new) do |html|
+      html_position = []
+      [["Up", "move_higher"], ["Down", "move_lower"]].each do |position|
+        html_position << "#{link_to position.first, :params => params.merge(:controller => "admin/#{item.class.name.tableize}", :action => 'position', :id => item.id, :go => position.last)}"
+      end
+      html << "<td>#{html_position.join("/")}</td>"
+    end
+  end
+
+  def typus_table_datetime_field(item, column)
+    returning(String.new) do |html|
+      html << <<-HTML
+<td>#{item.send(column[0]).to_s(:db)}</td>
+      HTML
+    end
+  end
+
   def typus_table_boolean_field(item, column)
 
-    image = "#{image_tag(status = item.send(column[0])? "admin/status_true.gif" : "admin/status_false.gif")}"
+    image = "admin/status_#{item.send(column[0])}.gif"
 
     returning(String.new) do |html|
 
       if Typus::Configuration.options[:toggle]
         html << <<-HTML
 <td width="20px" align="center">
-  #{link_to image, {:params => params.merge(:controller => "admin/#{item.class.modelize}", :action => 'toggle', :field => column[0], :id => item.id)} , :confirm => "Change #{column[0]}?"}
+  #{link_to image_tag(image), {:params => params.merge(:controller => "admin/#{item.class.name.tableize}", :action => 'toggle', :field => column[0], :id => item.id)} , :confirm => "Change #{column[0]}?"}
 </td>
         HTML
-        #{link_to image, {:params => params.merge(:controller => "admin/#{model.name.tableize}", :action => 'toggle', :field => column[0], :id => item.id)} , :confirm => "Change #{column[0]}?"}
       else
         html << <<-HTML
 <td width="20px" align="center">#{image}</td>
