79635e38bcfcaed2de66e1dedd7a5d97544300bf
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-09 23:03:11 +0100

    Use single quotes

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -5,13 +5,13 @@ end
 ActionController::Routing::Routes.draw do |map|
 
   map.with_options :controller => 'typus', :path_prefix => Typus::Configuration.options[:prefix] do |i|
-    i.typus_dashboard "", :action => 'dashboard'
-    i.typus_overview "overview", :action => 'overview'
-    i.typus_login "login", :action => 'login'
-    i.typus_logout "logout", :action => 'logout'
-    i.typus_setup "setup", :action => 'setup'
-    i.typus_recover_password "recover_password", :action => 'recover_password'
-    i.typus_reset_password "reset_password", :action => 'reset_password'
+    i.typus_dashboard '', :action => 'dashboard'
+    i.typus_overview 'overview', :action => 'overview'
+    i.typus_login 'login', :action => 'login'
+    i.typus_logout 'logout', :action => 'logout'
+    i.typus_setup 'setup', :action => 'setup'
+    i.typus_recover_password 'recover_password', :action => 'recover_password'
+    i.typus_reset_password 'reset_password', :action => 'reset_password'
   end
 
   map.namespace :admin do |admin|
