c652e687190340b73feea15b0bd7ef0fe379a44c
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-11 17:17:33 +0100

    Fixed sort by primary_key

--- app/helpers/admin_table_helper.rb
+++ app/helpers/admin_table_helper.rb
@@ -73,9 +73,9 @@ module AdminTableHelper
     returning(String.new) do |html|
       html << "<tr>"
       model.typus_fields_for(fields).map(&:first).each do |field|
-        order_by = field
+        order_by = model.reflect_on_association(field.to_sym).primary_key_name rescue field
         sort_order = (params[:sort_order] == 'asc') ? 'desc' : 'asc'
-        if model.model_fields.map(&:first).include?(field) && params[:action] == 'index'
+        if model.model_fields.map(&:first).include?(field) || model.reflect_on_all_associations.map(&:name).include?(field.to_sym)
           html << "<th>#{link_to "<div class=\"#{sort_order}\">#{field.titleize.capitalize}</div>", { :params => params.merge(:order_by => order_by, :sort_order => sort_order) }}</th>"
         else
           html << "<th>#{field.titleize.capitalize}</th>"
