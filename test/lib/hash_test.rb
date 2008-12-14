46aaf36ceffc3448ac4f3722882813384054f8c9
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-14 18:13:15 +0100

    Clarity ...

--- test/lib/hash_test.rb
+++ test/lib/hash_test.rb
@@ -3,8 +3,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 class HashTest < Test::Unit::TestCase
 
   def test_should_verify_compact
-    hsh = { 'a' => '', 'b'=> nil, 'c' => 'hello' }
-    assert_equal ({ 'c' => 'hello' }), hsh.compact
+    hash = { 'a' => '', 'b'=> nil, 'c' => 'hello' }
+    hash_compacted = { 'c' => 'hello' }
+    assert_equal hash_compacted, hash.compact
   end
 
 end
