f9335bfa97fddbaf9a4dbf59d8c3ded2c2d2814d
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-16 15:13:57 +0100

    Removed code ...

--- test/fixtures/app/controllers/admin/posts_controller.rb
+++ test/fixtures/app/controllers/admin/posts_controller.rb
@@ -4,28 +4,4 @@
 ##
 class Admin::PostsController < AdminController
 
-=begin
-
-  ##
-  # You can overwrite any of the AdminController methods.
-  #
-  def index
-  end
-
-  ##
-  # You can extend the AdminController with your actions.
-  #
-  # This actions have to be defined in `typus.yml`.
-  #
-  #   Post:
-  #     actions:
-  #       list: action_for_the_listing
-  #       form: action_for_the_form
-  #
-  def your_action
-  end
-
-=end
-
 end
-
