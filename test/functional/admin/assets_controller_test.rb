496d91df2ae3c842453a34d79f2cd12d5d727af8
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-16 15:20:44 +0100

    assert_difference is your friend

--- test/functional/admin/assets_controller_test.rb
+++ test/functional/admin/assets_controller_test.rb
@@ -20,13 +20,13 @@ class Admin::AssetsControllerTest < ActionController::TestCase
   def test_should_create_a_polymorphic_relationship
 
     post_ = posts(:published)
-    assert_equal 2, post_.assets.size
 
-    post :create, { :back_to => "/admin/posts/#{post_.id}/edit", :resource => post_.class.name, :resource_id => post_.id }
+    assert_difference('post_.assets.count') do
+      post :create, { :back_to => "/admin/posts/#{post_.id}/edit", :resource => post_.class.name, :resource_id => post_.id }
+    end
+
     assert_response :redirect
     assert_redirected_to '/admin/posts/1/edit'
-
-    assert_equal 3, post_.assets.size
     assert flash[:success]
     assert_equal "Asset successfully assigned to Post.", flash[:success]
 
@@ -35,17 +35,17 @@ class Admin::AssetsControllerTest < ActionController::TestCase
   def test_should_unrelate_a_polymorphic_relationship
 
     post_ = posts(:published)
-    assert_equal 2, post_.assets.size
 
     @request.env["HTTP_REFERER"] = "/admin/posts/#{post_.id}/edit"
 
-    get :unrelate, { :id => post_.assets.first.id, :model => post_.class.name, :model_id => post_.id }
+    assert_difference('post_.assets.count', -1) do
+      get :unrelate, { :id => post_.assets.first.id, :model => post_.class.name, :model_id => post_.id }
+    end
+
     assert_response :redirect
     assert_redirected_to @request.env["HTTP_REFERER"]
     assert flash[:success]
     assert_match "Asset removed from Post.", flash[:success]
-
-    assert_equal 1, post_.assets.size
 
   end
 
