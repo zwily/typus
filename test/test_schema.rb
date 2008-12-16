3f085eec40df3ddac076aadc9f5cf80724640ca7
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-16 15:11:06 +0100

    Cleaned schema

--- test/test_schema.rb
+++ test/test_schema.rb
@@ -2,29 +2,12 @@ ActiveRecord::Migration.verbose = false
 
 ActiveRecord::Schema.define do
 
-  create_table :typus_users, :force => true do |t|
-    t.string :email, :first_name, :last_name
-    t.string :salt, :crypted_password
-    t.boolean :status, :default => false
-    t.string :roles
-    t.string :token
-    t.timestamps
+  create_table :assets, :force => true do |t|
+    t.string :caption
+    t.string :resource_type
+    t.integer :resource_id
+    t.integer :position
   end
-
-  create_table :users, :force => true do |t|
-    t.string :first_name, :last_name, :email
-  end
-
-  create_table :posts, :force => true do |t|
-    t.string :title
-    t.text :body
-    t.boolean :status
-    t.timestamps
-    t.datetime :published_at
-    t.integer :user_id
-  end
-
-  add_index :posts, :user_id
 
   create_table :categories, :force => true do |t|
     t.string :name
@@ -32,22 +15,6 @@ ActiveRecord::Schema.define do
     t.text :description
     t.integer :position
   end
-
-  create_table :categories_posts, :force => true, :id => false do |t|
-    t.column :category_id, :integer
-    t.column :post_id, :integer
-  end
-
-  add_index :categories_posts, :category_id
-  add_index :categories_posts, :post_id
-
-  create_table :posts_tags, :force => true, :id => false do |t|
-    t.column :post_id, :integer
-    t.column :tag_id, :integer
-  end
-
-  add_index :posts_tags, :post_id
-  add_index :posts_tags, :tag_id
 
   create_table :comments, :force => true do |t|
     t.string :email, :name
@@ -63,11 +30,36 @@ ActiveRecord::Schema.define do
     t.boolean :status
   end
 
-  create_table :assets, :force => true do |t|
-    t.string :caption
-    t.string :resource_type
-    t.integer :resource_id
-    t.integer :position
+  create_table :posts, :force => true do |t|
+    t.string :title
+    t.text :body
+    t.boolean :status
+    t.timestamps
+    t.datetime :published_at
+    t.integer :user_id
   end
+
+  add_index :posts, :user_id
+
+  create_table :typus_users, :force => true do |t|
+    t.string :email, :first_name, :last_name
+    t.string :salt, :crypted_password
+    t.boolean :status, :default => false
+    t.string :roles
+    t.string :token
+    t.timestamps
+  end
+
+  create_table :users, :force => true do |t|
+    t.string :first_name, :last_name, :email
+  end
+
+  create_table :categories_posts, :force => true, :id => false do |t|
+    t.column :category_id, :integer
+    t.column :post_id, :integer
+  end
+
+  add_index :categories_posts, :category_id
+  add_index :categories_posts, :post_id
 
 end
