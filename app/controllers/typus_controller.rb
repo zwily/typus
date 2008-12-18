a53133fe14dbca9b71a4f89f19776f9d1a342be1
Francesc Esplugas <fesplugas@intraducibles.net>
2008-12-18 10:11:05 +0100

    TypusController and views are now translatable ...

--- app/controllers/typus_controller.rb
+++ app/controllers/typus_controller.rb
@@ -19,7 +19,7 @@ class TypusController < ApplicationController
   # Application Dashboard
   #
   def dashboard
-    flash[:notice] = "There are not defined applications in config/typus/*.yml" if Typus.applications.empty?
+    flash[:notice] = t("There are not defined applications in config/typus/*.yml") if Typus.applications.empty?
   end
 
   ##
@@ -39,7 +39,7 @@ class TypusController < ApplicationController
           redirect_to typus_dashboard_url
         end
       else
-        flash[:error] = "The Email and/or Password you entered is invalid."
+        flash[:error] = t("The Email and/or Password you entered is invalid.")
         redirect_to typus_login_url
       end
     else
@@ -65,7 +65,7 @@ class TypusController < ApplicationController
       if typus_user
         ActionMailer::Base.default_url_options[:host] = request.host_with_port
         typus_user.reset_password
-        flash[:success] = "Password recovery link sent to your email."
+        flash[:success] = t("Password recovery link sent to your email.")
         redirect_to typus_login_url
       else
         redirect_to typus_recover_password_url
@@ -83,17 +83,17 @@ class TypusController < ApplicationController
     if request.post?
       typus_user = TypusUser.find_by_token(params[:user][:token])
       if typus_user.update_attributes(params[:user])
-        flash[:success] = "You can login with your new password."
+        flash[:success] = t("You can login with your new password.")
         redirect_to typus_login_url
       else
-        flash[:error] = "Passwords don't match."
+        flash[:error] = t("Passwords don't match.")
         redirect_to :action => 'reset_password', :token => params[:user][:token]
       end
     else
       if TypusUser.find_by_token(params[:token])
         render :layout => 'typus_login'
       else
-        render :text => "A valid token is required."
+        render :text => t("A valid token is required.")
       end
     end
   end
@@ -113,16 +113,16 @@ class TypusController < ApplicationController
 
       if @typus_user.save
         session[:typus] = @typus_user.id
-        flash[:notice] = "Your new password is <strong>%s</strong>." % password
+        flash[:notice] = "Your new password is %s." % password
         redirect_to :action => 'dashboard'
       else
-        flash[:error] = "<strong>Yay!</strong> That doesn't seem like a valid email address."
+        flash[:error] = t("Yay! That doesn't seem like a valid email address.")
         redirect_to :action => 'setup'
       end
 
     else
 
-      flash[:success] = "<strong>Welcome to Typus!</strong> Write your email to create a typus user."
+      flash[:success] = t("Welcome to Typus! Write your email to create a typus user.")
       render :layout => 'typus_login'
 
     end
