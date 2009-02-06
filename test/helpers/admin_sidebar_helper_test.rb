abb394268f9822f7e2827e5e02d5ea1c534460b4
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-06 12:42:26 +0100

    Test build my list.

--- test/helpers/admin_sidebar_helper_test.rb
+++ test/helpers/admin_sidebar_helper_test.rb
@@ -8,4 +8,20 @@ class AdminSidebarHelperTest < Test::Unit::TestCase
     assert true
   end
 
+  def test_build_my_list
+
+    output = build_my_list([], header = nil)
+    assert output.empty?
+
+    output = build_my_list(['item1', 'item2'], "Chunky Bacon")
+    assert !output.empty?
+    assert_match /Chunky bacon/, output
+
+    output = build_my_list(['item1', 'item2'])
+    assert !output.empty?
+    assert_no_match /h2/, output
+    assert_no_match /\/h2/, output
+
+  end
+
 end
