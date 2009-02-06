d4b59f510f70da4e46a5921ad82259cf69b50eae
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-06 12:06:59 +0100

    Initial work on testing helpers.

--- test/helpers/typus_helper_test.rb
+++ test/helpers/typus_helper_test.rb
@@ -5,7 +5,19 @@ class TypusHelperTest < Test::Unit::TestCase
   include TypusHelper
 
   def test_header
-    assert true
+    output = header
+    assert_match /#{Typus::Configuration.options[:app_name]}/, output
+  end
+
+  def test_typus_message
+    output = typus_message('chunky bacon', 'yay')
+    assert_match "<div id=\"flash\" class=\"yay\">", output
+    assert_match /<p>chunky bacon<\/p>/, output
+  end
+
+  def test_typus_block
+    output = typus_block(:model => 'posts', :location => 'sidebar', :partial => 'pum')
+    assert output.nil?
   end
 
 end
