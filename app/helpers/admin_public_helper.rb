304d2f41018e4fcbf18d61457873ae1f80e3ebfa
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-06 12:27:41 +0100

    Test and refactor of quick_edit.

--- app/helpers/admin_public_helper.rb
+++ app/helpers/admin_public_helper.rb
@@ -4,14 +4,22 @@ module AdminPublicHelper
   #
   #
   def quick_edit(*args)
+
     options = args.extract_options!
     options[:color] ||= '#000'
+    options[:link] ||= admin_quick_edit_path
+
+    query = options.dup
+    [ :color, :link ].each { |o| query.delete(o) }
+
     html = <<-HTML
 <script type="text/javascript">
-  document.write('<script type="text/javascript" src="#{admin_quick_edit_path}?#{options.to_query}" />');
+  document.write('<script type="text/javascript" src="#{options[:link]}?#{query.to_query}" />');
 </script>
     HTML
+
     return html
+
   end
 
 end
