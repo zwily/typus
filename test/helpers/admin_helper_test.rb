f45fb566fbc0fb3ff7e658c789b6c8ca29c56005
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-10 12:00:17 +0100

    Sorted tests.

--- test/helpers/admin_helper_test.rb
+++ test/helpers/admin_helper_test.rb
@@ -4,13 +4,13 @@ class AdminHelperTest < ActiveSupport::TestCase
 
   include AdminHelper
 
+  def test_display_link_to_previous
+    assert true
+  end
+
   def test_remove_filter_link
     output = remove_filter_link('')
     assert output.nil?
-  end
-
-  def test_display_link_to_previous
-    assert true
   end
 
   def test_build_list
