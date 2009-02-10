21d9dc33c1c9816f3b3802978607bd4f2d589de2
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-10 11:59:23 +0100

    Placeholder for tests.

--- test/helpers/typus_helper_test.rb
+++ test/helpers/typus_helper_test.rb
@@ -4,15 +4,12 @@ class TypusHelperTest < ActiveSupport::TestCase
 
   include TypusHelper
 
-  def test_header
-    output = header
-    assert_match /#{Typus::Configuration.options[:app_name]}/, output
+  def test_applications
+    assert true
   end
 
-  def test_typus_message
-    output = typus_message('chunky bacon', 'yay')
-    assert_match "<div id=\"flash\" class=\"yay\">", output
-    assert_match /<p>chunky bacon<\/p>/, output
+  def test_resources
+    assert true
   end
 
   def test_typus_block
@@ -25,6 +22,25 @@ class TypusHelperTest < ActiveSupport::TestCase
     Typus::Configuration.options[:app_name] = 'whatistypus.com'
     output = page_title('custom_action')
     assert_equal "whatistypus.com &rsaquo; Custom action", output
+  end
+
+  def test_header
+    output = header
+    assert_match /#{Typus::Configuration.options[:app_name]}/, output
+  end
+
+  def test_login_info
+    assert true
+  end
+
+  def test_display_flash_message
+    assert true
+  end
+
+  def test_typus_message
+    output = typus_message('chunky bacon', 'yay')
+    assert_match "<div id=\"flash\" class=\"yay\">", output
+    assert_match /<p>chunky bacon<\/p>/, output
   end
 
 end
