6d624c007d177bd4a8801a0c3782302865aa9b6f
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-10 12:05:20 +0100

    Placeholder for tests.

--- test/helpers/admin_sidebar_helper_test.rb
+++ test/helpers/admin_sidebar_helper_test.rb
@@ -4,6 +4,18 @@ class AdminSidebarHelperTest < ActiveSupport::TestCase
 
   include AdminSidebarHelper
 
+  def test_actions
+    assert true
+  end
+
+  def test_default_actions
+    assert true
+  end
+
+  def test_non_crud_actions
+    assert true
+  end
+
   def test_build_typus_list
 
     output = build_typus_list([], header = nil)
@@ -18,6 +30,38 @@ class AdminSidebarHelperTest < ActiveSupport::TestCase
     assert_no_match /h2/, output
     assert_no_match /\/h2/, output
 
+  end
+
+  def test_modules
+    assert true
+  end
+
+  def test_previous_and_next
+    assert true
+  end
+
+  def test_search
+    assert true
+  end
+
+  def test_filters
+    assert true
+  end
+
+  def test_relationship_filter
+    assert true
+  end
+
+  def test_datetime_filter
+    assert true
+  end
+
+  def test_boolean_filter
+    assert true
+  end
+
+  def test_string_filter
+    assert true
   end
 
 end
--- test/helpers/admin_table_helper_test.rb
+++ test/helpers/admin_table_helper_test.rb
@@ -4,7 +4,39 @@ class AdminTableHelperTest < ActiveSupport::TestCase
 
   include AdminTableHelper
 
-  def test_default
+  def test_build_typus_table
+    assert true
+  end
+
+  def test_typus_table_header
+    assert true
+  end
+
+  def test_typus_table_belongs_to_field
+    assert true
+  end
+
+  def test_typus_table_has_and_belongs_to_many_field
+    assert true
+  end
+
+  def test_typus_table_string_field
+    assert true
+  end
+
+  def test_typus_table_tree_field
+    assert true
+  end
+
+  def test_typus_table_position_field
+    assert true
+  end
+
+  def test_typus_table_datetime_field
+    assert true
+  end
+
+  def test_typus_table_boolean_field
     assert true
   end
 
