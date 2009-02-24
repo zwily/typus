28bded4f90f417412d21fa90401d0797eb4efac8
Francesc Esplugas <fesplugas@intraducibles.net>
2009-02-24 17:05:35 +0100

    Removed unused code.

--- lib/typus/user.rb
+++ lib/typus/user.rb
@@ -106,7 +106,6 @@ module Typus
       end
 
       def set_token
-        record = "#{self.class.name}#{id}"
         chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
         @attributes['token'] = chars.sort_by{rand}.to_s[0..12]
       end
