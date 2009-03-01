2779cd7ea840529bb3737e132a4a0b0d5b8f692f
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-01 14:13:11 +0100

    Placeholder for admin_form_helper tests.

--- test/helpers/admin_form_helper_test.rb
+++ test/helpers/admin_form_helper_test.rb
@@ -4,4 +4,76 @@ class AdminFormHelperTest < ActiveSupport::TestCase
 
   include AdminFormHelper
 
+  def test_build_form
+    assert true
+  end
+
+  def test_typus_belongs_to_field
+    assert true
+  end
+
+  def test_typus_boolean_field
+    assert true
+  end
+
+  def test_typus_date_field
+    assert true
+  end
+
+  def test_typus_datetime_field
+    assert true
+  end
+
+  def test_typus_file_field
+    assert true
+  end
+
+  def test_typus_password_field
+    assert true
+  end
+
+  def test_typus_selector_field
+    assert true
+  end
+
+  def test_typus_text_field
+    assert true
+  end
+
+  def test_typus_time_field
+    assert true
+  end
+
+  def test_typus_tree_field
+    assert true
+  end
+
+  def test_typus_string_field
+    assert true
+  end
+
+  def test_typus_relationships
+    assert true
+  end
+
+  def test_typus_form_has_many
+    assert true
+  end
+
+  def test_typus_form_has_and_belongs_to_many
+    assert true
+  end
+
+  def test_typus_template_field
+    assert true
+  end
+
+  def test_attribute_disabled
+    assert true
+  end
+
+  def test_expand_tree_into_select_field
+    assert true
+  end
+
 end
