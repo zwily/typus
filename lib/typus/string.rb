f9eebf3d9facd12a56124042f93ed153d5c6421d
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-06 09:42:21 +0100

    Removed old stuff.

--- lib/typus/string.rb
+++ lib/typus/string.rb
@@ -1,11 +1,7 @@
 class String
 
   def typus_actions_for(filter)
-    if self.respond_to?("admin_actions_for_#{filter}")
-      self.send("admin_actions_for_#{filter}").map { |a| a.to_s }
-    else
-      Typus::Configuration.config[self]['actions'][filter.to_s].split(', ') rescue []
-    end
+    Typus::Configuration.config[self]['actions'][filter.to_s].split(', ') rescue []
   end
 
   def typus_defaults_for(filter)
