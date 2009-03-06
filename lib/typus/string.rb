22023b40c44f56b353caafd84820aa698b106cc1
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-06 09:42:39 +0100

    Mark things to optimize.

--- lib/typus/string.rb
+++ lib/typus/string.rb
@@ -1,10 +1,12 @@
 class String
 
   def typus_actions_for(filter)
+    # OPTIMIZE
     Typus::Configuration.config[self]['actions'][filter.to_s].split(', ') rescue []
   end
 
   def typus_defaults_for(filter)
+    # OPTIMIZE
     Typus::Configuration.config[self][filter.to_s].split(', ') rescue []
   end
 
