58be40cf2e3eb3d1ce6bd26e93e8ad869dcb0e0b
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-16 23:04:41 +0100

    Test for build_list when returning a table and a template.

--- test/helpers/admin_helper_test.rb
+++ test/helpers/admin_helper_test.rb
@@ -24,8 +24,42 @@ class AdminHelperTest < ActiveSupport::TestCase
     assert output.nil?
   end
 
-  def test_build_list
-    assert true
+  def test_build_list_when_returns_a_typus_table
+
+    model = TypusUser
+    fields = [ 'email', 'roles', 'status' ]
+    items = TypusUser.find(:all)
+    resource = 'typus_users'
+
+    def build_typus_table(model, fields, items, link_options)
+      return 'a_list_with_items'
+    end
+
+    output = build_list(model, fields, items, resource)
+    expected = "a_list_with_items"
+
+    assert_equal expected, output
+
+  end
+
+  def test_build_list_when_returns_a_template
+
+    model = TypusUser
+    fields = [ 'email', 'roles', 'status' ]
+    items = TypusUser.find(:all)
+    resource = 'typus_users'
+
+    def render(*args)
+      return 'a_template'
+    end
+
+    File.stubs(:exists?).returns(true)
+
+    output = build_list(model, fields, items, resource)
+    expected = 'a_template'
+
+    assert_equal expected, output
+
   end
 
   def test_build_pagination
