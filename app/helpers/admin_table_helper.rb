e7b4c621e6e69b68cc7f39fe796859a53789ee18
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-16 13:11:26 +0100

    Compatibility for Ruby 1.8.6-p287 and Ruby 1.8.7-p72.

--- app/helpers/admin_table_helper.rb
+++ app/helpers/admin_table_helper.rb
@@ -22,16 +22,16 @@ module AdminTableHelper
         fields.each do |key, value|
           case value
           when :boolean:           html << typus_table_boolean_field(key, item)
-          when :datetime:          html << typus_table_datetime_field(key, item, fields.first.first, link_options)
-          when :date:              html << typus_table_datetime_field(key, item, fields.first.first, link_options)
-          when :time:              html << typus_table_datetime_field(key, item, fields.first.first, link_options)
+          when :datetime:          html << typus_table_datetime_field(key, item, fields.keys.first, link_options)
+          when :date:              html << typus_table_datetime_field(key, item, fields.keys.first, link_options)
+          when :time:              html << typus_table_datetime_field(key, item, fields.keys.first, link_options)
           when :belongs_to:        html << typus_table_belongs_to_field(key, item)
           when :tree:              html << typus_table_tree_field(key, item)
           when :position:          html << typus_table_position_field(key, item)
           when :has_and_belongs_to_many:
             html << typus_table_has_and_belongs_to_many_field(key, item)
           else
-            html << typus_table_string_field(key, item, fields.first.first, link_options)
+            html << typus_table_string_field(key, item, fields.keys.first, link_options)
           end
         end
 
