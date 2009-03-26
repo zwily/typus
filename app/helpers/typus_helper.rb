ab956e1bc8cb43bd17676348cffbb340db8d0df2
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-26 13:47:16 +0100

    Bug fix.

--- app/helpers/typus_helper.rb
+++ app/helpers/typus_helper.rb
@@ -25,7 +25,7 @@ module TypusHelper
           description = Typus.module_description(model)
           html << <<-HTML
 <tr class="#{cycle('even', 'odd')}">
-<td>#{link_to I18n.t(model.humanize.pluralize, :default => model.humanize.pluralize), send("admin_#{model.tableize}_path")}<br /><small>#{I18n.t(description, :default => "#{description}")}</small></td>
+<td>#{link_to I18n.t(model.titleize.capitalize.pluralize, :default => model.titleize.capitalize.pluralize), send("admin_#{model.tableize}_path")}<br /><small>#{I18n.t(description, :default => "#{description}")}</small></td>
 <td class="right"><small>
 #{link_to I18n.t("Add", :default => "Add"), send("new_admin_#{model.tableize.singularize}_path") if @current_user.can_perform?(model, 'create')}
 </small></td>
