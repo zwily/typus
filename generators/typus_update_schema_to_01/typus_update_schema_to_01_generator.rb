4582f7af0890305c30a53ea83e70bc586d386f88
Francesc Esplugas <fesplugas@intraducibles.net>
2009-03-30 12:53:51 +0200

    Generate migrations at the end of the generator.

--- generators/typus_update_schema_to_01/typus_update_schema_to_01_generator.rb
+++ generators/typus_update_schema_to_01/typus_update_schema_to_01_generator.rb
@@ -4,13 +4,13 @@ class TypusUpdateSchemaTo01Generator < Rails::Generator::Base
 
     record do |m|
 
-      m.migration_template 'migration.rb', 'db/migrate', { :migration_file_name => 'update_typus_schema_to_01' }
-
       config_folder = Typus::Configuration.options[:config_folder]
       Dir["#{Typus.root}/generators/typus_update_schema_to_01/templates/config/*"].each do |f|
         base = File.basename(f)
         m.template "config/#{base}", "#{config_folder}/#{base}"
       end
+
+      m.migration_template 'migration.rb', 'db/migrate', { :migration_file_name => 'update_typus_schema_to_01' }
 
     end
 
