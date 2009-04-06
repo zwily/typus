43462e6632b38f67216364f795c5a962a37e5a4d
Francesc Esplugas <fesplugas@intraducibles.net>
2009-04-06 03:24:41 +0200

    Refactored routes with only one path_prefix.

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -4,22 +4,20 @@ end
 
 ActionController::Routing::Routes.draw do |map|
 
-  path_prefix = Typus::Configuration.options[:path_prefix]
+  map.with_options :path_prefix => Typus::Configuration.options[:path_prefix] do |typus|
 
-  map.with_options :controller => 'typus', :path_prefix => path_prefix do |i|
-    i.admin_quick_edit 'quick_edit', :action => 'quick_edit'
-    i.admin_dashboard '', :action => 'dashboard'
-    i.admin_sign_in 'sign_in', :action => 'sign_in'
-    i.admin_sign_out 'sign_out', :action => 'sign_out'
-    i.admin_sign_up 'sign_up', :action => 'sign_up'
-    i.admin_recover_password 'recover_password', :action => 'recover_password'
-    i.admin_reset_password 'reset_password', :action => 'reset_password'
-    i.admin_set_locale 'set_locale', :action => 'set_locale'
-  end
+    typus.with_options :controller => 'typus' do |i|
+      i.admin_quick_edit 'quick_edit', :action => 'quick_edit'
+      i.admin_dashboard '', :action => 'dashboard'
+      i.admin_sign_in 'sign_in', :action => 'sign_in'
+      i.admin_sign_out 'sign_out', :action => 'sign_out'
+      i.admin_sign_up 'sign_up', :action => 'sign_up'
+      i.admin_recover_password 'recover_password', :action => 'recover_password'
+      i.admin_reset_password 'reset_password', :action => 'reset_password'
+      i.admin_set_locale 'set_locale', :action => 'set_locale'
+    end
 
-  map.namespace :admin do |admin|
-
-    admin.with_options :path_prefix => path_prefix do |i|
+    typus.namespace :admin do |i|
 
       Typus.resources.each do |resource|
         i.connect "#{resource.underscore}/:action", :controller => resource.underscore
