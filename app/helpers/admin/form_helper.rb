51849a9d5888a494a3e3a1164a314411984ccc24
Francesc Esplugas <fesplugas@intraducibles.net>
2009-04-30 14:21:25 +0200

    Updated link message. I understand that when adding something, I'm adding something new.

--- app/helpers/admin/form_helper.rb
+++ app/helpers/admin/form_helper.rb
@@ -57,7 +57,7 @@ module Admin::FormHelper
       else
         html << <<-HTML
 <li><label for="item_#{attribute}">#{_(related_fk.humanize)}
-    <small>#{link_to _('Add new'), { :controller => "admin/#{related.class_name.tableize}", :action => 'new', :back_to => back_to, :selected => related_fk }, :confirm => message.join("\n\n") if @current_user.can_perform?(related, 'create')}</small>
+    <small>#{link_to _('Add'), { :controller => "admin/#{related.class_name.tableize}", :action => 'new', :back_to => back_to, :selected => related_fk }, :confirm => message.join("\n\n") if @current_user.can_perform?(related, 'create')}</small>
     </label>
 #{select :item, related_fk, related.find(:all, :order => related.typus_order_by).collect { |p| [p.typus_name, p.id] }, { :include_blank => true }, { :disabled => attribute_disabled?(attribute, klass) } }</li>
         HTML
