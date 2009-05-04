5b780808a4ddb82ab601b359acd17f5a5b2a5b18
Francesc Esplugas <fesplugas@intraducibles.net>
2009-05-04 09:02:35 +0200

    Comments on Object#_.

--- lib/typus/object.rb
+++ lib/typus/object.rb
@@ -1,5 +1,18 @@
 class Object
 
+  ##
+  # Instead of having to translate strings and defining a default value:
+  #
+  #     t('Hello World!', :default => 'Hello World!')
+  #
+  # We define this method to define the value only once:
+  #
+  #     _('Hello World!')
+  #
+  # Note that interpolation still works ...
+  #
+  #     _('Hello {{world}}!', :world => @world)
+  #
   def _(msg, *args)
     options = args.extract_options!
     options[:default] = msg
