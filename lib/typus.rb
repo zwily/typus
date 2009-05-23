c2f628dee3e5f55799729fdbd268bb1e0939a09f
Francesc Esplugas <francesc@intraducibles.com>
2009-05-23 12:19:35 +0200

    Removed version dependency.

--- lib/typus.rb
+++ lib/typus.rb
@@ -2,10 +2,6 @@ module Typus
 
   class << self
 
-    def version
-      "0.9.0"
-    end
-
     def root
       File.dirname(__FILE__) + '/../'
     end
@@ -108,7 +104,7 @@ module Typus
       if plugin?
         I18n.load_path += Dir[File.join("#{Rails.root}/vendor/plugins/typus/config/locales/**/*.{rb,yml}")]
       else
-        Gem.path.each { |g| I18n.load_path += Dir[File.join("#{g}/gems/typus-#{version}/config/locales/**/*.{rb,yml}")] }
+        Gem.path.each { |g| I18n.load_path += Dir[File.join("#{g}/gems/typus-**/config/locales/**/*.{rb,yml}")] }
       end
 
       # Require the test/models on when testing.
