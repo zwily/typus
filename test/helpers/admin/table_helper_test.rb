fcd11b69827e86ed48ee8c05b18650d1f03c1c84
Francesc Esplugas <francesc@intraducibles.com>
2009-05-25 21:54:03 +0200

    Removed old test.

--- test/helpers/admin/table_helper_test.rb
+++ test/helpers/admin/table_helper_test.rb
@@ -185,22 +185,6 @@ class Admin::TableHelperTest < ActiveSupport::TestCase
 
   end
 
-=begin
-
-  # FIXME
-
-  def test_typus_table_position_field
-
-    category = categories(:first)
-    output = typus_table_position_field('position', category)
-    expected = ""
-
-    assert_equal expected, output
-
-  end
-
-=end
-
   def test_typus_table_datetime_field
 
     post = posts(:published)
