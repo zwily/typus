b9cb5ff2a5c9bf81e712c0987001ad791c0447cd
Francesc Esplugas <francesc@intraducibles.com>
2009-05-30 23:30:19 +0200

    Added pending options on model_configuration_options Test.

--- test/lib/configuration_test.rb
+++ test/lib/configuration_test.rb
@@ -24,12 +24,14 @@ class ConfigurationTest < ActiveSupport::TestCase
   def test_should_verify_model_configuration_options
     initializer = "#{Rails.root}/config/initializers/typus.rb"
     return if File.exists?(initializer)
+    assert_equal 'edit', Typus::Configuration.options[:default_action_on_item]
     assert_equal nil, Typus::Configuration.options[:end_year]
     assert_equal 10, Typus::Configuration.options[:form_rows]
     assert_equal true, Typus::Configuration.options[:icon_on_boolean]
     assert_equal false, Typus::Configuration.options[:index_after_save]
     assert_equal 5, Typus::Configuration.options[:minute_step]
     assert_equal 'nil', Typus::Configuration.options[:nil]
+    assert_equal false, Typus::Configuration.options[:only_user_items]
     assert_equal 15, Typus::Configuration.options[:per_page]
     assert_equal 5, Typus::Configuration.options[:sidebar_selector]
     assert_equal nil, Typus::Configuration.options[:start_year]
