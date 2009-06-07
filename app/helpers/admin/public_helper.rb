8dfbaddef796a8116d6e2e14826c0a5e07205b11
Francesc Esplugas <francesc@intraducibles.com>
2009-06-07 21:50:16 +0200

    Explanation on quick_edit usage.

--- app/helpers/admin/public_helper.rb
+++ app/helpers/admin/public_helper.rb
@@ -2,6 +2,14 @@ module Admin
 
   module PublicHelper
 
+    ##
+    # Quick edit usage:
+    #
+    #     <%= quick_edit(:message => "Edit this article", :path => "articles/edit/#{@article.id}") %>
+    #
+    # If user is logged in Typus, a link will appear on the top/right of 
+    # the pages where you insert this helper.
+    #
     def quick_edit(*args)
 
       options = args.extract_options!
