dd269b2699854480a7feb5c751b27a4ffa4cdefa
Francesc Esplugas <francesc@intraducibles.com>
2009-07-01 08:30:45 +0200

    Updated copy on the files generator.

--- lib/typus/generator.rb
+++ lib/typus/generator.rb
@@ -118,12 +118,22 @@ class Admin::#{model.pluralize}Controller < Admin::MasterController
   ##
   # You can extend Admin::MasterController with your methods.
   #
-  # This actions have to be defined in <tt>config/typus/application.yml</tt>.
+  # This actions have to be defined in <tt>config/typus/application.yml</tt>:
   #
   #   #{model}:
   #     actions:
   #       index: custom_action
   #       edit: custom_action_for_an_item
+  #
+  # And you have to add permissions on <tt>config/typus/application_roles.yml</tt> 
+  # to have access to them.
+  #
+  #   admin:
+  #     #{model}: create, read, update, destroy, custom_action
+  #
+  #   editor:
+  #     #{model}: create, read, update, custom_action_for_an_item
+  #
   #
   def custom_action
   end
