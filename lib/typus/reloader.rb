7a99cb1ec5faef1c14c832b2da74b8d4e5ee1f0c
Francesc Esplugas <francesc@intraducibles.com>
2009-07-07 13:26:45 +0200

    Cleaned comments.

--- lib/typus/reloader.rb
+++ lib/typus/reloader.rb
@@ -2,9 +2,7 @@ module Typus
 
   module Reloader
 
-    ##
     # Reload config and roles when app is running in development.
-    #
     def reload_config_et_roles
       return if Rails.env.production?
       logger.info "=> [typus] Configuration files have been reloaded."
