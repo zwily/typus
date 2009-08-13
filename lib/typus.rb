8ac49b5f2375715e56377cce0468a2e7ade4b305
Francesc Esplugas <francesc@intraducibles.com>
2009-08-13 10:40:47 +0200

    Moved code to an extension.

--- lib/typus.rb
+++ lib/typus.rb
@@ -1,15 +1,4 @@
-class ActionController::Routing::RouteSet
-
-  def load_routes_with_typus!
-    lib_path = File.dirname(__FILE__)
-    typus_routes = File.join(lib_path, *%w[ .. config routes_hack.rb ])
-    add_configuration_file(typus_routes) unless configuration_files.include?(typus_routes)
-    load_routes_without_typus!
-  end
-
-  alias_method_chain :load_routes!, :typus
-
-end
+require 'typus/extensions/routes'
 
 module Typus
 
---
+++ lib/typus/extensions/routes.rb
@@ -0,0 +1,16 @@
+if defined?(ActionController::Routing::RouteSet)
+
+  class ActionController::Routing::RouteSet
+
+    def load_routes_with_typus!
+      lib_path = File.dirname(__FILE__)
+      typus_routes = File.join(lib_path, *%w[ .. .. .. config routes_hack.rb ])
+      add_configuration_file(typus_routes) unless configuration_files.include?(typus_routes)
+      load_routes_without_typus!
+    end
+
+    alias_method_chain :load_routes!, :typus
+
+  end
+
+end
