c62b090c128c02382749287a0a3fed125aa685c9
Francesc Esplugas <francesc@intraducibles.com>
2009-09-09 08:09:22 +0200

    Scaffold for MasterController tests.

--- test/functional/admin/master_controller_test.rb
+++ test/functional/admin/master_controller_test.rb
@@ -2,4 +2,48 @@ require 'test/helper'
 
 class Admin::MasterControllerTest < ActionController::TestCase
 
+  def test_check_resource
+    assert true
+  end
+
+  def test_find_item
+    assert true
+  end
+
+  def test_check_ownership_of_item
+    assert true
+  end
+
+  def test_check_ownership_of_items
+    assert true
+  end
+
+  def check_ownership_of_referal_item
+    assert true
+  end
+
+  def test_set_fields
+    assert true
+  end
+
+  def test_set_order
+    assert true
+  end
+
+  def test_set_tiny_mce
+    assert true
+  end
+
+  def test_select_template
+    assert true
+  end
+
+  def test_create_with_back_to
+    assert true
+  end
+
+  def test_error_handler
+    assert true
+  end
+
 end
