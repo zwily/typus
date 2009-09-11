1428e1afb046cf25521a3b8af6d2b3def2babf29
Francesc Esplugas <francesc@intraducibles.com>
2009-09-11 18:52:36 +0200

    Refactored typus_order_by method.

--- lib/typus/active_record.rb
+++ lib/typus/active_record.rb
@@ -167,11 +167,14 @@ module Typus
     def typus_order_by
 
       fields = typus_defaults_for(:order_by)
-      return "#{table_name}.id ASC" if fields.empty?
 
-      order = fields.map do |field|
-                (field.include?('-')) ? "#{table_name}.#{field.delete('-')} DESC" : "#{table_name}.#{field} ASC"
-              end.join(', ')
+      order = if fields.empty?
+                "#{table_name}.id ASC"
+              else
+                fields.map do |field|
+                  field.include?('-') ? "#{table_name}.#{field.delete('-')} DESC" : "#{table_name}.#{field} ASC"
+                end.join(', ')
+              end
 
       return order
 
