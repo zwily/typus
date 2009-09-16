8be070961cfd648dde870a8fa7f0a4978256f1d9
Francesc Esplugas <francesc@intraducibles.com>
2009-09-16 19:23:14 +0200

    Test has_many and has_and_belongs_to_many.

--- test/functional/admin/master_controller_relationships_test.rb
+++ test/functional/admin/master_controller_relationships_test.rb
@@ -58,21 +58,70 @@ class Admin::PostsControllerTest < ActionController::TestCase
     @request.session[:typus_user_id] = typus_user.id
   end
 
-  def test_should_unrelate_category_from_post_which_is_a_habtm_relationship
-    category = categories(:first)
+  ##
+  # Post => has_many :comments
+  ##
+
+  def test_should_relate_comments_to_post_and_then_unrelate
+
+    comment = comments(:without_post_id)
     post_ = posts(:published)
-    @request.env['HTTP_REFERER'] = "/admin/posts/edit/#{post_.id}#categories"
-    assert_difference('category.posts.count', 0) do
-      post :unrelate, { :id => post_.id, :resource => 'Category', :resource_id => category.id, :association => 'has_and_belongs_to_many' }
+    @request.env['HTTP_REFERER'] = "/admin/posts/edit/#{post_.id}#comments"
+
+    assert_difference('post_.comments.count') do
+      post :relate, { :id => post_.id, 
+                      :related => { :model => 'Comment', :id => comment.id } }
     end
+
     assert_response :redirect
-    assert_equal "Category unrelated from Post.", flash[:success]
     assert_redirected_to @request.env['HTTP_REFERER']
+    assert_equal "Comment related to Post.", flash[:success]
+
+    assert_difference('post_.comments.count', -1) do
+      post :unrelate, { :id => post_.id, 
+                        :resource => 'Comment', :resource_id => comment.id }
+    end
+
+    assert_response :redirect
+    assert_redirected_to @request.env['HTTP_REFERER']
+    assert_equal "Comment unrelated from Post.", flash[:success]
+
   end
 
   ##
-  # This is a polimorphic relationship.
-  #
+  # Post => has_and_belongs_to_many :categories
+  ##
+
+  def test_should_relate_post_to_category_and_then_unrelate
+
+    category = categories(:first)
+    post_ = posts(:published)
+    @request.env['HTTP_REFERER'] = "/admin/posts/edit/#{post_.id}#categories"
+
+    assert_difference('category.posts.count') do
+      post :relate, { :id => post_.id, 
+                      :related => { :model => 'Category', :id => category.id } }
+    end
+
+    assert_response :redirect
+    assert_redirected_to @request.env['HTTP_REFERER']
+    assert_equal "Category related to Post.", flash[:success]
+
+    assert_difference('category.posts.count', -1) do
+      post :unrelate, { :id => post_.id, 
+                        :resource => 'Category', :resource_id => category.id }
+    end
+
+    assert_response :redirect
+    assert_redirected_to @request.env['HTTP_REFERER']
+    assert_equal "Category unrelated from Post.", flash[:success]
+
+  end
+
+  ##
+  # Post => has_many :assets, :as => resource, :dependent => :destroy
+  ##
+
   def test_should_unrelate_an_asset_from_a_post
 
     post_ = posts(:published)
@@ -80,25 +129,14 @@ class Admin::PostsControllerTest < ActionController::TestCase
     @request.env['HTTP_REFERER'] = "/admin/posts/edit/#{post_.id}#assets"
 
     assert_difference('post_.assets.count', -1) do
-      get :unrelate, { :id => post_.id, :resource => 'Asset', :resource_id => post_.assets.first.id, :association => 'has_many' }
+      get :unrelate, { :id => post_.id,  
+                       :resource => 'Asset', :resource_id => post_.assets.first.id }
     end
 
     assert_response :redirect
     assert_redirected_to @request.env['HTTP_REFERER']
     assert_equal "Asset unrelated from Post.", flash[:success]
 
-  end
-
-  def test_should_relate_category_to_post_which_is_a_habtm_relationship
-    category = categories(:first)
-    post_ = posts(:published)
-    @request.env['HTTP_REFERER'] = "/admin/posts/edit/#{post_.id}#categories"
-    assert_difference('category.posts.count') do
-      post :relate, { :id => post_.id, :related => { :model => 'Category', :id => category.id } }
-    end
-    assert_response :redirect
-    assert_equal "Category related to Post.", flash[:success]
-    assert_redirected_to @request.env['HTTP_REFERER']
   end
 
 end
