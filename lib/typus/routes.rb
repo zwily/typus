c62387e83e64c89758a71de223ef675597e8e05a
Francesc Esplugas <francesc@intraducibles.com>
2010-03-30 21:47:45 +0200

    Experiment to see if we can rewrite the admin_prefix.

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -15,20 +15,20 @@ module Typus
     # @example
     #   map.resources :users, :only => [:new, :create]
     #   Typus::Routes.draw(map)
-    def self.draw(map)
+    def self.draw(map, prefix = 'admin')
 
       map.with_options :controller => 'admin/account' do |i|
         # i.admin_quick_edit 'quick_edit', :action => 'quick_edit'
-        i.admin_sign_in 'admin/sign_in', :action => 'sign_in'
-        i.admin_sign_out 'admin/sign_out', :action => 'sign_out'
-        i.admin_sign_up 'admin/sign_up', :action => 'sign_up'
-        i.admin_recover_password 'admin/recover_password', :action => 'recover_password'
-        i.admin_reset_password 'admin/reset_password', :action => 'reset_password'
+        i.admin_sign_in "#{prefix}/sign_in", :action => 'sign_in'
+        i.admin_sign_out "#{prefix}/sign_out", :action => 'sign_out'
+        i.admin_sign_up "#{prefix}/sign_up", :action => 'sign_up'
+        i.admin_recover_password "#{prefix}/recover_password", :action => 'recover_password'
+        i.admin_reset_password "#{prefix}/reset_password", :action => 'reset_password'
       end
 
       map.with_options :controller => 'admin/dashboard' do |i|
-        i.connect 'admin', :action => 'index'
-        i.admin_dashboard 'admin/dashboard', :action => 'index'
+        i.connect "#{prefix}", :action => 'index'
+        i.admin_dashboard "#{prefix}/dashboard", :action => 'index'
       end
 
       map.connect ':controller/:action/:id', :controller => /admin\/\w+/
