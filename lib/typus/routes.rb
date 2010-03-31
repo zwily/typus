691bc5d6889bf53254def70b132b1d3024a0e720
Francesc Esplugas <francesc@intraducibles.com>
2010-03-31 09:16:12 +0200

    Use :path_prefix on routes.

--- lib/typus/routes.rb
+++ lib/typus/routes.rb
@@ -17,23 +17,25 @@ module Typus
     #   Typus::Routes.draw(map)
     def self.draw(map, prefix = 'admin')
 
-      map.admin "admin", :controller => 'admin', :action => 'index'
+      map.with_options :controller => "admin", :path_prefix => prefix do |i|
+        i.admin ""
+      end
 
-      map.with_options :controller => 'admin/account' do |i|
+      map.with_options :controller => "admin/account", :path_prefix => prefix do |i|
         # i.admin_quick_edit 'quick_edit', :action => 'quick_edit'
-        i.admin_sign_in "#{prefix}/sign_in", :action => 'sign_in'
-        i.admin_sign_out "#{prefix}/sign_out", :action => 'sign_out'
-        i.admin_sign_up "#{prefix}/sign_up", :action => 'sign_up'
-        i.admin_recover_password "#{prefix}/recover_password", :action => 'recover_password'
-        i.admin_reset_password "#{prefix}/reset_password", :action => 'reset_password'
+        i.admin_sign_in "sign_in", :action => 'sign_in'
+        i.admin_sign_out "sign_out", :action => 'sign_out'
+        i.admin_sign_up "sign_up", :action => 'sign_up'
+        i.admin_recover_password "recover_password", :action => 'recover_password'
+        i.admin_reset_password "reset_password", :action => 'reset_password'
       end
 
-      map.with_options :controller => 'admin/dashboard' do |i|
-        i.admin_dashboard "#{prefix}/dashboard", :action => 'index'
+      map.with_options :controller => "admin/dashboard", :path_prefix => prefix do |i|
+        i.admin_dashboard "dashboard"
       end
 
-      map.connect ':controller/:action/:id', :controller => /admin\/\w+/
-      map.connect ':controller/:action/:id.:format', :controller => /admin\/\w+/
+      map.connect ":controller/:action/:id", :controller => /admin\/\w+/
+      map.connect ":controller/:action/:id.:format", :controller => /admin\/\w+/
 
     end
 
