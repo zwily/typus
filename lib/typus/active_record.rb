8939709ea472f1c531c21d215fbbcffa4ec4d6a6
Francesc Esplugas <francesc@intraducibles.com>
2010-04-06 08:58:18 +0200

    Added TODO

--- lib/typus/active_record.rb
+++ lib/typus/active_record.rb
@@ -354,6 +354,7 @@ module Typus
     # Determine if file attachment is blank, taking into account that
     # user may be using a Paperclip attachment without a _file_name
     # suffix
+    # TODO: Test attachment_present? method.
     def attachment_present?(attribute)
       attribute = attribute.to_s
       if attribute.index('_file_name')
