439a38361e59bc69d7aa9528ee63eac47b3218b0
Francesc Esplugas <francesc@intraducibles.com>
2010-04-08 15:12:17 +0200

    Take back the merge conditions until we move everything to Arel.

--- lib/extensions/active_record.rb
+++ lib/extensions/active_record.rb
@@ -1,5 +1,21 @@
 class ActiveRecord::Base
 
+  # Merges conditions so that the result is a valid +condition+
+  def self.merge_conditions(*conditions)
+
+    segments = []
+
+    conditions.each do |condition|
+      unless condition.blank?
+        sql = sanitize_sql(condition)
+        segments << sql unless sql.blank?
+      end
+    end
+
+    "(#{segments.join(') AND (')})" unless segments.empty?
+
+  end
+
   def to_dom(*args)
 
     options = args.extract_options!
