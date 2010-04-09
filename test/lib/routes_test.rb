e5a457621c979c43cbd08c2fd0fcae3957f371fb
Francesc Esplugas <francesc@intraducibles.com>
2010-04-09 13:53:16 +0200

    Gardening

--- test/lib/routes_test.rb
+++ test/lib/routes_test.rb
@@ -10,23 +10,22 @@ class RoutesTest < ActiveSupport::TestCase
 
   def test_should_verify_admin_dashboard_routes
 
-    expected = [ :admin_dashboard ]
-    expected.each { |route| assert @routes.include?(route) }
+    expected = %w( admin_dashboard )
+    expected.each { |r| assert @routes.include?(r.to_sym) }
 
-    assert_routing 'admin/dashboard', :controller => 'admin/dashboard', :action => 'index'
+    assert_routing "admin/dashboard", :controller => "admin/dashboard", :action => "index"
 
   end
 
   def test_should_verify_admin_account_named_routes
 
-    expected = [ :admin_sign_up, :admin_sign_in, :admin_sign_out, 
-                 :admin_recover_password, :admin_reset_password ]
-    expected.each { |route| assert @routes.include?(route) }
+    expected = %w( admin_sign_up admin_sign_in admin_sign_out 
+                   admin_recover_password admin_reset_password )
+    expected.each { |r| assert @routes.include?(r.to_sym) }
 
-    actions = [ 'sign_up', 'sign_in', 'sign_out', 
-                'recover_password', 'reset_password' ]
+    actions = %w( sign_up sign_in sign_out recover_password reset_password )
 
-    actions.each { |a| assert_routing "admin/#{a}", :controller => 'admin/account', :action => a }
+    actions.each { |a| assert_routing "admin/#{a}", :controller => "admin/account", :action => a }
 
   end
 
