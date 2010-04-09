15be83140a62dad6ffd51de5c715143ce21f7b5f
Francesc Esplugas <francesc@intraducibles.com>
2010-04-09 21:40:19 +0200

    Boot application inside the test folder.

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,7 @@
 
 ENV['RAILS_ENV'] = 'test'
-require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
+
+require 'rails_app/config/environment'
 require 'test_help'
 require 'mocha'
 
