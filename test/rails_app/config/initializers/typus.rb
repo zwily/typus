32c33609a06b0ae29209fc39b69e2a8552cbb569
Francesc Esplugas <francesc@intraducibles.com>
2010-04-10 17:54:54 +0200

    Typus::Configuration.setup becomes Typus.setup.

--- test/rails_app/config/initializers/typus.rb
+++ test/rails_app/config/initializers/typus.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Typus::Configuration.setup do |config|
+Typus.setup do |config|
   config.app_name = 'rails3_typus'
-  config.user_class_name = 'TypusUser'
-  config.user_fk = 'typus_user_id'
   config.config_folder = '../../../../config/working'
 end
