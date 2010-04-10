68d743a68e6c369f87f83673b5639c597f1a9085
Francesc Esplugas <francesc@intraducibles.com>
2010-04-10 18:18:29 +0200

    Updated rails_app

--- test/rails_app/config/initializers/typus.rb
+++ test/rails_app/config/initializers/typus.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Typus.setup do |config|
-  config.app_name = 'rails3_typus'
-  config.config_folder = '../../../../config/working'
+  config.config_folder = Rails.root.join("../config/working")
 end
