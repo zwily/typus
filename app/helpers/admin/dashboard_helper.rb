f91acb52e9a6d075a4d5c28b3d97279c88fb8612
Francesc Esplugas <francesc@intraducibles.com>
2010-04-16 11:04:56 +0200

    Gardening

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -3,37 +3,27 @@ module Admin
   module DashboardHelper
 
     def applications
-
       apps = ActiveSupport::OrderedHash.new
 
       Typus.applications.each do |app|
-
         available = Typus.application(app).map do |resource|
                       resource if @current_user.resources.include?(resource)
                     end.compact
-
         next if available.empty?
-
         apps[app] = available.sort_by { |x| x.constantize.model_name.human }
-
       end
 
-      render "admin/helpers/applications", 
-             :applications => apps
-
+      render "admin/helpers/applications", :applications => apps
     end
 
     def resources
-
       available = Typus.resources.map do |resource|
                     resource if @current_user.resources.include?(resource)
                   end.compact
 
       return if available.empty?
 
-      render "admin/helpers/resources", 
-             :resources => available
-
+      render "admin/helpers/resources", :resources => available
     end
 
   end
