e4521c90bfc586daad4c1dd31f81f495f7e8d1c8
Francesc Esplugas <francesc@intraducibles.com>
2010-04-20 14:41:45 +0200

    Fixed typus_belongs_to_field

--- app/helpers/admin/form_helper.rb
+++ app/helpers/admin/form_helper.rb
@@ -76,7 +76,7 @@ module Admin
           label_text = @resource[:class].human_attribute_name(attribute)
           html << <<-HTML
   <li>
-    #{form.label related_fk, "#{label_text} <small>#{message}</small>"}
+    #{form.label related_fk, raw("#{label_text} <small>#{message}</small>")}
     #{form.select related_fk, values, options, html_options }
   </li>
           HTML
