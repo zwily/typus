1f720978d3652368792304309f4f446a5cab48aa
Francesc Esplugas <francesc@intraducibles.com>
2010-04-20 18:11:39 +0200

    Massive updates ...

---
+++ app/helpers/admin/filters_helper.rb
@@ -0,0 +1,106 @@
+module Admin
+
+  module FiltersHelper
+
+    def build_typus_selector(filter, items)
+      render "admin/helpers/selector", :items => items, :attribute => filter
+    end
+
+    def filters
+
+      typus_filters = @resource[:class].typus_filters
+      return if typus_filters.empty?
+
+      current_request = request.env['QUERY_STRING'] || []
+
+      html = "<ul id=\"filters\">"
+
+      typus_filters.each do |key, value|
+        case value
+        when :boolean then html << boolean_filter(current_request, key)
+        when :string then html << string_filter(current_request, key)
+        when :date, :datetime then html << date_filter(current_request, key)
+        when :belongs_to then html << relationship_filter(current_request, key)
+        when :has_many || :has_and_belongs_to_many then
+          html << relationship_filter(current_request, key, true)
+        when nil then
+          # Do nothing. This is ugly but for now it's ok.
+        else
+          html << string_filter(current_request, key)
+        end
+      end
+
+      html << "</ul>"
+
+      return html
+
+    end
+
+    def relationship_filter(request, filter, habtm = false)
+      att_assoc = @resource[:class].reflect_on_association(filter.to_sym)
+      class_name = att_assoc.options[:class_name] || ((habtm) ? filter.classify : filter.capitalize.camelize)
+      model = class_name.constantize
+      related_fk = (habtm) ? filter : att_assoc.primary_key_name
+
+      params_without_filter = params.dup
+      %w( controller action page ).each { |p| params_without_filter.delete(p) }
+      params_without_filter.delete(related_fk)
+
+      items = model.all(:order => model.typus_order_by)
+
+      build_typus_selector(filter, items)
+    end
+
+    def date_filter(request, filter)
+
+      items = {}
+
+      # if !@resource[:class].typus_field_options_for(:filter_by_date_range).include?(filter.to_sym)
+      %w( today last_few_days last_7_days last_30_days ).map do |timeline|
+        switch = request.include?("#{filter}=#{timeline}") ? 'on' : 'off'
+        options = { :page => nil }
+        if switch == 'on'
+          params.delete(filter)
+        else
+          options.merge!(filter.to_sym => timeline)
+        end
+        items[timeline] = _(timeline.humanize)
+        # link_to _(timeline.humanize), params.merge(options), :class => switch
+      end
+      build_typus_selector(filter, items)
+        #, :attribute => filter)
+      # else
+      #  date_params = params.dup
+
+      #  %w( action controller page id ).each { |p| date_params.delete(p) }
+      #  date_params.delete(filter)
+
+      #  hidden_params = date_params.map { |k, v| hidden_field_tag(k, v) }
+      #  render "admin/helpers/date", :hidden_params => hidden_params, :filter => filter, :resource => @resource
+      # end
+
+    end
+
+    def boolean_filter(request, filter)
+      items = @resource[:class].typus_boolean(filter)
+      build_typus_selector(filter, items)
+    end
+
+    def string_filter(request, filter)
+      values = @resource[:class]::const_get("#{filter.to_s.upcase}")
+
+      if values.kind_of?(Hash)
+        items = values.invert
+      else
+        items = {}
+        values.each do |value|
+          items[value] = value
+        end
+      end
+
+      build_typus_selector(filter, items)
+    end
+
+  end
+
+end
---
+++ app/helpers/admin/search_helper.rb
@@ -0,0 +1,25 @@
+module Admin
+
+  module SearchHelper
+
+    def search
+
+      typus_search = @resource[:class].typus_defaults_for(:search)
+      return if typus_search.empty?
+
+      search_by = typus_search.collect { |x| @resource[:class].human_attribute_name(x) }.to_sentence
+
+      search_params = params.dup
+      %w( action controller search page id ).each { |p| search_params.delete(p) }
+
+      hidden_params = search_params.map { |k, v| hidden_field_tag(k, v) }
+
+      render "admin/helpers/search", 
+             :hidden_params => hidden_params, 
+             :search_by => search_by
+
+    end
+
+  end
+
+end
