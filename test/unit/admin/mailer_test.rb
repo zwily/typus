2144e7fec690d5f3114a928e270af989bd6e2d00
Francesc Esplugas <francesc@intraducibles.com>
2010-04-22 18:50:16 +0200

    Fixed Admin::Mailer tests

--- test/unit/admin/mailer_test.rb
+++ test/unit/admin/mailer_test.rb
@@ -5,30 +5,30 @@ class Admin::MailerTest < ActiveSupport::TestCase
 
   def setup
     @user = typus_users(:admin)
-    url = "http://test.host/admin/reset_password?token=#{@user.token}"
-    @response = Admin::Mailer.reset_password_link(@user, url)
+    url = "http://test.host/admin/account/#{@user.token}"
+    @email = Admin::Mailer.reset_password_link(@user, url)
   end
 
-  # FIXME: For some reason we cannot stub Typus.email
   def test_should_verify_email_from_is_defined_by_typus_options
     assert Admin::Mailer.default[:from].nil?
   end
 
   def test_should_verify_email_to_is_typus_user_email
-    assert_equal [@user.email], @response.to
+    assert_equal [@user.email], @email.to
   end
 
   def test_should_verify_email_subject
     expected = "[#{Typus.admin_title}] Reset password"
-    assert_equal expected, @response.subject
+    assert_equal expected, @email.subject
   end
 
-=begin
-  # FIXME: ActionMailer has changed, so email body is not in the response. Have to investigate.
-  def test_should_verify_email_contains_reset_password_link_with_token
-    expected = "http://test.host/admin/reset_password?token=1A2B3C4D5E6F"
-    assert_match expected, @response.body
+  def test_should_verify_email_mime_type
+    assert_equal "text/plain", @email.mime_type
   end
-=end
+
+  def test_should_verify_email_contains_reset_password_link_with_token
+    expected = "http://test.host/admin/account/1A2B3C4D5E6F"
+    assert_match expected, @email.body.encoded
+  end
 
 end
