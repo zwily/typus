604370de0bb6a86bf284861427e21cd07c29b106
Francesc Esplugas <francesc@intraducibles.com>
2010-04-24 08:44:20 +0200

    Renamed test

--- test/lib/support/hash_test.rb
+++ test/lib/support/hash_test.rb
@@ -2,7 +2,7 @@ require "test/test_helper"
 
 class HashTest < ActiveSupport::TestCase
 
-  def test_should_verify_compact
+  def test_compact
     hash = { "a" => "", "b" => nil, "c" => "hello" }
     hash_compacted = { "c" => "hello" }
     assert_equal hash_compacted, hash.compact
