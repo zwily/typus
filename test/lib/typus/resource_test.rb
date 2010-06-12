f4aaacf77b09caa9453bb04865515b5d1cef3b69
Francesc Esplugas <francesc@intraducibles.com>
2010-06-12 18:21:35 +0200

    on_header options is no longer available

--- test/lib/typus/resource_test.rb
+++ test/lib/typus/resource_test.rb
@@ -9,7 +9,6 @@ class ResourceTest < ActiveSupport::TestCase
     assert_equal "show", Typus::Resource.action_after_save
     assert_equal 5, Typus::Resource.minute_step
     assert_equal "nil", Typus::Resource.nil
-    assert_equal false, Typus::Resource.on_header
     assert_equal false, Typus::Resource.only_user_items
     assert_equal 15, Typus::Resource.per_page
     assert_nil Typus::Resource.start_year
