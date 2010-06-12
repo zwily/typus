ace3bdb7e167ec4bdbf037bb6636c14c98d5a89d
Ben Scofield <git@turrean.com>
2010-06-13 02:19:22 +0800

    simplify routes in the testing app by making :format optional

--- test/fixtures/rails_app/config/routes.rb
+++ test/fixtures/rails_app/config/routes.rb
@@ -1,7 +1,6 @@
 RailsApp::Application.routes.draw do |map|
 
-  map.connect "admin/delayed/tasks/:action/:id", :controller => "admin/delayed/tasks"
-  map.connect "admin/delayed/tasks/:action/:id.:format", :controller => "admin/delayed/tasks"
+  map.connect "admin/delayed/tasks/:action/:id(.:format)", :controller => "admin/delayed/tasks"
 
   Typus::Routes.draw(map)
   root :to => "welcome#index"
