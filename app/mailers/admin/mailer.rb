1671e822d059c2b6242769f5cb41fab02fcac9a5
Francesc Esplugas <francesc@intraducibles.com>
2010-06-13 22:37:55 +0200

    Gardening

--- app/mailers/admin/mailer.rb
+++ app/mailers/admin/mailer.rb
@@ -5,8 +5,7 @@ class Admin::Mailer < ActionMailer::Base
   def reset_password_link(user, url)
     @user = user
     @url = url
-    mail :to => user.email, 
-         :subject => "[#{Typus.admin_title}] #{_("Reset password")}"
+    mail :to => user.email, :subject => "[#{Typus.admin_title}] #{_("Reset password")}"
   end
 
 end
