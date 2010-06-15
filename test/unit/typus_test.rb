122eb03029ee5ffd4f8446bd627ae9e9eb7c538a
Francesc Esplugas <francesc@intraducibles.com>
2010-06-15 13:32:06 +0200

    We are going to replace fixtures by factory_girl

--- test/unit/typus_test.rb
+++ test/unit/typus_test.rb
@@ -7,14 +7,4 @@ class TypusTest < ActiveSupport::TestCase
     models.each { |m| assert m.superclass.equal?(ActiveRecord::Base) }
   end
 
-  def test_should_verify_fixtures_are_loaded
-    assert_equal 2, Asset.count
-    assert_equal 3, Category.count
-    assert_equal 4, Comment.count
-    assert_equal 6, Page.count
-    assert_equal 2, Picture.count
-    assert_equal 4, Post.count
-    assert_equal 5, TypusUser.count
-  end
-
 end
