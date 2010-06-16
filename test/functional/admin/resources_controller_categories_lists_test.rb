e92da2246f3833a8da4367ea7736f2c900a64759
Francesc Esplugas <francesc@intraducibles.com>
2010-06-16 10:23:58 +0200

    Gardening

--- test/functional/admin/resources_controller_categories_lists_test.rb
+++ test/functional/admin/resources_controller_categories_lists_test.rb
@@ -4,15 +4,13 @@ class Admin::CategoriesControllerTest < ActionController::TestCase
 
   setup do
     @request.session[:typus_user_id] = typus_users(:admin).id
+    @request.env['HTTP_REFERER'] = '/admin/categories'
     @first_category = Factory(:category, :position => 1)
     @second_category = Factory(:category, :position => 2)
   end
 
   should "verify referer" do
-    @request.env['HTTP_REFERER'] = '/admin/categories'
-
     get :position, { :id => @first_category.id, :go => 'move_lower' }
-
     assert_response :redirect
     assert_redirected_to @request.env['HTTP_REFERER']
   end
@@ -35,14 +33,12 @@ class Admin::CategoriesControllerTest < ActionController::TestCase
 
   should "position top item to bottom" do
     get :position, { :id => @first_category.id, :go => 'move_to_bottom' }
-
     assert_equal "Record moved to bottom.", flash[:notice]
     assert_equal 2, @first_category.reload.position
   end
 
   should "position bottom item to top" do
     get :position, { :id => @second_category.id, :go => 'move_to_top' }
-
     assert_equal "Record moved to top.", flash[:notice]
     assert_equal 1, @second_category.reload.position
   end
