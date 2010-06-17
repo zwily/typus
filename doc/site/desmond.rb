522b95af358191b34f09fbd26b9c4f652a0f442d
Francesc Esplugas <francesc@intraducibles.com>
2010-06-17 17:31:47 +0200

    Updated desmond

--- doc/site/desmond.rb
+++ doc/site/desmond.rb
@@ -1,8 +1,22 @@
-##
-# $ rails new desmond -m http://github.com/fesplugas/typus/raw/3-0-unstable/doc/site/desmond.rb
-##
+=begin
 
-plugin "typus", :git => "git://github.com/fesplugas/typus.git -r 3-0-unstable"
+== Rails & Typus ==
+
+Install typus, typus_cms and typus_settings.
+
+    $ rails new desmond -m http://github.com/fesplugas/typus/raw/3-0-unstable/doc/site/desmond.rb
+
+Enjoy!
+
+=end
+
+if yes?("Are you using Rails 3.0.0.beta4?")
+  branch = "3-0-unstable"
+else
+  branch = "2-3-stable"
+end
+
+plugin "typus", :git => "git://github.com/fesplugas/typus.git -r #{branch}"
 plugin "typus_cms", :git => "git@trunksapp.com:fesplugas/typus_cms.git"
 plugin "typus_settings", :git => "git@trunksapp.com:fesplugas/typus_settings.git"
 
@@ -23,6 +37,8 @@ generate "typus_migration"
 generate "typus_cms_migration"
 generate "typus_settings_migration"
 
+##
 # Migrate the database!
+##
 
 rake "db:migrate"
