dfda3a5bf8b69c7a43de7ce4a868e137cc95e620
Francesc Esplugas <francesc@intraducibles.com>
2010-06-19 11:34:39 +0200

    Fixed templates path

--- lib/generators/typus/migration_generator.rb
+++ lib/generators/typus/migration_generator.rb
@@ -8,7 +8,7 @@ module Typus
 
       include Rails::Generators::Migration
 
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
 
       Typus.reload!
 
