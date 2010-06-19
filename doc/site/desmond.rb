3436cbd30425d37324675b036c4d68d258ec7132
Francesc Esplugas <francesc@intraducibles.com>
2010-06-19 11:32:53 +0200

    Updated desmond template

--- doc/site/desmond.rb
+++ doc/site/desmond.rb
@@ -8,29 +8,38 @@ Enjoy!
 
 =end
 
+##
+# Install typus
+##
+
 answer = ask("Are you using Rails 3.0.0.beta4? (YES/no)").downcase
 branch = (answer == "no" || answer == "n") ? "2-3-stable" : "3-0-unstable"
 
 plugin "typus", :git => "git://github.com/fesplugas/typus.git -r #{branch}"
-plugin "typus_cms", :git => "git@trunksapp.com:fesplugas/typus_cms.git"
-plugin "typus_settings", :git => "git@trunksapp.com:fesplugas/typus_settings.git"
+generate "typus"
+generate "typus:migration"
 
 ##
 # Install useful plugins
 ##
 
-plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 plugin "acts_as_list", :git => "git://github.com/rails/acts_as_list.git"
 plugin "acts_as_tree", :git => "git://github.com/rails/acts_as_tree.git"
+plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 
 ##
-# Run typus generators
+# Install typus extras only if user has access to trunksapp.com
 ##
 
-generate "typus"
-generate "typus:migration"
-generate "typus_cms_migration"
-generate "typus_settings_migration"
+answer = ask("Do you have access to trunksapp.com? (yes/NO)").downcase
+extras = (answer == "yes" || answer == "y") ? true : false
+
+if extras
+  plugin "typus_cms", :git => "git@trunksapp.com:fesplugas/typus_cms.git"
+  plugin "typus_settings", :git => "git@trunksapp.com:fesplugas/typus_settings.git"
+  generate "typus_cms_migration"
+  generate "typus_settings_migration"
+end
 
 ##
 # Migrate the database!
