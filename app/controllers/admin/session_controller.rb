fa0dda39cf50593afcf6473b98af3e34f4f1741e
Francesc Esplugas <francesc@intraducibles.com>
2010-07-18 19:35:56 +0200

    Renamed account layout to session

--- app/controllers/admin/session_controller.rb
+++ app/controllers/admin/session_controller.rb
@@ -1,7 +1,5 @@
 class Admin::SessionController < AdminController
 
-  layout "admin/account"
-
   skip_before_filter :reload_config_and_roles
   skip_before_filter :set_preferences
   skip_before_filter :authenticate
