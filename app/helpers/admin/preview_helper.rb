6858de299df129fd27f11af60abd95ae74576c86
Francesc Esplugas <francesc@intraducibles.com>
2010-08-10 13:06:12 +0200

    If attachment is not an image return a link to view action

--- app/helpers/admin/preview_helper.rb
+++ app/helpers/admin/preview_helper.rb
@@ -6,12 +6,18 @@ module Admin
 
       return unless item.send(attribute).exists?
 
+      file_preview_is_image = (item.send("#{attribute}_content_type") =~ /^image\/.+/)
+
+      unless file_preview_is_image
+        file = File.basename(item.send(attribute).path(:original))
+        link = link_to(file, :action => 'view', :id => item)
+        return link
+      end
+
       file = { :preview => Typus.file_preview, :thumbnail => Typus.file_thumbnail }
 
       has_file_preview = item.send(attribute).styles.member?(file[:preview])
       has_file_thumbnail = item.send(attribute).styles.member?(file[:thumbnail])
-
-      file_preview_is_image = (item.send("#{attribute}_content_type") =~ /^image\/.+/)
 
       href = if has_file_preview && file_preview_is_image
                url = item.send(attribute).url(file[:preview])
