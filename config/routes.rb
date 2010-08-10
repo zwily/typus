0174383bec4b585bf93b0b030948a171d98abb25
Francesc Esplugas <francesc@intraducibles.com>
2010-08-10 16:36:01 +0200

    Added missing route and fixed it.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
     resource :dashboard, :only => [:show], :controller => :dashboard
     resource :session, :only => [:new, :create, :destroy], :controller => :session
-    resources :account, :only => [:new, :create, :show] do
+    resources :account, :only => [:new, :create, :show, :forgot_password] do
       collection { get :forgot_password }
     end
 
