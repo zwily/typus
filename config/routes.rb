8ccdbda5e7cb8b660574e5d94d6d2a07d6cf4e64
Francesc Esplugas <francesc@intraducibles.com>
2010-08-13 08:46:54 +0200

    Disable session and account resource if needed

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   namespace :admin do
 
     resource :dashboard, :only => [:show], :controller => :dashboard
-    resource :session, :only => [:new, :create, :destroy], :controller => :session
-    resources :account, :only => [:new, :create, :show, :forgot_password] do
-      collection { get :forgot_password }
+
+    if Typus.authentication == :session
+      resource :session, :only => [:new, :create, :destroy], :controller => :session
+      resources :account, :only => [:new, :create, :show, :forgot_password] do
+        collection { get :forgot_password }
+      end
     end
 
   end
