dbdbd9f13dd75d038828c5412b22fb7c1bf299ec
Francesc Esplugas <francesc@intraducibles.com>
2010-08-30 18:14:11 +0200

    Typus::Resource.setup with all available options

--- lib/generators/templates/config/initializers/typus_resources.rb
+++ lib/generators/templates/config/initializers/typus_resources.rb
@@ -1,8 +1,30 @@
 Typus::Resource.setup do |config|
-  config.default_action_on_item = 'edit'
-  config.action_after_save = 'edit'
 
-  # config.only_user_items = true
+  # Defines the default_action_on_item.
+  # config.default_action_on_item = "edit"
 
-  config.per_page = 30
+  # Defines the end_year.
+  # config.end_year = nil
+
+  # Defines the form_rows.
+  # config.form_rows = 15
+
+  # Defines the action_after_save.
+  # config.action_after_save = "edit"
+
+  # Defines the minute step.
+  # config.minute_step = 5
+
+  # Defines nil.
+  # config.human_nil = "nil"
+
+  # Defines only_user_items.
+  # config.only_user_items = false
+
+  # Defines per_page
+  # config.per_page = 15
+
+  # Defines start year
+  # config.start_year = nil
+
 end
