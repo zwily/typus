81f2844e6e8dd306b8be2ea43f9c997f00275479
Francesc Esplugas <francesc@intraducibles.com>
2010-09-02 14:06:37 +0200

    Fixed paths to make gem work

--- lib/typus/configuration.rb
+++ lib/typus/configuration.rb
@@ -4,8 +4,8 @@ module Typus
 
     # Read configuration from <tt>config/typus/**/*.yml</tt>.
     def self.config!
-      application = Dir[Rails.root.join(Typus.config_folder, "**", "*.yml").to_s]
-      plugins = Dir[Rails.root.join("vendor", "plugins", "*", "config", "typus", "*.yml").to_s]
+      application = Dir[File.join(Typus.config_folder, "**", "*.yml").to_s]
+      plugins = Dir[File.join("vendor", "plugins", "*", "config", "typus", "*.yml").to_s]
       files = (application + plugins).reject { |f| f.include?("_roles.yml") }
 
       @@config = {}
@@ -22,8 +22,8 @@ module Typus
 
     # Read roles from files <tt>config/typus/**/*_roles.yml</tt>.
     def self.roles!
-      application = Dir[Rails.root.join(Typus.config_folder, "**", "*_roles.yml").to_s]
-      plugins = Dir[Rails.root.join("vendor", "plugins", "*", "config", "typus", "*_roles.yml").to_s]
+      application = Dir[File.join(Typus.config_folder, "**", "*_roles.yml").to_s]
+      plugins = Dir[File.join("vendor", "plugins", "*", "config", "typus", "*_roles.yml").to_s]
       files = (application + plugins).sort
 
       @@roles = { Typus.master_role => {} }
