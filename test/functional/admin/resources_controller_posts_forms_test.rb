c0cbea5f6b4c238087b9e9c0ff4ebc70f6c71a65
Francesc Esplugas <francesc@intraducibles.com>
2010-09-02 13:16:06 +0200

    Updated tests for forms

--- test/functional/admin/resources_controller_posts_forms_test.rb
+++ test/functional/admin/resources_controller_posts_forms_test.rb
@@ -17,18 +17,41 @@ class Admin::PostsControllerTest < ActionController::TestCase
 
     assert_select "form"
 
-    assert_select "form input", 2
-    assert_select "form textarea", 1
-    assert_select "form select", 6
+    # We have 3 inputs: 1 hidden which is the UTF8 stuff, one which is the 
+    # Post#title and finally the submit button.
 
+    assert_select "form input", 3
+
+    # Post#title: Input
     assert_select 'label[for="post_title"]'
     assert_select 'input#post_title[type="text"]'
 
+    # We have 1 textarea
+    assert_select "form textarea", 1
+
+    # Post#body: Text Area
     assert_select 'label[for="post_body"]'
     assert_select 'textarea#post_body'
 
+    # We have 6 selectors
+    assert_select "form select", 6
+
+    # Post#created_at: Datetime
+    assert_select 'label[for="post_created_at"]'
+    assert_select 'select#post_created_at_1i'
+    assert_select 'select#post_created_at_2i'
+    assert_select 'select#post_created_at_3i'
+    assert_select 'select#post_created_at_4i'
+    assert_select 'select#post_created_at_5i'
+
+    # Post#status: Selector
     assert_select 'label[for="post_status"]'
     assert_select 'select#post_status'
+
+    # We have 1 template
+
+    # Post#published_at: Datetime
+    assert_match "templates#datepicker_template_published_at", @response.body
 
   end
 
