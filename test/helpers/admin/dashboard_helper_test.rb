a543686b9fa03d4b98db1669ac4e0cb728696bc7
Francesc Esplugas <francesc@intraducibles.com>
2010-09-18 17:35:08 +0200

    We are now using another method ...

--- test/helpers/admin/dashboard_helper_test.rb
+++ test/helpers/admin/dashboard_helper_test.rb
@@ -9,25 +9,6 @@ class Admin::DashboardHelperTest < ActiveSupport::TestCase
 
   def render(*args); args; end
 
-=begin
-
-  # FIXME: Pending to verify the applications included. Use the keys.
-  should "render applications" do
-    @current_user = Factory(:typus_user)
-
-    output = applications
-    partial = "admin/helpers/dashboard/applications"
-    options = { :applications => { [ "Site", [ "Asset", "Page" ] ] => nil,
-                                   [ "System", [ "Delayed::Task" ] ] => nil,
-                                   [ "Blog", [ "Comment", "Post" ] ] => nil,
-                                   [ "Typus", [ "TypusUser" ] ] => nil } }
-
-    assert_equal partial, output.first
-    assert_equal options, output.last
-  end
-
-=end
-
   should "render resources" do
     @current_user = Factory(:typus_user)
 
