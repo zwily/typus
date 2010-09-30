2761fdf7c74a3c00d3a8f744747b0146816f792d
Francesc Esplugas <francesc@intraducibles.com>
2010-09-30 18:03:01 +0200

    This is broken on Rails 3, so I had to change the implementation

--- app/helpers/admin/resources_helper.rb
+++ app/helpers/admin/resources_helper.rb
@@ -16,13 +16,9 @@ module Admin
     # it's build from the options defined on the yaml configuration file.
     #++
     def build_list(model, fields, items, resource = @resource.to_resource, link_options = {}, association = nil)
-      template = "app/views/admin/#{resource}/_#{resource.singularize}.html.erb"
-
-      if File.exist?(template)
-        render :partial => template.gsub('/_', '/'), :collection => items, :as => :item
-      else
-        build_table(model, fields, items, link_options, association)
-      end
+      render "admin/#{resource}/list", :items => items
+    rescue ActionView::MissingTemplate
+      build_table(model, fields, items, link_options, association)
     end
 
     def display_link_to_previous
