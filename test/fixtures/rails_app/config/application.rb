b43ab03da54ac848d5d08af56a1b575c2965fb05
Francesc Esplugas <francesc@intraducibles.com>
2010-09-30 17:24:57 +0200

    Do not remove this

--- test/fixtures/rails_app/config/application.rb
+++ test/fixtures/rails_app/config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-require "typus"
-
 module RailsApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -47,4 +45,9 @@ module RailsApp
   end
 end
 
+##
+# NOTE: This is here to fake the first request!
+##
+
+require "typus"
 Typus.reload!
