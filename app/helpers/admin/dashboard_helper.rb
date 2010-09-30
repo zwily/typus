f9fd3fbda2963400417b1d044edd119595d887d5
Francesc Esplugas <francesc@intraducibles.com>
2010-09-30 14:52:46 +0200

    The partial will decide if prints resources

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -10,9 +10,6 @@ module Admin
       available = Typus.resources.map do |resource|
                     resource if @current_user.resources.include?(resource)
                   end.compact
-
-      return if available.empty?
-
       render File.join(path, "resources"), :resources => available
     end
 
