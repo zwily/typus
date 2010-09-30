6a0359191ef1e78b82708f23bb98cede03fc41f8
Francesc Esplugas <francesc@intraducibles.com>
2010-09-30 16:41:46 +0200

    Remove unused method

--- lib/support/string.rb
+++ lib/support/string.rb
@@ -12,15 +12,4 @@ class String
     remove_prefix.camelize.classify.constantize
   end
 
-  #--
-  # OPTIMIZE: Find a way to remove the rescue.
-  #++
-  def typus_defaults_for(filter)
-    if settings = Typus::Configuration.config[self][filter.to_s]
-      settings.extract_settings
-    else
-      []
-    end
-  end
-
 end
