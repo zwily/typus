a9f4ee99cbf6bcbfc3e8dbbc060c6df14f795482
Francesc Esplugas <francesc@intraducibles.com>
2010-10-06 15:17:20 +0200

    Fixed db connections ...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,3 @@
-
 ENV["RAILS_ENV"] = "test"
 
 # Boot rails application and testing parts ...
@@ -9,17 +8,6 @@ require "rails/test_help"
 # configurations to get the custom paths.
 Typus.reload!
 
-# Different DB settings and load our schema.
-connection = case ENV["DB"]
-             when "mysql"
-               { :adapter => "mysql", :database => "typus_test" }
-             when "postgresql"
-               { :adapter => "postgresql", :database => "typus_test", :encoding => "unicode" }
-             else
-               { :adapter => "sqlite3", :database => "db/test.sqlite3" }
-             end
-
-ActiveRecord::Base.establish_connection(connection)
 load File.join(File.dirname(__FILE__), "schema.rb")
 Dir[File.join(File.dirname(__FILE__), "factories", "**","*.rb")].each { |factory| require factory }
 
