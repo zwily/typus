9e034d05aba01a0137c4cfc31f9dce4b1744269e
Francesc Esplugas <francesc@intraducibles.com>
2010-10-12 11:45:57 +0200

    Cleaned test

--- test/lib/support/array_test.rb
+++ test/lib/support/array_test.rb
@@ -5,11 +5,7 @@ class ArrayTest < ActiveSupport::TestCase
   should "verify to_hash_with" do
     keys, values = %w(a b c), %w(1 2 3)
     expected = { "a" => "1", "b" => "2", "c" => "3" }
-
-    hash = keys.to_hash_with(values)
-
-    assert hash.kind_of?(Hash)
-    assert_equal expected, hash
+    assert_equal expected, keys.to_hash_with(values)
   end
 
 end
