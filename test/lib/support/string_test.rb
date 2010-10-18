922770d5f912743f02c3b01f61e365bdece4630b
Francesc Esplugas <francesc@intraducibles.com>
2010-10-18 20:31:51 +0200

    Contexts

--- test/lib/support/string_test.rb
+++ test/lib/support/string_test.rb
@@ -7,22 +7,33 @@ class StringTest < ActiveSupport::TestCase
     assert_equal %w( a b c ), " a  , b,  c ".extract_settings
   end
 
-  should "remove prefix" do
-    assert_equal "posts", "admin/posts".remove_prefix
-    assert_equal "typus_users", "admin/typus_users".remove_prefix
-    assert_equal "delayed/jobs", "admin/delayed/jobs".remove_prefix
+  context "remove_prefix" do
+
+    should "remove admin by default" do
+      assert_equal "posts", "admin/posts".remove_prefix
+      assert_equal "typus_users", "admin/typus_users".remove_prefix
+      assert_equal "delayed/jobs", "admin/delayed/jobs".remove_prefix
+    end
+
+    should "remove what we want to" do
+      assert_equal "posts", "typus/posts".remove_prefix("typus/")
+      assert_equal "typus_users", "typus/typus_users".remove_prefix("typus/")
+      assert_equal "delayed/tasks", "typus/delayed/tasks".remove_prefix("typus/")
+    end
+
   end
 
-  should "remove prefix with params" do
-    assert_equal "posts", "typus/posts".remove_prefix("typus/")
-    assert_equal "typus_users", "typus/typus_users".remove_prefix("typus/")
-    assert_equal "delayed/tasks", "typus/delayed/tasks".remove_prefix("typus/")
-  end
+  context "extract_class" do
 
-  should "extract_class" do
-    assert_equal Post, "admin/posts".extract_class
-    assert_equal TypusUser, "admin/typus_users".extract_class
-    assert_equal Delayed::Task, "admin/delayed/tasks".extract_class
+    should "work for models" do
+      assert_equal Post, "admin/posts".extract_class
+      assert_equal TypusUser, "admin/typus_users".extract_class
+    end
+
+    should "work for namespaced models" do
+      assert_equal Delayed::Task, "admin/delayed/tasks".extract_class
+    end
+
   end
 
 end
