2582cb9c3accd2452ec6657c1f74f91ad892ddec
Francesc Esplugas <francesc@intraducibles.com>
2010-10-18 20:07:53 +0200

    After requiring all the stuff, reload Typus

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,6 @@ ENV["RAILS_ENV"] = "test"
 require "fixtures/rails_app/config/environment"
 require "rails/test_help"
 
-# As we are simulating the application we need to reload the
-# configurations to get the custom paths.
-Typus.reload!
-
 load File.join(File.dirname(__FILE__), "schema.rb")
 require 'factories'
 
@@ -15,3 +11,6 @@ class ActiveSupport::TestCase
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
 end
+
+# As we are mocking a Rails Application, we need to load configurations.
+Typus.reload!
