f3870f8c03d8d68b1bb974c85aac99aa9f66f8bc
Francesc Esplugas <francesc@intraducibles.com>
2010-10-22 18:26:33 +0200

    Direct array generation ...

--- lib/typus/pagination.rb
+++ lib/typus/pagination.rb
@@ -6,9 +6,8 @@ module Typus
       previous_label = "&larr; " + _t("Previous")
       next_label = _t("Next") + " &rarr;"
 
-      html = []
-      html << previous_or_next_page(@collection.previous_page, previous_label, 'left')
-      html << previous_or_next_page(@collection.next_page, next_label, 'right')
+      html = [previous_or_next_page(@collection.previous_page, previous_label, 'left'),
+              previous_or_next_page(@collection.next_page, next_label, 'right')]
 
       html_container(html)
     end
