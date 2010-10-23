a842e936a343cfb15dcaf9a0b5ee3622849d188b
masone (Christian Felder) <ema@rh-productions.ch>
2010-10-24 00:58:59 +0800

    Cleaner admin route definition

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 
   end
 
-  match ':controller(/:action(/:id))', :controller => /admin\/[^\/]+/
-  match ':controller(/:action(.:format))', :controller => /admin\/[^\/]+/
+  match ':controller(/:action(/:id(.:format)))', :controller => /admin\/[^\/]+/
 
 end
