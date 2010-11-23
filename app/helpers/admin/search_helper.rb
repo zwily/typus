4d0a196d73db71b06233f65e419047ae7a30b834
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-11-23 23:30:30 +0100

    Refactored Admin::SearchHelper.search(...)

--- app/helpers/admin/search_helper.rb
+++ app/helpers/admin/search_helper.rb
@@ -3,15 +3,12 @@ module Admin
   module SearchHelper
 
     def search(resource = @resource)
-      typus_search = resource.typus_defaults_for(:search)
-      return if typus_search.empty?
-
-      search_by = typus_search.map { |x| resource.human_attribute_name(x) }.to_sentence
-      search_params = params.dup
-      %w(action controller id search page utf8).each { |p| search_params.delete(p) }
-      hidden_params = search_params.map { |k, v| hidden_field_tag(k, v) }
-
-      render "admin/helpers/search/search", :hidden_params => hidden_params, :search_by => search_by
+      if (typus_search = resource.typus_defaults_for(:search)) && typus_search.any?
+        search_params = params.dup
+        %w(action controller id search page utf8).each { |p| search_params.delete(p) }
+        options = { :hidden_params => search_params.map { |k, v| hidden_field_tag(k, v) } }
+        render "admin/helpers/search/search", options
+      end
     end
 
   end
