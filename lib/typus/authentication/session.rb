890010662ae33b5e5f47740301cdeec5cd7d1715
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-03 21:12:33 +0100

    Do not use explicit return

--- lib/typus/authentication/session.rb
+++ lib/typus/authentication/session.rb
@@ -67,9 +67,7 @@ module Typus
       # It works on models, so its available on the `resources_controller`.
       #++
       def check_if_user_can_perform_action_on_resources
-        return if @item.is_a?(Typus.user_class)
-
-        unless current_user.can?(params[:action], @resource)
+        if !@item.is_a?(Typus.user_class) && current_user.cannot?(params[:action], @resource)
           message = _t("%{current_user_role} is not able to perform this action. (%{action})",
                        :current_user_role => current_user.role.capitalize,
                        :action => params[:action])
