af8c20cace3028644388c9243e6b84e9d141c561
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-03 20:52:01 +0100

    Updated tests ...

--- test/functional/admin/typus_users_controller_test.rb
+++ test/functional/admin/typus_users_controller_test.rb
@@ -2,44 +2,41 @@ require "test_helper"
 
 class Admin::TypusUsersControllerTest < ActionController::TestCase
 
-  context "Master role" do
+  context "Admin" do
 
     setup do
-      Typus.master_role = 'admin'
       @typus_user = Factory(:typus_user)
       @typus_user_editor = Factory(:typus_user, :email => "editor@example.com", :role => "editor")
       @request.session[:typus_user_id] = @typus_user.id
       @request.env['HTTP_REFERER'] = '/admin/typus_users'
     end
 
-    should "allow_admin_to_create_typus_users" do
+    should "be able to render new" do
       get :new
       assert_response :success
     end
 
-    should "not allow admin to toogle her status" do
+    should "not be able to toogle his status" do
       get :toggle, { :id => @typus_user.id, :field => 'status' }
-
       assert_response :redirect
       assert_redirected_to @request.env['HTTP_REFERER']
       assert_equal "You can't toggle your status.", flash[:notice]
     end
 
-    should "allow_admin_to_toggle_other_users_status" do
+    should "be able to toggle other users status" do
       get :toggle, { :id => @typus_user_editor.id, :field => 'status' }
-
       assert_response :redirect
       assert_redirected_to @request.env['HTTP_REFERER']
       assert_equal "Typus user status changed.", flash[:notice]
     end
 
-    should "not be able to destroy herself" do
+    should "not be able to destroy himself" do
       assert_raises RuntimeError do
         delete :destroy, :id => @typus_user.id
       end
     end
 
-    should "verify_admin_can_destroy_others" do
+    should "be able to destroy other users" do
       assert_difference('TypusUser.count', -1) do
         delete :destroy, :id => @typus_user_editor.id
       end
@@ -49,36 +46,11 @@ class Admin::TypusUsersControllerTest < ActionController::TestCase
       assert_equal "Typus user successfully removed.", flash[:notice]
     end
 
-    should "allow admin to change other users role" do
+    should "be able to update other users role" do
       post :update, { :id => @typus_user_editor.id, :typus_user => { :role => 'admin' } }
-
       assert_response :redirect
       assert_redirected_to "/admin/typus_users/edit/#{@typus_user_editor.id}"
       assert_equal "Typus user successfully updated.", flash[:notice]
-    end
-
-  end
-
-  context "No master role" do
-
-    setup do
-      @typus_user = Factory(:typus_user, :role => "editor")
-      @request.session[:typus_user_id] = @typus_user.id
-      @request.env['HTTP_REFERER'] = '/admin/typus_users'
-    end
-
-    should "not_allow_non_root_typus_user_to_toggle_status" do
-      assert_raises RuntimeError do
-        get :toggle, { :id => @typus_user.id, :field => 'status' }
-      end
-    end
-
-    should "not_allow_editor_to_create_typus_users" do
-      get :new
-
-      assert_response :redirect
-      assert_redirected_to @request.env['HTTP_REFERER']
-      assert_equal "Editor is not able to perform this action. (new)", flash[:notice]
     end
 
   end
@@ -87,66 +59,74 @@ class Admin::TypusUsersControllerTest < ActionController::TestCase
 
     setup do
       @request.env['HTTP_REFERER'] = '/admin/typus_users'
-      @typus_user = Factory(:typus_user, :email => "editor@example.com", :role => "editor")
-      @request.session[:typus_user_id] = @typus_user.id
+      @editor = Factory(:typus_user, :email => "editor@example.com", :role => "editor")
+      @request.session[:typus_user_id] = @editor.id
     end
 
-    should "allow_editor_to_update_himself" do
-      post :update, { :id => @typus_user.id, :typus_user => { :role => 'editor' } }
-
+    should "not_allow_editor_to_create_typus_users" do
+      get :new
       assert_response :redirect
-      assert_redirected_to "/admin/typus_users/edit/#{@typus_user.id}"
+      assert_redirected_to @request.env['HTTP_REFERER']
+      assert_equal "Editor is not able to perform this action. (new)", flash[:notice]
+    end
+
+    should "be able to edit his profile" do
+      get :edit, { :id => @editor.id }
+      assert_response :success
+    end
+
+    should "be able to update his profile" do
+      post :update, { :id => @editor.id, :typus_user => { :role => 'editor' } }
+      assert_response :redirect
+      assert_redirected_to "/admin/typus_users/edit/#{@editor.id}"
       assert_equal "Typus user successfully updated.", flash[:notice]
     end
 
-    should "not_allow_editor_to_update_himself_to_become_admin" do
-      post :update, { :id => @typus_user.id, :typus_user => { :role => 'admin' } }
-
+    should "not be able to update his profile and become root" do
+      post :update, { :id => @editor.id, :typus_user => { :role => 'admin' } }
       assert_response :redirect
       assert_redirected_to @request.env['HTTP_REFERER']
       assert_equal "You can't change your role.", flash[:notice]
     end
 
-    should "not_allow_editor_to_edit_other_users_profiles" do
+    should "not be able to destroy his profile" do
       assert_raises RuntimeError do
-        get :edit, { :id => Factory(:typus_user).id }
+        delete :destroy, :id => @editor.id
       end
     end
 
-    should "not_allow_editor_to_destroy_users" do
+    should "not be able to toggle other his status" do
       assert_raises RuntimeError do
-        delete :destroy, :id => Factory(:typus_user).id
+        get :toggle, { :id => @editor.id, :field => 'status' }
       end
     end
 
-    should "not_allow_editor_to_destroy_herself" do
+    should "not be able to edit other profiles" do
+      user = Factory(:typus_user)
       assert_raises RuntimeError do
-        delete :destroy, :id => @typus_user.id
+        get :edit, { :id => user.id }
       end
     end
 
-    should "change_root_to_editor_so_editor_can_edit_others_content" do
-      Typus.expects(:master_role).at_least_once.returns("editor")
-      get :edit, { :id => Factory(:typus_user).id }
-      assert_response :success
+    should_eventually "not be able to update other profiles" do
+      user = Factory(:typus_user)
+      assert_raises RuntimeError do
+        post :update, { :id => user.id, :typus_user => { :role => 'admin' } }
+      end
     end
 
-  end
-
-  context "Designer" do
-
-    setup do
-      @request.env['HTTP_REFERER'] = '/admin'
-      @typus_user = Factory(:typus_user, :role => "designer")
-      @request.session[:typus_user_id] = @typus_user.id
+    should "not be able to destroy other profiles" do
+      user = Factory(:typus_user)
+      assert_raises RuntimeError do
+        delete :destroy, :id => user.id
+      end
     end
 
-    should "redirect_to_admin_dashboard_if_user_does_not_have_privileges" do
-      get :index
-
-      assert_response :redirect
-      assert_redirected_to @request.env['HTTP_REFERER']
-      assert_equal "Designer is not able to perform this action. (index)", flash[:notice]
+    should "not be able to toggle other profiles status" do
+      user = Factory(:typus_user)
+      assert_raises RuntimeError do
+        get :toggle, { :id => user.id, :field => 'status' }
+      end
     end
 
   end
