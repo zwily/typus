41b0aed4264380b5c1bdf8482e767aea9997c150
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-12 11:26:47 +0100

    Generate controller from the controller generator

--- lib/generators/typus/migration_generator.rb
+++ lib/generators/typus/migration_generator.rb
@@ -1,4 +1,5 @@
 require "rails/generators/migration"
+require "generators/typus/controller_generator"
 
 module Typus
 
@@ -63,12 +64,8 @@ Examples:
         template "config/typus/typus_roles.yml", "config/typus/typus_roles.yml"
       end
 
-      def generate_controllers
-        invoke "controller", ["admin/#{admin_users_table_name}"]
-      end
-
-      def make_controllers_inherit_from_resources_controller
-        gsub_file "app/controllers/admin/#{admin_users_table_name}_controller.rb", /ApplicationController/, "Admin::ResourcesController"
+      def generate_controller
+        Typus::Generators::ControllerGenerator.new([options[:user_class_name].pluralize]).invoke_all
       end
 
       def generate_migration
