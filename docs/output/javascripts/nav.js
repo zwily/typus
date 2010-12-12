a841960738b3c817661584cf3d4dc0d7881fb5a0
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-12 22:10:52 +0100

    [docs] Table of contents

---
+++ docs/output/javascripts/nav.js
@@ -0,0 +1,77 @@
+function create_menu(basepath)
+{
+
+	document.write(
+
+		'<table cellpadding="0" cellspaceing="0" border="0" style="width:98%"><tr>' +
+		'<td class="td" valign="top">' +
+
+		'<h3>Basic Info</h3>' +
+		'<ul>' +
+		'<li><a href="#">Server Requirements</a></li>' +
+		'<li><a href="#">License Agreement</a></li>' +
+		'<li><a href="#">Change Log</a></li>' +
+		'<li><a href="#">Credits</a></li>' +
+		'</ul>' +	
+		
+		'<h3>Installation</h3>' +
+		'<ul>' +
+		'<li><a href="#">Downloading Typus</a></li>' +
+		'<li><a href="#">Installation Instructions</a></li>' +
+		'<li><a href="#">Upgrading from a Previous Version</a></li>' +
+		'<li><a href="#">Troubleshooting</a></li>' +
+		'</ul>' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Overview</h3>' +
+		'<ul>' +
+		'<li><a href="#">Getting Started</a></li>' +
+		'<li><a href="#">Typus at a Glance</a></li>' +
+		'<li><a href="#">Supported Features</a></li>' +
+		'<li><a href="#">Architectural Goals</a></li>' +
+		'</ul>' +	
+
+		'<h3>Documentation</h3>' +
+		'<ul>' +
+		'<li><a href="/documentation/installation/">Installing Typus</a></li>' +
+		'<li><a href="/documentation/configuration/">Main Configuration</a></li>' +
+		'<li><a href="/documentation/configuration-resources/">Resources Configuration</a></li>' +
+		'<li><a href="/documentation/configuration-resource/">Resource Configuration</a></li>' +
+		'<li><a href="/documentation/configuration-roles/">Roles Configuration</a></li>' +
+		'<li><a href="/documentation/configuration-ui/">User Interface Customization</a></li>' +
+		'</ul>' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Recipes</h3>' +
+		'<ul>' +
+		'<li><a href="/recipes/rich-text-editor/">Adding a Rich Text Editor</a></li>' +
+		'<li><a href="/recipes/custom-actions/">Custom Actions</a></li>' +
+		'<li><a href="/recipes/single-table-inheritance/">Single Table Inheritance</a></li>' +
+		'<li><a href="/recipes/namespaced-models/">Namespaced Models</a></li>' +
+		'<li><a href="/recipes/configuration-files/">Managing Configuration Files</a></li>' +
+		'<li><a href="/recipes/testing-the-plugin/">Testing the Plugin</a></li>' +
+		'</ul>' +
+
+		'<h3>Recipes Published Elsewhere</h3>' +
+		'<ul>' +
+		'<li><a href="http://www.joshcrews.com/2010/03/using-heroku-to-host-your-rails-cms/">Using Heroku to host your Rails CMS</a></li>' +
+		'<li><a href="http://ruby-toolbox.com/categories/rails_admin_interfaces.html">Rails Admin Interfaces in the Ruby Toolbox</a></li>' +
+		'</ul>' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Additional Resources</h3>' +
+		'<ul>' +
+		'<li><a href="http://rubygems.org/gems/typus">Gems</a></li>' +
+		'<li><a href="http://github.com/fesplugas/typus">Plugin Source Code</a></li>' +
+		'<li><a href="http://groups.google.com/group/typus">Mailing List</a></li>' +
+		'<li><a href="http://github.com/fesplugas/typus/issues">Bug reports</a></li>' +
+		'<li><a href="http://github.com/fesplugas/typus/contributors">Contributors List</a></li>' +
+		'<li><a href="http://pledgie.com/campaigns/11233">Donate</a></li>' +
+		'</ul>' +	
+
+		'</td></tr></table>');
+
+}
