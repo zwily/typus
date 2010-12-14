f37e7427d66f93601391c504fa531dbe64613af5
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-14 08:42:54 +0100

    I still prefer redirecting to edit after update

--- lib/typus/resources.rb
+++ lib/typus/resources.rb
@@ -13,7 +13,7 @@ module Typus
     end
 
     mattr_accessor :action_after_save
-    @@action_after_save = "index"
+    @@action_after_save = "edit"
 
     mattr_accessor :default_action_on_item
     @@default_action_on_item = "edit"
