0de41f2995f1d6ae7cf0e0ee0a65dd0fec858df5
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-14 17:28:10 +0100

    Small refactor to add clarity to code

--- app/helpers/admin/table_helper.rb
+++ app/helpers/admin/table_helper.rb
@@ -167,13 +167,14 @@ module Admin
     def table_position_field(attribute, item, connector = " / ")
       html_position = []
 
-      [ [:move_to_top, "Top"],
-        [:move_higher,  "Up"],
-        [:move_lower,   "Down"],
-        [:move_to_bottom, "Bottom"] ].each do |key, value|
+      actions_on_list = {:move_to_top => "Top", :move_higher => "Up", :move_lower => "Down", :move_to_bottom => "Bottom"}
 
-        options = { :controller => "/admin/#{item.class.to_resource}", :action => "position", :id => item.id, :go => key }
-        should_be_inactive = (item.respond_to?(:first?) && ([:move_higher, :move_to_top].include?(key) && item.first?)) || 
+      actions_on_list.each do |key, value|
+        options = { :controller => "/admin/#{item.class.to_resource}",
+                    :action => "position",
+                    :id => item.id,
+                    :go => key }
+        should_be_inactive = (item.respond_to?(:first?) && ([:move_higher, :move_to_top].include?(key) && item.first?)) ||
                              (item.respond_to?(:last?) &&  ([:move_lower, :move_to_bottom].include?(key) && item.last?))
         html_position << link_to_unless(should_be_inactive, _t(value), params.merge(options)) do |name|
           raw %(<span class="inactive">#{name}</span>)
