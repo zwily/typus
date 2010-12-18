dcd70ac7bf132ca8a2e1f5d27943963dd64d908e
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-18 10:13:20 +0100

    truncate fields of type :text (guns)

--- app/helpers/admin/table_helper.rb
+++ app/helpers/admin/table_helper.rb
@@ -54,6 +54,7 @@ module Admin
         when :selector then table_selector(key, item)
         when :transversal then table_transversal(key, item)
         when :has_and_belongs_to_many then table_has_and_belongs_to_many_field(key, item)
+        when :text then table_text_field(key, item, link_options)
         else
           table_string_field(key, item)
         end
@@ -148,6 +149,10 @@ module Admin
     def table_transversal(attribute, item)
       _attribute, virtual = attribute.split(".")
       item.send(_attribute).send(virtual)
+    end
+
+    def table_text_field(*args)
+      truncate(table_string_field(*args))
     end
 
   end
