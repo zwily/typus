05a0922b838451a7ff5fa5b7b147b58f4c48e78c
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-22 12:06:16 +0100

    [docs] Added new pages (pending) and some reorganization

--- docs/output/javascripts/nav.js
+++ docs/output/javascripts/nav.js
@@ -6,31 +6,31 @@ function create_menu(basepath)
 		'<table cellpadding="0" cellspaceing="0" border="0" style="width:98%"><tr>' +
 		'<td class="td" valign="top">' +
 
+		'<h3>Overview</h3>' +
+		'<ul>' +
+		'<li><a href="/overview/getting-started/">Getting Started</a></li>' +
+		'<li><a href="/overview/typus-at-a-glance/">Typus at a Glance</a></li>' +
+		'<li><a href="/overview/supported-features/">Supported Features</a></li>' +
+		'<li><a href="/overview/architectural-goals/">Architectural Goals</a></li>' +
+		'</ul>' +	
+
 		'<h3>Basic Info</h3>' +
 		'<ul>' +
-		'<li><a href="#">Server Requirements</a></li>' +
-		'<li><a href="#">License Agreement</a></li>' +
-		'<li><a href="#">Change Log</a></li>' +
-		'<li><a href="#">Credits</a></li>' +
+		'<li><a href="/requirements/">Requirements</a></li>' +
+		'<li><a href="/mit-license/">MIT License</a></li>' +
+		'<li><a href="/changelog/">Change Log</a></li>' +
+		'<li><a href="/credits/">Credits</a></li>' +
 		'</ul>' +	
-		
+
+		'</td><td class="td_sep" valign="top">' +
+
 		'<h3>Installation</h3>' +
 		'<ul>' +
 		'<li><a href="#">Downloading Typus</a></li>' +
 		'<li><a href="#">Installation Instructions</a></li>' +
-		'<li><a href="#">Upgrading from a Previous Version</a></li>' +
-		'<li><a href="#">Troubleshooting</a></li>' +
+		'<li><a href="/installation/upgrading/">Upgrading from a Previous Version</a></li>' +
+		'<li><a href="/installation/troubleshooting/">Troubleshooting</a></li>' +
 		'</ul>' +
-
-		'</td><td class="td_sep" valign="top">' +
-
-		'<h3>Overview</h3>' +
-		'<ul>' +
-		'<li><a href="#">Getting Started</a></li>' +
-		'<li><a href="#">Typus at a Glance</a></li>' +
-		'<li><a href="#">Supported Features</a></li>' +
-		'<li><a href="#">Architectural Goals</a></li>' +
-		'</ul>' +	
 
 		'<h3>Documentation</h3>' +
 		'<ul>' +
@@ -44,30 +44,27 @@ function create_menu(basepath)
 
 		'</td><td class="td_sep" valign="top">' +
 
+		'<h3>Customization</h3>' +
+		'<ul>' +
+		'<li><a href="/documentation/configuration-ui/">User Interface</a></li>' +
+		'</ul>' +
+
 		'<h3>Recipes</h3>' +
 		'<ul>' +
 		'<li><a href="/recipes/rich-text-editor/">Adding a Rich Text Editor</a></li>' +
 		'<li><a href="/recipes/custom-actions/">Custom Actions</a></li>' +
 		'<li><a href="/recipes/single-table-inheritance/">Single Table Inheritance</a></li>' +
 		'<li><a href="/recipes/namespaced-models/">Namespaced Models</a></li>' +
-		'<li><a href="/recipes/configuration-files/">Managing Configuration Files</a></li>' +
+		'<li><a href="/recipes/configuration-files/">Configuration Files</a></li>' +
 		'<li><a href="/recipes/testing-the-plugin/">Testing the Plugin</a></li>' +
-		'</ul>' +
-
-		'<h3>Recipes Published Elsewhere</h3>' +
-		'<ul>' +
-		'<li><a href="http://www.joshcrews.com/2010/03/using-heroku-to-host-your-rails-cms/">Using Heroku to host your Rails CMS</a></li>' +
-		'</ul>' +
-
-		'<h3>Rails Application Templates</h3>' +
-		'<ul>' +
-		'<li><a href="/desmond.rb">Desmond</a></li>' +
 		'</ul>' +
 
 		'</td><td class="td_sep" valign="top">' +
 
 		'<h3>Additional Resources</h3>' +
 		'<ul>' +
+		'<li><a href="/desmond.rb">Application Template</a></li>' +
+		'<li><a href="http://demo.typuscms.com/">Application Demo</a></li>' +
 		'<li><a href="http://rubygems.org/gems/typus">Gems</a></li>' +
 		'<li><a href="http://github.com/fesplugas/typus">Plugin Source Code</a></li>' +
 		'<li><a href="http://groups.google.com/group/typus">Mailing List</a></li>' +
@@ -75,7 +72,12 @@ function create_menu(basepath)
 		'<li><a href="http://github.com/fesplugas/typus/contributors">Contributors List</a></li>' +
 		'<li><a href="http://pledgie.com/campaigns/11233">Donate</a></li>' +
 		'<li><a href="http://ruby-toolbox.com/categories/rails_admin_interfaces.html">Ruby Toolbox</a></li>' +
-		'</ul>' +	
+		'</ul>' +
+
+		'<h3>Elsewhere</h3>' +
+		'<ul>' +
+		'<li><a href="http://www.joshcrews.com/2010/03/using-heroku-to-host-your-rails-cms/">Using Heroku to host your Rails CMS</a></li>' +
+		'</ul>' +
 
 		'</td></tr></table>');
 
