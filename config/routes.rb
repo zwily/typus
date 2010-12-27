d8a98905b231a582af3edc0bcfc34ccd66871dcf
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-27 16:30:55 +0100

    Cleaned routes to make them more engine friendly

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
 
   scope "admin", :module => :admin, :as => "admin" do
 
-    match "/" => redirect("/admin/dashboard")
+    match "/" => "dashboard#show", :as => "dashboard"
     match "user_guide" => "base#user_guide"
-
-    resource :dashboard, :only => [:show], :controller => :dashboard
 
     if Typus.authentication == :session
       resource :session, :only => [:new, :create, :destroy], :controller => :session
