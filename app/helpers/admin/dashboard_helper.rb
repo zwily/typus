46db4118336053191d8598fdd14d4c255ad88e4b
Francesc Esplugas <francesc.esplugas@gmail.com>
2010-12-27 13:13:53 +0100

    Do not set the path from a method

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -3,20 +3,14 @@ module Admin
   module DashboardHelper
 
     def applications
-      render File.join(path, "applications")
+      render "admin/helpers/dashboard/applications"
     end
 
     def resources(admin_user)
       available = Typus.resources.map do |resource|
                     resource if admin_user.resources.include?(resource)
                   end.compact
-      render File.join(path, "resources"), :resources => available
-    end
-
-    private
-
-    def path
-      "admin/helpers/dashboard"
+      render "admin/helpers/dashboard/resources", :resources => available
     end
 
   end
