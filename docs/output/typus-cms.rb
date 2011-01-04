c15dd79b91f22ef2056c5bf0bd65fb56674c8e34
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-04 16:22:51 +0100

    My version of acts_as_trashable

--- docs/output/typus-cms.rb
+++ docs/output/typus-cms.rb
@@ -11,7 +11,7 @@
 
 gem 'acts_as_list'
 gem 'acts_as_tree'
-gem 'acts_as_trashable'
+gem 'acts_as_trashable', :git => 'https://github.com/fesplugas/rails-acts_as_trashable.git'
 gem 'dragonfly', '~>0.8.1'
 gem 'typus', :git => "https://github.com/fesplugas/typus.git"
 gem 'rack-cache', :require => 'rack/cache'
