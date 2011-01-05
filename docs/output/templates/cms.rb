40830411c8488351b7205e4a24e71d7ed03697ea
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-05 12:02:38 +0100

    Updated comments

--- docs/output/templates/cms.rb
+++ docs/output/templates/cms.rb
@@ -1,5 +1,5 @@
 ##
-# Generate a Rails application with is something like a CMS.
+# Generate a Rails application which is something like a CMS.
 #
 #     $ rails new rails-app-cms -m http://core.typuscms.com/templates/cms.rb
 #
@@ -11,7 +11,7 @@
 #
 # Enjoy!
 
-# Add gems to Gemfile
+# Add gems to Gemfile.
 apply "http://core.typuscms.com/templates/extras/gems.rb"
 
 # Run generators.
@@ -25,3 +25,4 @@ rake "db:migrate"
 
 # Load extra templates.
 apply "http://core.typuscms.com/templates/extras/ckeditor.rb"
+apply "http://core.typuscms.com/templates/extras/themes.rb"
