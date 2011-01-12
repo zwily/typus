bed6d0eff2f73bb72d48c2a8f3f322745d3a295a
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-12 10:05:57 +0100

    Explain why category model is used

--- test/fixtures/rails_app/app/models/category.rb
+++ test/fixtures/rails_app/app/models/category.rb
@@ -1,11 +1,14 @@
+##
+# This model is used to test:
+#
+#     - ActsAsList on resources_controller
+#     - Relate and unrelate for has_and_belongs_to_many.
+#
+##
 class Category < ActiveRecord::Base
 
   acts_as_list
 
-  validates_presence_of :name
   has_and_belongs_to_many :posts
-
-  def self.typus
-  end
 
 end
