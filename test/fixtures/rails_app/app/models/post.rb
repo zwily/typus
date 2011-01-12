ec0cf201283f3f18c6bf7e7d34df7d442407a6df
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-12 10:11:23 +0100

    Sorted methods

--- test/fixtures/rails_app/app/models/post.rb
+++ test/fixtures/rails_app/app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ActiveRecord::Base
 
+  STATUS = %w(pending published unpublished)
+
   validates_presence_of :title, :body
+
+  belongs_to :favorite_comment, :class_name => "Comment"
+  belongs_to :typus_user
   has_and_belongs_to_many :categories
   has_many :assets, :as => :resource, :dependent => :destroy
   has_many :comments
   has_many :views
-
-  belongs_to :favorite_comment, :class_name => "Comment"
-  belongs_to :typus_user
-
-  STATUS = %w( pending published unpublished )
 
 end
