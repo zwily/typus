74da2d3bbce0e80d68659bfcb6c43d0178da46c7
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-16 20:47:00 +0100

    Use custom bundle_flags

--- test/fixtures/rails_app/config/deploy.rb
+++ test/fixtures/rails_app/config/deploy.rb
@@ -9,6 +9,9 @@ set :repository, "git://github.com/fesplugas/typus.git"
 set :deploy_via, :remote_cache
 set :keep_releases, 2
 
+# Override default :bundle_flags because I don't provice a Gemfile.lock.
+set :bundle_flags, "--quiet"
+
 set :domain, "demo.typuscms.com"
 
 role :web, domain
@@ -17,7 +20,6 @@ role :db, domain, :primary => true # This is where Rails migrations will run
 role :db, domain
 
 after "deploy", "deploy:cleanup"
-after "deploy:update_code", "my_bundle:install"
 
 namespace :deploy do
   task :start do ; end
@@ -26,12 +28,5 @@ namespace :deploy do
     run "cd #{current_path}/test/fixtures/rails_app && rake db:setup RAILS_ENV=production"
     run "cd #{current_path}/test/fixtures/rails_app && touch tmp/restart.txt"
   end
-end
-
-namespace :my_bundle do
-
-  task :install do
-    run "cd #{release_path} && bundle install --path #{shared_path}/bundle --quiet --without development test"
-  end
-
+  task :finalize_update do ; end
 end
