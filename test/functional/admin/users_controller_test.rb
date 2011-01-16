7cb28161e4339898b21cbb772e672e42c86fbb4d
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-16 21:55:16 +0100

    Failing test

---
+++ test/functional/admin/users_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class Admin::UsersControllerTest < ActionController::TestCase
+
+  setup do
+    @typus_user = Factory(:typus_user)
+    @request.session[:typus_user_id] = @typus_user.id
+
+    @project = Factory(:project)
+    @user = @project.user
+  end
+
+  should "be able to destroy items" do
+    get :destroy, { :id => @user.id, :method => :delete }
+
+    assert_response :redirect
+    assert_equal "User successfully removed.", flash[:notice]
+    assert_redirected_to :action => :index
+  end
+
+end
