3d867966a3bd4d33fff66e5ea206cd1648f9b2bf
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-26 19:39:25 +0100

    This is a SUPER hack but I need this working today

--- app/helpers/admin/list_helper.rb
+++ app/helpers/admin/list_helper.rb
@@ -9,9 +9,13 @@ module Admin
     def list_actions
       resources_actions.map do |body, url, options|
         if admin_user.can?(url[:action], @resource.name)
-          link_to Typus::I18n.t(body),
-                  url.merge(:layout => params[:layout]),
-                  options.merge(:target => "_parent")
+
+          this_url = params.merge(url)
+          whitelist = %w(layout CKEditor CKEditorFuncNum langCode)
+          this_url.delete_if { |k, v| !whitelist.include?(k) }
+
+          this_options = options.merge(:target => "_parent")
+          link_to Typus::I18n.t(body), this_url, this_options
         end
       end.compact.join(" / ").html_safe
     end
