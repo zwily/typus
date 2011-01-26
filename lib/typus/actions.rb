01dad8e55b1b1fc20fb4d9fc283f74ac8aeea1b1
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-26 14:06:47 +0100

    add_resources_action

--- lib/typus/actions.rb
+++ lib/typus/actions.rb
@@ -22,6 +22,26 @@ module Typus
       @resource_actions = @resource_actions.concat([options])
     end
 
+    # FIXME: I know this is ugly but first of all I want to see it working.
+
+    def add_resources_action(*args)
+      options = args.extract_options!
+      @resources_actions ||= []
+      @resources_actions << options
+    end
+
+    def prepend_resources_action(*args)
+      options = args.extract_options!
+      @resources_actions ||= []
+      @resources_actions = @resources_actions.unshift(options)
+    end
+
+    def append_resources_action(*args)
+      options = args.extract_options!
+      @resources_actions ||= []
+      @resources_actions = @resources_actions.concat([options])
+    end
+
   end
 
 end
