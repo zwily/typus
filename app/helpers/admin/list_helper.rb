26baff4c6e2a19c5f5ee24e5be4d0f22f4dcd0fb
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-27 19:37:43 +0100

    Do not use the targe

--- app/helpers/admin/list_helper.rb
+++ app/helpers/admin/list_helper.rb
@@ -10,7 +10,7 @@ module Admin
       resources_actions.map do |body, url, options|
         if admin_user.can?(url[:action], @resource.name)
           path = params.dup.merge!(url)
-          link_to Typus::I18n.t(body), path.cleanup, options.merge(:target => "_parent")
+          link_to Typus::I18n.t(body), path.cleanup, options # .merge(:target => "_parent")
         end
       end.compact.join(" / ").html_safe
     end
