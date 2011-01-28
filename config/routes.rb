55d68755c7eb30fe860272e323261ba845187dfc
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-01-28 22:42:05 +0100

    forgot_password should respond to post

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     if Typus.authentication == :session
       resource :session, :only => [:new, :create, :destroy], :controller => :session
       resources :account, :only => [:new, :create, :show, :forgot_password] do
-        collection { get :forgot_password }
+        collection do
+          get :forgot_password
+          post :forgot_password
+        end
       end
     end
 
