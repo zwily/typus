72fca23445c5ff28406482103fa713d85cc0d408
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-01 19:11:11 +0100

    Somehow I removed this ...

--- test/helpers/admin/relationships_helper_test.rb
+++ test/helpers/admin/relationships_helper_test.rb
@@ -8,6 +8,12 @@ class Admin::RelationshipsHelperTest < ActiveSupport::TestCase
   should_eventually "test typus_form_has_many"
   should_eventually "test typus_form_has_and_belongs_to_many"
   should_eventually "test build_pagination"
-  
+  should_eventually "test build_relate_form"
+  should_eventually "test build_relationship_table"
+  should_eventually "test build_add_new"
+  should_eventually "test set_condition"
+  should_eventually "test set_conditions"
+  should_eventually "test typus_form_has_one"
+  should_eventually "test typus_belongs_to_field"
 
 end
