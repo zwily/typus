901087e5e598aa13ec050e07277e7d18a545645a
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-02 10:28:39 +0100

    I don't want empty items on the arrays

--- lib/typus/actions.rb
+++ lib/typus/actions.rb
@@ -6,34 +6,34 @@ module Typus
 
     def add_resource_action(*args)
       @resource_actions ||= []
-      @resource_actions << args
+      @resource_actions << args unless args.empty?
     end
 
     def prepend_resource_action(*args)
       @resource_actions ||= []
-      @resource_actions = @resource_actions.unshift(args)
+      @resource_actions = @resource_actions.unshift(args) unless args.empty?
     end
 
     def append_resource_action(*args)
       @resource_actions ||= []
-      @resource_actions = @resource_actions.concat([args])
+      @resource_actions = @resource_actions.concat([args]) unless args.empty?
     end
 
     # FIXME: I know this is ugly but first of all I want to see it working.
 
     def add_resources_action(*args)
       @resources_actions ||= []
-      @resources_actions << args
+      @resources_actions << args unless args.empty?
     end
 
     def prepend_resources_action(*args)
       @resources_actions ||= []
-      @resources_actions = @resources_actions.unshift(args)
+      @resources_actions = @resources_actions.unshift(args) unless args.empty?
     end
 
     def append_resources_action(*args)
       @resources_actions ||= []
-      @resources_actions = @resources_actions.concat([args])
+      @resources_actions = @resources_actions.concat([args]) unless args.empty?
     end
 
   end
