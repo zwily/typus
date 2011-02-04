61b86b2026b7a76d87d63fa4f7faf077892424eb
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-04 18:01:25 +0100

    Refactored Hash tests

--- test/lib/support/hash_test.rb
+++ test/lib/support/hash_test.rb
@@ -11,32 +11,16 @@ class HashTest < ActiveSupport::TestCase
   context "cleanup" do
 
     should "work" do
-      hash = {"value"=>"1000"}
+      whitelist = %w(controller action id attribute layout resource resource_id resource_action selected back_to )
+      whitelist.each do |w|
+        expected = { w => w }
+        assert_equal expected, expected.dup.cleanup
+      end
+    end
+
+    should "reject unwanted stuff" do
+      hash = {"value"=>"1000", "CKEditor"=>"1", "CKEditorFuncNum"=>"1", "langCode"=>"en", "target"=>"_parent"}
       expected = {}
-      assert_equal expected, hash.cleanup
-    end
-
-    should "verify whitelist for basics" do
-      hash = {"controller"=>"welcome", "action"=>"index", "id" => "1"}
-      expected = hash.dup
-      assert_equal expected, hash.cleanup
-    end
-
-    should "verify whitelist for CKEditor stuff" do
-      hash = {"CKEditor"=>"1", "CKEditorFuncNum"=>"1", "langCode"=>"en"}
-      expected = hash.dup
-      assert_equal expected, hash.cleanup
-    end
-
-    should "verify whitelist for layout" do
-      hash = {"layout"=>"admin/headless"}
-      expected = hash.dup
-      assert_equal expected, hash.cleanup
-    end
-
-    should "verify whitelist for target" do
-      hash = {"target"=>"_parent"}
-      expected = hash.dup
       assert_equal expected, hash.cleanup
     end
 
