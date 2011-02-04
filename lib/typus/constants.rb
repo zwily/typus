af5be237e41a73db71ef4610eca09752c4579105
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-04 20:53:31 +0100

    Tests for the constants.

--- lib/typus/constants.rb
+++ lib/typus/constants.rb
@@ -1,6 +1,10 @@
 module Typus
 
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
-  URI_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+  module Constants
+
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+    URI_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+
+  end
 
 end
