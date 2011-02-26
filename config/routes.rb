043fd79279d633f136d680586e258253678d620d
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-26 23:21:17 +0100

    Logout with :get and not :delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     match "user_guide" => "base#user_guide"
 
     if Typus.authentication == :session
-      resource :session, :only => [:new, :create, :destroy], :controller => :session
+      resource :session, :only => [:new, :create], :controller => :session do
+        get :destroy, :as => "destroy"
+      end
+
       resources :account, :only => [:new, :create, :show, :forgot_password] do
         collection do
           get :forgot_password
