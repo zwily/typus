83a3ad991b9d3768e0a973fc3be452b9e5bdebab
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-03-03 09:24:52 +0100

    Simplified how applications are rendered on dashboard

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,10 +1,6 @@
 module Admin
   module DashboardHelper
 
-    def applications
-      render "admin/helpers/dashboard/applications"
-    end
-
     def resources(admin_user)
       available = Typus.resources.delete_if { |r| !(admin_user.is_a?(FakeUser) || admin_user.resources.include?(r)) }
       render "admin/helpers/dashboard/resources", :resources => available
