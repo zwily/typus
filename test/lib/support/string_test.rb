b20b0d06497a91043440b12139930859d5bb1981
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-23 00:17:28 +0200

    Less code

--- test/lib/support/string_test.rb
+++ test/lib/support/string_test.rb
@@ -84,16 +84,9 @@ class StringTest < ActiveSupport::TestCase
 
   end
 
-  context "to_resource" do
-
-    should "convert String into resource" do
-      assert_equal "entries", "Entry".to_resource
-    end
-
-    should "convert namespaced String into resource" do
-      assert_equal "category/entries", "Category::Entry".to_resource
-    end
-
+  test "to_resource converts String into resource" do
+    assert_equal "entries", "Entry".to_resource
+    assert_equal "category/entries", "Category::Entry".to_resource
   end
 
 end
