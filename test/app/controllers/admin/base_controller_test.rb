02bfd50f19ff8786ae76a279ab57660d8d2ae617
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-23 00:03:47 +0200

    Simplified tests

--- test/app/controllers/admin/base_controller_test.rb
+++ test/app/controllers/admin/base_controller_test.rb
@@ -5,37 +5,21 @@ require "test_helper"
   What's being tested here?
 
     - Access to user_guide.
-    - ...
 
 =end
 
 class Admin::BaseControllerTest < ActionController::TestCase
 
-  context "non-authenticated" do
-
-    should "not be able to get the user_guide" do
-      get :user_guide
-      assert_response :redirect
-      assert_redirected_to new_admin_session_path # (:back_to => '/admin/user_guide')
-    end
-
+  test "user_guide is not displayed when user is not logged in" do
+    get :user_guide
+    assert_response :redirect
+    assert_redirected_to new_admin_session_path # (:back_to => '/admin/user_guide')
   end
 
-  context "authenticated" do
-
-    setup do
-      @request.session[:typus_user_id] = Factory(:typus_user).id
-    end
-
-    teardown do
-      reset_session
-    end
-
-    should "be able to get the user_guide" do
-      get :user_guide
-      assert_response :success
-    end
-
+  test "user_guide display" do
+    admin_sign_in
+    get :user_guide
+    assert_response :success
   end
 
 end
