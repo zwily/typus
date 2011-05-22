89ff2597b79a5c3fe1ebf6c94547d462022a2952
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-22 21:30:17 +0200

    Gardening

--- test/app/helpers/admin/sidebar_helper_test.rb
+++ test/app/helpers/admin/sidebar_helper_test.rb
@@ -17,18 +17,14 @@ class Admin::SidebarHelperTest < ActiveSupport::TestCase
     default_url_options[:host] = 'test.host'
   end
 
-  context "export" do
+  test "export returns array with expected values" do
+    params = { :controller => '/admin/posts', :action => 'index' }
 
-    should "return array with expected values" do
-      params = { :controller => '/admin/posts', :action => 'index' }
+    output = export(Post , params)
+    expected = [["Export as CSV", { :action => "index", :format => "csv", :controller => "/admin/posts" }],
+                ["Export as XML", { :action => "index", :format => "xml", :controller => "/admin/posts" }]]
 
-      output = export(Post , params)
-      expected = [["Export as CSV", { :action => "index", :format => "csv", :controller => "/admin/posts" }],
-                  ["Export as XML", { :action => "index", :format => "xml", :controller => "/admin/posts" }]]
-
-      assert_equal expected, output
-    end
-
+    assert_equal expected, output
   end
 
 end
