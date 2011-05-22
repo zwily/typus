1b53da52071917add9132b593dd2ec54166c3299
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-22 22:23:24 +0200

    Less code

--- test/app/controllers/admin/status_controller_test.rb
+++ test/app/controllers/admin/status_controller_test.rb
@@ -10,45 +10,24 @@ require "test_helper"
 
 class Admin::StatusControllerTest < ActionController::TestCase
 
-  context "Admin" do
-
-    setup do
-      @request.session[:typus_user_id] = Factory(:typus_user).id
-    end
-
-    should "render index" do
-      get :index
-      assert_response :success
-      assert_template 'index'
-    end
-
+  test "admin has access" do
+    admin_sign_in
+    get :index
+    assert_response :success
+    assert_template 'index'
   end
 
-  context "Editor" do
-
-    setup do
-      @request.session[:typus_user_id] = Factory(:typus_user, :role => "editor").id
-    end
-
-    should "not render index" do
-      get :index
-      assert_response :unprocessable_entity
-    end
-
+  test "editor should not have access" do
+    editor_sign_in
+    get :index
+    assert_response :unprocessable_entity
   end
 
-  context "Not logged user" do
-
-    setup do
-      reset_session
-    end
-
-    should "not render index and redirect to new_admin_session_path with back_to" do
-      get :index
-      assert_response :redirect
-      assert_redirected_to new_admin_session_path # (:back_to => '/admin/status')
-    end
-
+  test "not logged in user is redirected to new_admin_session_path" do
+    reset_session
+    get :index
+    assert_response :redirect
+    assert_redirected_to new_admin_session_path # (:back_to => '/admin/status')
   end
 
 end
