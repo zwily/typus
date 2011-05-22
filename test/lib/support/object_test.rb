7ea96f036f8edf80446abee6aeb6c4e37a048002
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-23 00:14:58 +0200

    Less code

--- test/lib/support/object_test.rb
+++ test/lib/support/object_test.rb
@@ -2,16 +2,9 @@ require "test_helper"
 
 class ObjectTest < ActiveSupport::TestCase
 
-  context "is_sti?" do
-
-    should "return false" do
-      assert !Entry.is_sti?
-    end
-
-    should "return true" do
-      assert Case.is_sti?
-    end
-
+  test "is_sti?" do
+    assert Case.is_sti?
+    assert !Entry.is_sti?
   end
 
 end
