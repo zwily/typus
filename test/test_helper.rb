6ad327bd4589906afb941de4248256b5c85c0c93
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-05-23 00:31:32 +0200

    Make code breath

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,8 @@ ENV["RAILS_ENV"] = "test"
 
 ##
 # Boot rails_app and load the schema.
-#
+##
+
 require "fixtures/rails_app/config/environment"
 require "fixtures/rails_app/db/schema"
 
@@ -10,6 +11,7 @@ require "rails/test_help"
 require "factories"
 
 class ActiveSupport::TestCase
+
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
 
