29a717a411750056f73e43ec664b1c2a26625528
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-02 09:17:40 +0200

    Preview helper for paperclip was not behaving correctly with non image files

--- app/helpers/admin/file_preview_helper.rb
+++ app/helpers/admin/file_preview_helper.rb
@@ -63,16 +63,16 @@ module Admin
     end
 
     def typus_file_preview_for_paperclip(attachment, options = {})
-      if attachment.exists?
-        styles = attachment.styles.keys
-        if styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
-          render "admin/helpers/file_preview",
-                 :preview => attachment.url(Typus.file_preview, false),
-                 :thumb => attachment.url(Typus.file_thumbnail, false),
-                 :options => options
-        else
-          link_to attachment.original_filename, attachment.url(:original, false)
-        end
+      return unless attachment.exists?
+
+      styles = attachment.styles.keys
+      if attachment.content_type =~ /^image\/.+/ && styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
+        render "admin/helpers/file_preview",
+               :preview => attachment.url(Typus.file_preview, false),
+               :thumb => attachment.url(Typus.file_thumbnail, false),
+               :options => options
+      else
+        link_to attachment.original_filename, attachment.url(:original, false)
       end
     end
 
