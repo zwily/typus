b10b7f60e7755d47896155cccbca8f11cd2b1426
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-09 19:34:14 +0200

    Reorganized assets to be able to override stuff from the application

--- app/assets/javascripts/typus/application.js
+++ app/assets/javascripts/typus/application.js
@@ -1,7 +1,3 @@
-//= require jquery
-//= require jquery_ujs
-//= require_tree ../../vendor
-//= require typus/jquery.application
-//= require typus/jquery.rails.autocomplete
-//= require typus/jquery.searchField
-//= require typus/custom
+
+// Create the file `app/assets/javascripts/typus/application.js` in your
+// application if you need/want to extend some javascript functionality.
