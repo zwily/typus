ab3d6f4237d83f15ccc52413624cb6da5c20c6a1
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-14 18:10:43 +0200

    Moved helpers to ResourcesHelper

--- app/helpers/admin/resources_helper.rb
+++ app/helpers/admin/resources_helper.rb
@@ -12,5 +12,18 @@ module Admin
       end
     end
 
+    def build_sidebar
+      resources = ActiveSupport::OrderedHash.new
+      app_name = @resource.typus_application
+
+      admin_user.application(app_name).sort { |a,b| a.typus_constantize.model_name.human <=> b.typus_constantize.model_name.human }.each do |resource|
+        klass = resource.typus_constantize
+        resources[resource] = [ link_to_unless_current(Typus::I18n.t("All #{klass.model_name.human.pluralize}"), :action => "index") ]
+        resources[resource] << link_to_unless_current(Typus::I18n.t("Add New"), :action => "new") if admin_user.can?("create", klass)
+      end
+
+      render "admin/helpers/resources/sidebar", :resources => resources
+    end
+
   end
 end
