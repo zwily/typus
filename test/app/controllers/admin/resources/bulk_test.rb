67a70a06ee29cfe21d63c0459737f72ea48949e8
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-15 17:37:00 +0200

    Test for Bulk module

---
+++ test/app/controllers/admin/resources/bulk_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+=begin
+
+  What's being tested here?
+
+    - Typus::Controller::Trash
+
+=end
+
+class Admin::EntryBulksControllerTest < ActionController::TestCase
+
+  setup do
+    admin_sign_in
+  end
+
+  test "get index will show the available bulk_actions" do
+    get :index
+    assert_equal [["Move to Trash", "bulk_destroy"]], assigns(:bulk_actions)
+  end
+
+  test "get bulk_destroy removes all items" do
+    @request.env['HTTP_REFERER'] = "/admin/entry_bulks"
+
+    10.times { Factory(:entry_bulk) }
+    items_to_destroy = EntryBulk.limit(5).map(&:id)
+    items_to_keep = EntryBulk.limit(5).offset(5).map(&:id)
+
+    get :bulk, :batch_action => "bulk_destroy", :selected_item_ids => items_to_destroy
+    assert_response :redirect
+    assert_redirected_to @request.env['HTTP_REFERER']
+
+    assert_equal items_to_keep, EntryBulk.all.map(&:id)
+  end
+
+end
