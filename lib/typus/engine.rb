de687323ebc501da7b20086f49c5dfbb2141b9ac
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-17 19:22:28 +0200

    Support for themes

--- lib/typus/engine.rb
+++ lib/typus/engine.rb
@@ -1,6 +1,9 @@
 module Admin
   class Engine < Rails::Engine
 
+    paths["app/assets"] << "app/themes/default/assets"
+    paths["app/views"] << "app/themes/default/views"
+
 #    isolate_namespace Admin
 
   end
