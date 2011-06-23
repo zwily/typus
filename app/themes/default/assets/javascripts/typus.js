1e9eb306d15d6ea2ae19b47862ecd385f40c3be3
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-23 14:10:57 +0200

    Re-organized assets

--- app/themes/default/assets/javascripts/typus.js
+++ app/themes/default/assets/javascripts/typus.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
-//= require_tree ../vendor
+//= require jquery.fancybox-1.3.4.pack.js
+//= require jquery.tokeninput.js
 //= require typus/jquery.application
 //= require typus/jquery.rails.autocomplete
 //= require typus/jquery.searchField
