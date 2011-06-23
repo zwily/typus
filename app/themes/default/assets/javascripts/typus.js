1d95734700222c6b1201727bb023a6b11c1e6163
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-23 14:14:51 +0200

    Vendor as much stuff as possible

--- app/themes/default/assets/javascripts/typus.js
+++ app/themes/default/assets/javascripts/typus.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.fancybox-1.3.4.pack.js
+//= require jquery.rails.autocomplete
+//= require jquery.searchField
 //= require jquery.tokeninput.js
 //= require typus/jquery.application
-//= require typus/jquery.rails.autocomplete
-//= require typus/jquery.searchField
 //= require typus/application
