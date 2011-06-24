2632a03c5374ba314229897d052b6d95dea46711
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-24 11:44:44 +0200

    Assign two variable in a single line.

--- app/mailers/admin/mailer.rb
+++ app/mailers/admin/mailer.rb
@@ -3,8 +3,7 @@ class Admin::Mailer < ActionMailer::Base
   default :from => Typus.mailer_sender
 
   def reset_password_link(user, url)
-    @user = user
-    @url = url
+    @user, @url = user, url
 
     mail :to => user.email,
          :subject => "[#{Typus.admin_title}] #{Typus::I18n.t("Reset password")}"
