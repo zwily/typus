80c8a5ae02b24ffa358de2d66416bbdc9937b4a8
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-24 11:40:23 +0200

    Do not use return unless

--- app/helpers/admin/resources/data_types/paperclip_helper.rb
+++ app/helpers/admin/resources/data_types/paperclip_helper.rb
@@ -20,17 +20,16 @@ module Admin::Resources::DataTypes::PaperclipHelper
   end
 
   def typus_paperclip_preview(item, attachment, options = {})
-    data = item.send(attachment)
-    return unless data.exists?
-
-    styles = data.styles.keys
-    if data.content_type =~ /^image\/.+/ && styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
-      render "admin/templates/paperclip_preview",
-             :preview => data.url(Typus.file_preview, false),
-             :thumb => data.url(Typus.file_thumbnail, false),
-             :options => options
-    else
-      link_to data.original_filename, data.url(:original, false)
+    if (data = item.send(attachment)).exists?
+      styles = data.styles.keys
+      if data.content_type =~ /^image\/.+/ && styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
+        render "admin/templates/paperclip_preview",
+               :preview => data.url(Typus.file_preview, false),
+               :thumb => data.url(Typus.file_thumbnail, false),
+               :options => options
+      else
+        link_to data.original_filename, data.url(:original, false)
+      end
     end
   end
 
