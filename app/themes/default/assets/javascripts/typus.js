e2a1dc033d3ff1252ea5ce32da3e73a43e90ac67
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-25 17:40:13 +0200

    Autocomplete is no longer in Typus

--- app/themes/default/assets/javascripts/typus.js
+++ app/themes/default/assets/javascripts/typus.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.fancybox-1.3.4.pack
-//= require jquery.rails.autocomplete
 //= require jquery.searchField
 //= require jquery.tokeninput
 //= require typus/jquery.application
