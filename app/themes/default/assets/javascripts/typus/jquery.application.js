149867cd0d1b227f6f4840db1ccb515a0dda5abb
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-25 14:23:04 +0200

    Use javascript coding guidelines

--- app/themes/default/assets/javascripts/typus/jquery.application.js
+++ app/themes/default/assets/javascripts/typus/jquery.application.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
 
-  $("#quicksearch").searchField();
+    $("#quicksearch").searchField();
 
-  $('.resource :input', document.myForm).bind("change", function() { setConfirmUnload(true); });
+    $('.resource :input', document.myForm).bind("change", function() { setConfirmUnload(true); });
 
-  $("a.fancybox").fancybox({
-    'titlePosition': 'over',
-    'type': 'image',
-    'centerOnScroll': true,
-    'scrolling': false,
-  });
-
-  $(".iframe").fancybox({
-    'width': 720,
-    'height': '90%',
-    'autoScale': false,
-    'transitionIn': 'none',
-    'transitionOut': 'none',
-    'type': 'iframe',
-    'centerOnScroll': true,
-    'scrolling': false,
-  });
-
-    $(".action-toggle").click(function() {
-      var status = this.checked;
-      $('input.action-select').each(function() { this.checked = status; });
-      $('.action-toggle').each(function() { this.checked = status; });
+    $("a.fancybox").fancybox({
+        'titlePosition': 'over',
+        'type': 'image',
+        'centerOnScroll': true,
+        'scrolling': false,
     });
 
-  if ($('textarea.ckeditor').length > 0) {
-    var data = $('textarea');
-    $.each(data, function(i) { CKEDITOR.replace(data[i].id); });
-  }
+    $(".iframe").fancybox({
+        'width': 720,
+        'height': '90%',
+        'autoScale': false,
+        'transitionIn': 'none',
+        'transitionOut': 'none',
+        'type': 'iframe',
+        'centerOnScroll': true,
+        'scrolling': false,
+    });
+
+    $(".action-toggle").click(function() {
+        var status = this.checked;
+        $('input.action-select').each(function() { this.checked = status; });
+        $('.action-toggle').each(function() { this.checked = status; });
+    });
+
+    if ($('textarea.ckeditor').length > 0) {
+        var data = $('textarea');
+        $.each(data, function(i) { CKEDITOR.replace(data[i].id); });
+    }
 
 });
 
 function setConfirmUnload(on) {
-  window.onbeforeunload = (on) ? unloadMessage : null;
+    window.onbeforeunload = (on) ? unloadMessage : null;
 }
 
 function unloadMessage() {
-  return "You have entered new data on this page. If you navigate away from this page without first saving your data, the changes will be lost.";
+    return "You have entered new data on this page. If you navigate away from this page without first saving your data, the changes will be lost.";
 }
