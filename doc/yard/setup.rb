dd07bd9748cdef98c30dfff07cf8bb5600613b25
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-06-29 08:54:46 +0200

    Sorry, but no longer yardoc

--- doc/yard/setup.rb
+++
@@ -1,9 +0,0 @@
-this_dir = File.dirname(__FILE__)
-
-YARD::Templates::Engine.register_template_path(this_dir + '/templates')
-Dir[this_dir + '/handlers/*.rb'].each do |file|
-  require File.expand_path(file)
-end
-YARD::Parser::SourceParser.parser_type = :ruby18
-
-DRAGONFLY_VERSION = "0.0.0"
