780184369961b1dc221da5cae1a35c430ef48a2f
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-07-05 18:23:31 +0200

    We now have access to 2 Gemfiles

--- test/fixtures/rails_app/config/boot.rb
+++ test/fixtures/rails_app/config/boot.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../../Gemfile', __FILE__)
+typus_gemfile = File.expand_path('../../../../../Gemfile', __FILE__)
+
+ENV['BUNDLE_GEMFILE'] = if File.exists?(typus_gemfile)
+                          typus_gemfile
+                        else
+                          File.expand_path('../../Gemfile', __FILE__)
+                        end
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
