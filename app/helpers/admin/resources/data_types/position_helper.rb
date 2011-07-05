ce4104c5b0ac19454ffc3f1b71c880def54ceb99
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-07-05 17:03:49 +0200

    Refactored table_position_field

--- app/helpers/admin/resources/data_types/position_helper.rb
+++ app/helpers/admin/resources/data_types/position_helper.rb
@@ -1,22 +1,20 @@
 module Admin::Resources::DataTypes::PositionHelper
 
   def table_position_field(attribute, item, connector = " / ")
-    html_position = []
+    locals = { :html_position => [], :connector => connector, :item => item }
+    positions = { :move_to_top => "Top", :move_higher => "Up", :move_lower => "Down", :move_to_bottom => "Bottom" }
 
-    [ [:move_to_top, "Top"],
-      [:move_higher,  "Up"],
-      [:move_lower,   "Down"],
-      [:move_to_bottom, "Bottom"] ].each do |key, value|
+    positions.each do |key, value|
+      first_item = item.respond_to?(:first?) && ([:move_higher, :move_to_top].include?(key) && item.first?)
+      last_item = item.respond_to?(:last?) &&  ([:move_lower, :move_to_bottom].include?(key) && item.last?)
 
-      options = { :controller => "/admin/#{item.class.to_resource}", :action => "position", :id => item.id, :go => key }
-      should_be_inactive = (item.respond_to?(:first?) && ([:move_higher, :move_to_top].include?(key) && item.first?)) ||
-                           (item.respond_to?(:last?) &&  ([:move_lower, :move_to_bottom].include?(key) && item.last?))
-      html_position << link_to_unless(should_be_inactive, Typus::I18n.t(value), params.merge(options)) do |name|
-        %w(<span class="inactive">#{name}</span>)
+      unless first_item || last_item
+        options = { :controller => "/admin/#{item.class.to_resource}", :action => "position", :id => item.id, :go => key }
+        locals[:html_position] << link_to(Typus::I18n.t(value), params.merge(options))
       end
     end
 
-    "#{item.position}<br/><br/>#{html_position.compact.join(connector)}".html_safe
+    render "admin/templates/position", locals
   end
 
 end
