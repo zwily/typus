70a07d499c2ec4fce64c7a5085c136cad1321f69
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-07-13 19:53:55 +0200

    rails_app will be a module

--- test/fixtures/rails_app/app/controllers/admin/views_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::ViewsController < Admin::ResourcesController
-
-  def set_context
-    @resource = Site.find_by_domain(request.host).send(@object_name.pluralize)
-  end
-  private :set_context
-
-end
