56f1797dcb2f76ff818e04c414db51088df04655
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-07-14 16:48:13 +0200

    Include formalize

--- app/assets/javascripts/typus.js
+++ app/assets/javascripts/typus.js
@@ -3,5 +3,6 @@
 //= require jquery.fancybox-1.3.4.pack
 //= require jquery.searchField
 //= require jquery.tokeninput
+//= require js/jquery.formalize.min
 //= require typus/jquery.application
 //= require typus/application
