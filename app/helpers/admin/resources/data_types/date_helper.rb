ac8d725b66d8aea1bd9c4daddba2ff90a665a08d
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-10 19:14:42 +0200

    Use the attribute for date filters instead of 'Show all dates'

--- app/helpers/admin/resources/data_types/date_helper.rb
+++ app/helpers/admin/resources/data_types/date_helper.rb
@@ -2,7 +2,7 @@ module Admin::Resources::DataTypes::DateHelper
 
   def date_filter(filter)
     values = %w(today last_few_days last_7_days last_30_days)
-    items = [[Typus::I18n.t("Show all dates"), ""]]
+    items = [[@resource.human_attribute_name(filter).capitalize, ""]]
     items += values.map { |v| [Typus::I18n.t(v.humanize), v] }
   end
 
