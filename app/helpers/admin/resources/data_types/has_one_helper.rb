406cce7ffaccb3807b93d892b9efc21f3f986725
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-22 16:06:34 +0200

    Add New link only if necessary.

--- app/helpers/admin/resources/data_types/has_one_helper.rb
+++ app/helpers/admin/resources/data_types/has_one_helper.rb
@@ -12,12 +12,14 @@ module Admin::Resources::DataTypes::HasOneHelper
     @resource_actions = [["Edit", {:action=>"edit"}, {}],
                          ["Trash", { :resource_id => @item.id, :resource => @resource.model_name, :action => "destroy" }, { :confirm => "Trash?" }]]
 
-    options = { :resource_id => nil, @reflection.foreign_key => @item.id }
+    locals = { :association_name => @association_name, :table => build_relationship_table }
 
-    render "admin/templates/has_one",
-           :association_name => @association_name,
-           :add_new => @items.empty? ? build_add_new(options) : nil,
-           :table => build_relationship_table
+    if @items.empty?
+      options = { :resource_id => nil, @reflection.foreign_key => @item.id }
+      locals[:add_new] = build_add_new(options)
+    end
+
+    render "admin/templates/has_one", locals
   end
 
 end
