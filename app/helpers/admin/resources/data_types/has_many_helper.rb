58113b008c4f66ca7ff622c3465d2074e8bae5bb
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-22 16:28:36 +0200

    Removed useless code.

--- app/helpers/admin/resources/data_types/has_many_helper.rb
+++ app/helpers/admin/resources/data_types/has_many_helper.rb
@@ -14,7 +14,7 @@ module Admin::Resources::DataTypes::HasManyHelper
   def typus_form_has_many(field)
     setup_relationship(field)
 
-    options = @reflection.through_reflection ? {} : { @reflection.foreign_key => @item.id }
+    options = { @reflection.foreign_key => @item.id }
 
     # This options should only be merged when there's a Polymorphic association.
     # options.merge!(:resource => @resource.model_name, :resource_id => @item.id, :resource_action => "relate")
@@ -22,7 +22,7 @@ module Admin::Resources::DataTypes::HasManyHelper
     count_items_to_relate = @model_to_relate.order(@model_to_relate.typus_order_by).count - @item.send(field).count
 
     build_pagination
-    @reflection.through_reflection ? set_has_many_through_resource_actions : set_has_many_resource_actions
+    set_has_many_resource_actions
 
     locals = { :association_name => @association_name,
                :add_new => build_add_new_for_has_many(@model_to_relate, field, options),
@@ -44,15 +44,6 @@ module Admin::Resources::DataTypes::HasManyHelper
   def set_has_many_resource_actions
     @resource_actions = [["Edit", { :action => "edit", :layout => 'admin/headless' }, { :class => 'iframe' }],
                          ["Trash", { :action => "destroy" }, { :confirm => "Trash?" } ]]
-  end
-
-  def set_has_many_through_resource_actions
-    @resource_actions = [["Edit", { :action => "edit", :layout => 'admin/headless' }, { :class => 'iframe' }],
-                         ["Unrelate", { :resource_id => @item.id,
-                                        :resource => @resource.model_name,
-                                        :action => "unrelate",
-                                        :association_name => @association_name},
-                                      { :confirm => "Unrelate?" } ]]
   end
 
 end
