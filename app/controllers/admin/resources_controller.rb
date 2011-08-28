b79decc8fe00eb18f560afbd99f05bd20124428e
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-08-28 10:46:12 +0200

    Use the model name otherwise we pass the scope

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -217,7 +217,7 @@ class Admin::ResourcesController < Admin::BaseController
   end
 
   def custom_actions_for(action)
-    @resource.typus_actions_on(action).reject { |a| admin_user.cannot?(a, @resource) }
+    @resource.typus_actions_on(action).reject { |a| admin_user.cannot?(a, @resource.model_name) }
   end
 
 end
