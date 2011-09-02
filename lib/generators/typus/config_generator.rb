14cf7cd301d09e41f95ee043e4d38a71a3e0464e
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-09-02 08:50:48 +0200

    KISS: We don't really want to be so intelligent ...

--- lib/generators/typus/config_generator.rb
+++ lib/generators/typus/config_generator.rb
@@ -32,44 +32,24 @@ Description:
         Typus.reload!
 
         configuration = {}
-
-        models = Typus.application_models.reject { |m| Typus.models.include?(m) }
-        models = models.map { |m| m.constantize }
+        models = Typus.application_models.reject { |m| Typus.models.include?(m) }.map { |m| m.constantize }
 
         models.each do |model|
           configuration[model.table_name] = {}
 
           relationships = [ :has_many, :has_one ].map do |relationship|
                             model.reflect_on_all_associations(relationship).map { |i| i.name.to_s }
-                          end.flatten
+                          end.flatten.join(", ")
 
-          rejections = %w( ^id$
-                           created_at created_on updated_at updated_on deleted_at
-                           salt crypted_password
-                           password_salt persistence_token single_access_token perishable_token
-                           _type$ type
-                           _file_size$ )
-
-          default_rejections = (rejections + %w( password password_confirmation )).join("|")
-          form_rejections = (rejections + %w( position )).join("|")
-
-          fields = model.columns.map(&:name)
-          default = fields.reject { |f| f.match(default_rejections) }
-          form = fields.reject { |f| f.match(form_rejections) }
-
-          # We want attributes of belongs_to relationships to be shown in our
-          # field collections if those are not polymorphic.
-          [ default, form ].each do |fields|
-            fields << model.reflect_on_all_associations(:belongs_to).reject { |i| i.options[:polymorphic] }.map { |i| i.name.to_s }
-            fields.flatten!
-          end
+          rejections = %w( ^id$ _type$ type created_at created_on updated_at updated_on deleted_at ).join("|")
+          fields = model.columns.map(&:name).reject { |f| f.match(rejections) }.join(", ")
 
           configuration[model.table_name][:base] = <<-RAW
 #{model}:
   fields:
-    default: #{default.join(", ")}
-    form: #{form.join(", ")}
-  relationships: #{relationships.join(", ")}
+    default: #{fields}
+    form: #{fields}
+  relationships: #{relationships}
   application: #{options[:admin_title]}
           RAW
 
