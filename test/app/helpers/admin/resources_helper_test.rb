4d172f27add016a8448464ce2c7ee1fd4c3b403b
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-09-06 09:09:07 +0200

    Take back search helper tests.

--- test/app/helpers/admin/resources_helper_test.rb
+++ test/app/helpers/admin/resources_helper_test.rb
@@ -2,54 +2,52 @@
 
 require "test_helper"
 
-class Admin::SearchHelperTest < ActiveSupport::TestCase
+class Admin::ResourcesHelperTest < ActiveSupport::TestCase
 
-  include Admin::SearchHelper
+  include Admin::ResourcesHelper
 
   def render(*args); args; end
 
+  setup do
+    @expected = ["helpers/admin/resources/search", {:hidden_filters => {}}]
+  end
+
   test "search rejects controller and action params" do
     parameters = {"controller"=>"admin/posts", "action"=>"index"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   # Why do you need the pagination page for a new search?
   test "search rejects page param" do
     parameters = {"page"=>"1"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   # TODO: I want to think about it ...
   test "search rejects locale params" do
     parameters = {"locale"=>"jp"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   # TODO: I want to think about it ...
   test "search rejects to sort_order and order_by" do
     parameters = {"sort_order"=>"asc", "order_by"=>"title"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   test "search rejects utf8 param because form already contains it" do
     parameters = {"utf8"=>"✓"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   test "search rejects search param because form already contains it" do
     parameters = {"search"=>"Chunky Bacon"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters => {}}]
-    assert_equal expected, search(Entry, parameters)
+    assert_equal @expected, search(Entry, parameters)
   end
 
   test "search not rejects applied filters" do
     parameters = {"published"=>"true", "user_id"=>"1"}
-    expected = ["helpers/admin/resources/search/search", {:hidden_filters=>{"published"=>"true", "user_id"=>"1"}}]
+    expected = ["helpers/admin/resources/search", {:hidden_filters=>{"published"=>"true", "user_id"=>"1"}}]
     assert_equal expected, search(Entry, parameters)
   end
 
