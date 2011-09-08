957bf49dae5fe7ecf5bc094ead6acddb29e93bff
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-09-08 20:26:55 +0200

    There's no need to have the extension when require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require File.expand_path("../dummy/config/environment",  __FILE__)
 ActiveRecord::Schema.verbose = false
 require File.expand_path("../dummy/db/schema",  __FILE__)
 require "rails/test_help"
