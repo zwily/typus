cd3e4de928a55f53cabcc0a67d1fd9106c67c10f
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-09-13 09:17:28 +0200

    Simplified autocomplete code

--- lib/typus/controller/autocomplete.rb
+++ lib/typus/controller/autocomplete.rb
@@ -3,12 +3,9 @@ module Typus
     module Autocomplete
 
       def autocomplete
-        if params[:term]
-          params.merge!(:search => params[:term])
-          get_objects
-          data = @resource.limit(20)
-          render :json => data.map { |i| { "id" => i.id, "name" => i.to_label } }
-        end
+        get_objects
+        data = @resource.limit(20)
+        render :json => data.map { |i| { "id" => i.id, "name" => i.to_label } }
       end
 
     end
