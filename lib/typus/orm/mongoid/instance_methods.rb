198a3c38bcf97231189c2eb881411bbbdf310d9e
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-12-24 18:31:15 +0100

    Removed old code

--- lib/typus/orm/mongoid/instance_methods.rb
+++ lib/typus/orm/mongoid/instance_methods.rb
@@ -3,14 +3,6 @@ module Typus
     module Mongoid
       module InstanceMethods
 
-        # def assign_attributes(attrs = nil, options = {})
-        #   assigning do
-        #     process(attrs, !options[:without_protection]) do |document|
-        #       document.identify if new? && id.blank?
-        #     end
-        #   end
-        # end
-
         def update_attributes(attributes, options ={})
           super(attributes)
         end
