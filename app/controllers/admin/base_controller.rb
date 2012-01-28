879718427b74db2c3c50c7a4e721b7b07f3f4672
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-01-28 12:01:23 +0100

    3 lines into 1

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,9 +2,7 @@ class Admin::BaseController < ActionController::Base
 
   include Typus::Authentication::const_get(Typus.authentication.to_s.classify)
 
-  before_filter :reload_config_and_roles
-  before_filter :authenticate
-  before_filter :set_locale
+  before_filter :reload_config_and_roles, :authenticate, :set_locale
 
   helper_method :admin_user
   helper_method :current_role
