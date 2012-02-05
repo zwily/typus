e6475c4defab342482475f59acf2855c123821f9
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-02-05 20:51:04 +0100

    Enabled registerable module, so we can see it working.

--- test/dummy/app/models/devise_user.rb
+++ test/dummy/app/models/devise_user.rb
@@ -4,7 +4,7 @@ require 'typus/orm/active_record/user/instance_methods_more'
 class DeviseUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, # :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
