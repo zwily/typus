29ad05c413d49654746d9f99de46885a8a6a4014
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-02-08 08:30:33 +0100

    _addanother save option shown if allowed
    This option should be only be shown if the user has access to it.

--- app/helpers/admin/resources/form_helper.rb
+++ app/helpers/admin/resources/form_helper.rb
@@ -60,7 +60,10 @@ module Admin::Resources::FormHelper
 
   def save_options
     options = {}
-    options["_addanother"] = "Save and add another"
+    if admin_user.can?('create', @resource.model_name)
+      options["_addanother"] = "Save and add another"
+    end
+
     if admin_user.can?('edit', @resource.model_name)
       options["_continue"] = "Save and continue editing"
     end
