d082f9de4e5db98f685d946d53ce4be9a55f08a1
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-02-26 11:33:58 +0100

    Test to verify Typus::I18n.t interpolation.

--- test/lib/typus/i18n_test.rb
+++ test/lib/typus/i18n_test.rb
@@ -5,6 +5,7 @@ class I18nTest < ActiveSupport::TestCase
   test "t" do
     assert_equal "Hello", Typus::I18n.t("Hello")
     assert_equal "Hello", Typus::I18n.t(".hello", :default => "Hello")
+    assert_equal "Hello @fesplugas", Typus::I18n.t("Hello %{human}!", :human => "@fesplugas")
   end
 
   test "default_locale" do
