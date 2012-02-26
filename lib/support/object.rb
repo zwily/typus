829577fd8c38ac46e17cc0cd67367b658ddef08f
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-02-26 10:59:00 +0100

    Cleaned comment.

--- lib/support/object.rb
+++ lib/support/object.rb
@@ -1,11 +1,7 @@
 class Object
 
-  ##
-  # TODO: Improve this code, which works, but probably there's a better way
-  #       to make the verification.
-  #
-  # Probably there's a better way to verify if a model responds to an STI
-  # pattern.
+  # OPTIMIZE: Probably there's a better way to verify if a model responds
+  # to an STI pattern.
   def self.is_sti?
     (name != base_class.name) && base_class.descends_from_active_record?
   end
