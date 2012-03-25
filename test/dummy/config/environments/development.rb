602379a99d30eea9ed77e2cfee81ce2adeeda6fd
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-03-25 20:56:28 +0200

    Changed email delivery method on development

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -45,6 +45,11 @@ Dummy::Application.configure do
 
   # Default url options for action mailer
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
 end
 
 Typus.setup do |config|
