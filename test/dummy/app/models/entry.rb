025722c438d50d065d9f1f5b1d5d18e1fb1eaa3d
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-03-27 10:13:53 +0200

    Filter accepted scopes!

--- test/dummy/app/models/entry.rb
+++ test/dummy/app/models/entry.rb
@@ -22,6 +22,12 @@ class Entry < ActiveRecord::Base
   has_and_belongs_to_many :categories
 
   ##
+  # Scopes
+  #
+
+  scope :published, where(:published => true)
+
+  ##
   # Instance Methods
   #
 
