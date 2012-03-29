84bde2fc5edb20da51c2843db45602106b346e8d
Francesc Esplugas <francesc.esplugas@gmail.com>
2012-03-29 09:11:50 +0200

    Refactored pagination mechanism detection

--- app/helpers/admin/resources/pagination_helper.rb
+++ app/helpers/admin/resources/pagination_helper.rb
@@ -1,13 +1,21 @@
 module Admin::Resources::PaginationHelper
 
   def admin_paginate(items, options)
-    if defined?(Kaminari)
-      paginate(items, options)
+    helper = if defined?(Kaminari)
+      "paginate"
     elsif defined?(WillPaginate)
-      will_paginate(items, options)
+      "will_paginate"
     else
-      render :partial => "admin/resources/pagination"
+      "typus_paginate"
     end
+
+    send(helper, items, options)
+  end
+
+  # TODO: Quick and dirty pagination solution! We only want to go back and
+  # forward, so it will be simple.
+  def typus_paginate(items, options)
+    render "admin/resources/pagination"
   end
 
 end
