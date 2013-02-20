0343cf08fc7d91deb36197bd46c882e2e34a8611
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-02-20 10:23:55 +0100

    Updated Copyright.

--- doc/user_guide/source/conf.py
+++ doc/user_guide/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Typus'
-copyright = u'2012, Francesc Esplugas'
+copyright = u'2013, Francesc Esplugas'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
