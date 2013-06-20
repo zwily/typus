c4631e3df469bc16ec261542113dcb9b69fd1e97
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-20 22:04:40 +0200

    Fixed tests

--- test/lib/typus/i18n_test.rb
+++ test/lib/typus/i18n_test.rb
@@ -4,8 +4,15 @@ class I18nTest < ActiveSupport::TestCase
 
   test "t" do
     assert_equal "Hello", Typus::I18n.t("Hello")
-    assert_equal "Hello", Typus::I18n.t(".hello", :default => "Hello")
+
     assert_equal "Hello @fesplugas!", Typus::I18n.t("Hello %{human}!", :human => "@fesplugas")
+  end
+
+  test 't using keys' do
+    assert_equal "Hello world", Typus::I18n.t(".hello", :default => "Hello world!")
+
+    I18n.locale = :missing
+    assert_equal "Missing Hello world", Typus::I18n.t(".hello", :default => "Missing Hello world")
   end
 
   test "default_locale" do
