771398975a1ec760edc4b80c6c172cf61fc25971
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-23 15:39:58 +0200

    WIP

--- test/dummy/app/models/admin_user.rb
+++ test/dummy/app/models/admin_user.rb
@@ -1,5 +1,3 @@
-# Here we test the new version of Typus Authenticated users. This is stuff
-# will change so you might prefer using the other mixin.
 class AdminUser < ActiveRecord::Base
   has_admin
 end
