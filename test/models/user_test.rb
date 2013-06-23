b857abf6c6fca63042f1704b03678e5ada33d421
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-23 15:36:55 +0200

    Fixed tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,12 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   test "to_label" do
-    user = FactoryGirl.build(:user)
+    user = FactoryGirl.build(:typus_user)
     assert_equal user.email, user.to_label
   end
 
   test "can?" do
-    user = FactoryGirl.build(:user, :role => 'admin')
+    user = FactoryGirl.build(:typus_user, :role => 'admin')
     assert user.can?('delete', TypusUser)
     refute user.cannot?('delete', TypusUser)
     assert user.can?('delete', 'TypusUser')
@@ -24,16 +24,16 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "is_root?" do
-    user = FactoryGirl.build(:user, :role => 'admin')
+    user = FactoryGirl.build(:typus_user, :role => 'admin')
     assert user.is_root?
     refute user.is_not_root?
   end
 
   test "active?" do
-    assert FactoryGirl.build(:user, :role => 'admin', :status => true).active?
-    refute FactoryGirl.build(:user, :role => 'admin', :status => false).active?
-    refute FactoryGirl.build(:user, :role => 'unexisting', :status => true).active?
-    refute FactoryGirl.build(:user, :role => 'unexisting', :status => false).active?
+    assert FactoryGirl.build(:typus_user, :role => 'admin', :status => true).active?
+    refute FactoryGirl.build(:typus_user, :role => 'admin', :status => false).active?
+    refute FactoryGirl.build(:typus_user, :role => 'unexisting', :status => true).active?
+    refute FactoryGirl.build(:typus_user, :role => 'unexisting', :status => false).active?
   end
 
 end
