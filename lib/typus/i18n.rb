380b580a83a8c00d4b0d93cfd5ef50d76458d742
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-23 16:29:29 +0200

    Gardening

--- lib/typus/i18n.rb
+++ lib/typus/i18n.rb
@@ -5,13 +5,13 @@ module Typus
 
     class << self
 
-      # Instead of having to translate strings and defining a default
-      # value to avoid "missing translation" messages:
+      # Instead of having to translate strings and defining a default value to
+      # avoid "missing translation" messages:
       #
       #     I18n.t("Hello World!", :default => "Hello World!")
       #
-      # We define a Typus translation method which will set up a default
-      # value for you: (Interpolation still works)
+      # We define a Typus translation method which will set up a default value
+      # for you: (Interpolation still works)
       #
       #     Typus::I18n.t("Hello World!")
       #     Typus::I18n.t("Hello %{world}!", :world => @world)
