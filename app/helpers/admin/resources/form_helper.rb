2fb43f58f257a6cf0b0004f39c76fae011a000cd
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-23 17:02:18 +0200

    Gardening

--- app/helpers/admin/resources/form_helper.rb
+++ app/helpers/admin/resources/form_helper.rb
@@ -23,10 +23,12 @@ module Admin::Resources::FormHelper
   end
 
   def typus_template_field(attribute, template, form)
-    options = { :start_year => @resource.typus_options_for(:start_year),
-                :end_year => @resource.typus_options_for(:end_year),
-                :minute_step => @resource.typus_options_for(:minute_step),
-                :include_blank => true }
+    options = {
+      :start_year => @resource.typus_options_for(:start_year),
+      :end_year => @resource.typus_options_for(:end_year),
+      :minute_step => @resource.typus_options_for(:minute_step),
+      :include_blank => true,
+    }
 
     label_text = @resource.human_attribute_name(attribute)
 
@@ -34,13 +36,15 @@ module Admin::Resources::FormHelper
       label_text += "<small>#{Typus::I18n.t("Read only")}</small>"
     end
 
-    locals = { :resource => @resource,
-               :attribute => attribute,
-               :attribute_id => "#{@resource.table_name}_#{attribute}",
-               :options => options,
-               :html_options => {},
-               :form => form,
-               :label_text => label_text.html_safe }
+    locals = {
+      :resource => @resource,
+      :attribute => attribute,
+      :attribute_id => "#{@resource.table_name}_#{attribute}",
+      :options => options,
+      :html_options => {},
+      :form => form,
+      :label_text => label_text.html_safe,
+    }
 
     render "admin/templates/#{template}", locals
   end
