33ea60e1450e2352c43d13ca75f7bbab706a1864
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-26 22:14:35 +0200

    Failing test for has_and_belongs_to assocs.

---
+++ test/controllers/admin/resources/habtm_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+=begin
+
+  What's being tested here?
+
+    - has_and_belongs_to
+
+=end
+
+class Admin::EntriesControllerTest < ActionController::TestCase
+
+  setup do
+    admin_sign_in
+  end
+
+  test 'adding multiple categories to an entry' do
+    category_1 = FactoryGirl.create(:category)
+    category_2 = FactoryGirl.create(:category)
+
+    user = FactoryGirl.create(:user)
+
+    Entry.delete_all
+
+    assert_difference('Entry.count') do
+      entry_data = {
+        :title => 'Title' ,
+        :content => 'Content',
+        :category_ids => ["", "#{category_1.id}", "#{category_2.id}" ]
+      }
+
+      post :create, :entry => entry_data
+    end
+
+    assert_equal [category_1, category_2], Entry.first.categories
+  end
+
+end
