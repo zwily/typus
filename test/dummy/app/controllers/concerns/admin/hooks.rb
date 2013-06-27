d4210e499c905e5b022f0bc71b8439cdf6930d1f
Francesc Esplugas <francesc.esplugas@gmail.com>
2013-06-27 21:25:55 +0200

    Cleaned hook

--- test/dummy/app/controllers/concerns/admin/hooks.rb
+++ test/dummy/app/controllers/concerns/admin/hooks.rb
@@ -3,18 +3,7 @@ require 'active_support/concern'
 module Admin
   module Hooks
 
-    extend ActiveSupport::Concern
-
-    included do
-      before_filter :switch_label_2
-    end
-
-    def switch_label_2
-      logger.info "%" * 72
-      logger.info "%" * 72
-      logger.info "%" * 72
-      logger.info "%" * 72
-      logger.info "%" * 72
+    def white_label
     end
 
   end
