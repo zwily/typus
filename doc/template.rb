64281e04dca06d65983fd0ee15e2bc9e81067728
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-27 10:44:23 +0200

    Updated application template.

--- doc/template.rb
+++ doc/template.rb
@@ -1,6 +1,6 @@
 # To Run:
 #
-#     rails new typus-demo -m https://raw.github.com/fesplugas/typus/master/doc/template.rb
+#     rails new typus-demo -m https://raw.github.com/typus/typus/master/doc/template.rb
 #     rails new typus-demo -m ~/Development/typus/doc/template.rb
 #
 
@@ -9,8 +9,8 @@ generate :model, 'Entry title:string'
 rake 'db:migrate'
 
 # Add Typus to Gemfile and run generator
-gem 'typus', :git => 'git://github.com/fesplugas/typus.git'
-run 'bundle install --quiet'
+gem 'typus', github: 'typus/typus'
+run 'bundle install'
 generate 'typus'
 generate 'typus:migration'
 rake 'db:migrate'
