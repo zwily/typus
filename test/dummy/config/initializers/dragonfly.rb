f720923a9abbb7e315483de5ccb06198af63e214
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-27 10:15:16 +0200

    Updated dragonfly initializer

--- test/dummy/config/initializers/dragonfly.rb
+++ test/dummy/config/initializers/dragonfly.rb
@@ -4,8 +4,12 @@ require 'dragonfly'
 Dragonfly.app.configure do
   plugin :imagemagick
 
+<<<<<<< HEAD
   protect_from_dos_attacks true
   secret "7fac81075e295d08d80c5ed5c327bc11d6c42e5bd9614ac994c41bed40bbb5e9"
+=======
+  secret "a62cb203f9407666c5e5e2ffa739c4d1b6964bc3ea326c74a11ba75c2c2a39a6"
+>>>>>>> Updated dragonfly initializer
 
   url_format "/media/:job/:name"
 
