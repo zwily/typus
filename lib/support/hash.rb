a5eb92697c83144aa5dd9df66eae6cfeabff1f1d
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-27 15:09:52 +0200

    Replaced unused value in block by _

--- lib/support/hash.rb
+++ lib/support/hash.rb
@@ -2,7 +2,7 @@ class Hash
 
   def cleanup
     whitelist = %w(controller action id _input _popup resource attribute)
-    delete_if { |k, v| !whitelist.include?(k) }
+    delete_if { |k, _| !whitelist.include?(k) }
   end
 
 end
