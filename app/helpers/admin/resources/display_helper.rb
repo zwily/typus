77b0e7b307c739317da31847e8bfb5ca5dd74934
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-28 16:30:29 +0200

    Do now show attributes which are not present.

--- app/helpers/admin/resources/display_helper.rb
+++ app/helpers/admin/resources/display_helper.rb
@@ -1,15 +1,11 @@
 module Admin::Resources::DisplayHelper
 
-  def mdash
-    '&mdash;'.html_safe
-  end
-
   def build_display(item, fields)
     fields.map do |attribute, type|
       condition = (type == :boolean) || item.send(attribute).present?
-      value = condition ? send("display_#{type}", item, attribute) : mdash
-      [@resource.human_attribute_name(attribute), value]
-    end
+      next unless condition
+      [@resource.human_attribute_name(attribute), send("display_#{type}", item, attribute)]
+    end.compact
   end
 
 end
