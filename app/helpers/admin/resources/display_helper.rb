98873f15b86707334cc76d4254ce8d5c5dbc3666
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 17:14:49 +0200

    Revert "Removed "typus_relationships" method."
    This reverts commit f858c02cea352319f946e0bec3958d1cf52d9cd9.

--- app/helpers/admin/resources/display_helper.rb
+++ app/helpers/admin/resources/display_helper.rb
@@ -12,4 +12,16 @@ module Admin::Resources::DisplayHelper
     end.compact
   end
 
+  def typus_relationships
+    String.new.tap do |html|
+      @resource.typus_defaults_for(:relationships).each do |relationship|
+        association = @resource.reflect_on_association(relationship.to_sym)
+        macro, klass = association.macro, association.class_name.constantize
+        if [:has_many, :has_one].include?(macro) && admin_user.can?('read', klass)
+          html << send("typus_form_#{macro}", relationship)
+        end
+      end
+    end.html_safe
+  end
+
 end
