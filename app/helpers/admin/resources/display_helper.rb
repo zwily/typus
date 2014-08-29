4a53f1d27a36ec94d8606dcf956bce70a9abcc20
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 08:49:56 +0200

    Take back "mdash" helper method.

--- app/helpers/admin/resources/display_helper.rb
+++ app/helpers/admin/resources/display_helper.rb
@@ -1,5 +1,9 @@
 module Admin::Resources::DisplayHelper
 
+  def mdash
+    '&mdash;'.html_safe
+  end
+
   def build_display(item, fields)
     fields.map do |attribute, type|
       condition = (type == :boolean) || item.send(attribute).present?
