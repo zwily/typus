5e9668bf6ebb5f03fe65b1a87ca558505d7a97f4
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 09:23:18 +0200

    Still amazed that we don't need this anymore.

--- app/helpers/admin/resources/filters_helper.rb
+++ app/helpers/admin/resources/filters_helper.rb
@@ -5,7 +5,7 @@ module Admin::Resources::FiltersHelper
       locals = {}
 
       locals[:filters] = typus_filters.map do |key, value|
-                           { key: set_filter(key, value), value: send("#{value}_filter", key) }
+                           { key: key, value: send("#{value}_filter", key) }
                          end
 
       rejections = %w(controller action locale utf8 sort_order order_by) + locals[:filters].map { |f| f[:key] }
@@ -13,15 +13,6 @@ module Admin::Resources::FiltersHelper
 
       render 'helpers/admin/resources/filters', locals
     end
-  end
-
-  def set_filter(key, value)
-    return key unless value == :belongs_to
-
-    att_assoc = @resource.reflect_on_association(key.to_sym)
-    class_name = att_assoc.options[:class_name] || key.capitalize.camelize
-    resource = class_name.constantize
-    att_assoc.foreign_key
   end
 
 end
