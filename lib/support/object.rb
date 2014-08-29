3067d9a5318d971e1ee04523f23f75d8633f7600
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 12:38:57 +0200

    :nail_care:

--- lib/support/object.rb
+++ lib/support/object.rb
@@ -1,7 +1,7 @@
 class Object
 
   # OPTIMIZE: Probably there's a better way to verify if a model responds
-  # to an STI pattern.
+  # to a STI pattern.
   def self.is_sti?
     (name != base_class.name) && base_class.descends_from_active_record?
   end
