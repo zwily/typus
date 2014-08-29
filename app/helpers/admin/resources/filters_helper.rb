218a12146b10e27f7f09c2f0b7e8b896ab96e77f
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 09:22:55 +0200

    Use a wildcard for an unused variable.

--- app/helpers/admin/resources/filters_helper.rb
+++ app/helpers/admin/resources/filters_helper.rb
@@ -9,7 +9,7 @@ module Admin::Resources::FiltersHelper
                          end
 
       rejections = %w(controller action locale utf8 sort_order order_by) + locals[:filters].map { |f| f[:key] }
-      locals[:hidden_filters] = params.dup.delete_if { |k, v| rejections.include?(k) }
+      locals[:hidden_filters] = params.dup.delete_if { |k, _| rejections.include?(k) }
 
       render 'helpers/admin/resources/filters', locals
     end
