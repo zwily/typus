447c0b6e778dccf06c2001f9cae51714b181b7f3
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 09:32:09 +0200

    :nail_care:

--- app/helpers/admin/resources/data_types/tree_helper.rb
+++ app/helpers/admin/resources/data_types/tree_helper.rb
@@ -5,19 +5,21 @@ module Admin::Resources::DataTypes::TreeHelper
   end
 
   def typus_tree_field(attribute, form)
-    locals = { :attribute => attribute,
-               :attribute_id => "#{@resource.table_name}_#{attribute}",
-               :form => form,
-               :label_text => @resource.human_attribute_name(attribute),
-               :values => raw(expand_tree_into_select_field(@resource.roots, "parent_id")) }
+    locals = {
+      attribute: attribute,
+      attribute_id: "#{@resource.table_name}_#{attribute}",
+      form: form,
+      label_text: @resource.human_attribute_name(attribute),
+      values: raw(expand_tree_into_select_field(@resource.roots, 'parent_id')),
+    }
 
-    render "admin/templates/tree", locals
+    render 'admin/templates/tree', locals
   end
 
   def expand_tree_into_select_field(items, attribute)
     String.new.tap do |html|
       items.each do |item|
-        html << %{<option #{"selected" if @item.send(attribute) == item.id} value="#{item.id}">#{"&nbsp;" * item.ancestors.size * 2} #{item.to_label}</option>\n}
+        html << %{<option #{'selected' if @item.send(attribute) == item.id} value="#{item.id}">#{'&nbsp;' * item.ancestors.size * 2} #{item.to_label}</option>\n}
         html << expand_tree_into_select_field(item.children, attribute) unless item.children.empty?
       end
     end
