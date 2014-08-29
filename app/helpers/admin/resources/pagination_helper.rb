758d83dce81d53d1d7a47eacbeb73e164e95fc13
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 21:07:15 +0200

    Properly scope pagination.

--- app/helpers/admin/resources/pagination_helper.rb
+++ app/helpers/admin/resources/pagination_helper.rb
@@ -1,7 +1,7 @@
 module Admin::Resources::PaginationHelper
 
-  def admin_paginate
-    params[:per_page] ||= @resource.typus_options_for(:per_page)
+  def admin_paginate(resource = @resource)
+    params[:per_page] ||= resource.typus_options_for(:per_page)
     params[:per_page] = params[:per_page].to_i
 
     params[:offset] ||= 0
@@ -13,7 +13,7 @@ module Admin::Resources::PaginationHelper
     options = {}
 
     current_page = (params[:offset].to_f / params[:per_page]) + 1
-    num_pages = (@resource.count.to_f / params[:per_page]).ceil
+    num_pages = (resource.count.to_f / params[:per_page]).ceil
     unless current_page >= num_pages
       options[:next] = params.merge(offset: next_offset)
     end
