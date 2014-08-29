9a28cc4788a2fbbeaac748ef2b51daa938e572ff
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-29 22:01:54 +0200

    :nail_care:

--- test/dummy/app/models/project.rb
+++ test/dummy/app/models/project.rb
@@ -4,16 +4,16 @@ class Project < ActiveRecord::Base
   # Validations
   #
 
-  validates :name, :presence => true
-  validates :user, :presence => true
+  validates :name, presence: true
+  validates :user, presence: true
 
   ##
   # Associations
   #
 
   belongs_to :user
-  has_many :collaborators, :through => :project_collaborators, :source => :user
-  has_many :project_collaborators, :dependent => :destroy
+  has_many :collaborators, through: :project_collaborators, source: :user
+  has_many :project_collaborators, dependent: :destroy
   has_many :tasks
 
 end
