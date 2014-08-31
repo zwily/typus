4c0d950e420c48a6a4b5e79ad38f10fc6dbd98ea
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-31 08:49:01 +0200

    Removed puts

--- test/features/login_test.rb
+++ test/features/login_test.rb
@@ -7,9 +7,6 @@ class LoginTest < Capybara::Rails::TestCase
   end
 
   test 'Successful login' do
-
-    puts TypusUser.all.inspect
-
     visit root_path
     fill_in('Email', with: @user.email)
     fill_in('Password', with: @user.password)
