51fbf2d97ab0acaa43527fcf6d8b4f8f7741207b
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-31 08:49:21 +0200

    Moved from to method.

--- app/mailers/admin/mailer.rb
+++ app/mailers/admin/mailer.rb
@@ -1,12 +1,11 @@
 class Admin::Mailer < ActionMailer::Base
 
-  default from: Typus.mailer_sender
-
   def reset_password_instructions(user, host)
     @user = user
     @token_url = admin_account_url(user.token, host: host)
 
     options = {
+      from: Typus.mailer_sender,
       to: user.email,
       subject: I18n.t('typus.emails.password_reset.title', admin_title: Typus.admin_title)
     }
