ce68394b9f53ae9139559981fdc7f30248c16ced
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-08-31 09:56:19 +0200

    puts

--- test/controllers/admin/account_controller_test.rb
+++ test/controllers/admin/account_controller_test.rb
@@ -87,6 +87,8 @@ class Admin::AccountControllerTest < ActionController::TestCase
   test 'show with token generates a session a redirects user to edit' do
     typus_user = typus_users(:admin)
 
+    puts TypusUser.class_name
+
     get :show, id: typus_user.token
     assert_response :redirect
     assert_redirected_to controller: '/admin/typus_users', action: 'edit', id: typus_user.id
